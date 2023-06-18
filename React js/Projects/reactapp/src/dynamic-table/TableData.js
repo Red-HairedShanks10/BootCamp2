c9c92b08467aaf68b47ddc70c965a76193dae082
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-06-18 19:32:15 +0200

    extra update

---
+++ React js/Projects/reactapp/src/dynamic-table/TableData.js
@@ -0,0 +1,8 @@
+const TableData = [
+    { id: 1, fullName: "Ricardo Silva", email:"rickySil@gmail.com", age: 20, city: "Sao Paulo"  },
+    { id: 2, fullName: "Marco Dube", email:"marcoD@gmail.com", age: 22, city: "Johannesburg" },
+    { id: 3, fullName: "Sunil Singh", email:"SunSing@mymail.com", age: 18, city: "New Delhi" },
+    { id: 4, fullName: "Thabo Zulu", email:"Zulu-T@greenmail.com", age: 22, city: "KZN" },
+    { id: 5, fullName: "Edmil Dajinga", email:"EddyDaji@gmail.com", age: 21, city: "luanda" }
+]
+export default TableData;
