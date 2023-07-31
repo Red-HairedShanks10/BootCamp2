86c3071af429ac637ab3f1cbb4fe4bb70f22bcd9
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-07-31 14:09:19 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch1/HelloApp2.java
+++ JAVA/JavagroupA/src/book2/ch1/HelloApp2.java
@@ -2,8 +2,11 @@ package book2.ch1;
 
 public class HelloApp2
 {
-public static void main(String[] args)
-{
+public static void main(String[] args) {
+  Greeter gObject = new Greeter();
+  gObject.sayHello();
 
 }
+
+
 }
