ba7a47a185f7cee45599a69abb268da12d5a4341
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-07-31 14:14:14 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch1/HelloApp2.java
+++ JAVA/JavagroupA/src/book2/ch1/HelloApp2.java
@@ -1,7 +1,8 @@
 package book2.ch1;
-
+//creating instance of the Greeter class in HelloAPP
 public class HelloApp2 {
     public static void main(String[] args) {
+
         Greeter gObject = new Greeter();
         gObject.sayHello();
 
