6f18098cb2f86b602b16aa244068edd9cf50b71b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-07-31 21:12:57 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch2/HelloApp.java
+++ JAVA/JavagroupA/src/book2/ch2/HelloApp.java
@@ -2,7 +2,8 @@ package book2.ch2;
 
 public class HelloApp {
 
-    static String helloMessage;
+    static String helloMessage;//declaring a class variable of string type
+    int num1;
     public static void main(String[] args)
     {
         //declaring multiple variables in one statement
