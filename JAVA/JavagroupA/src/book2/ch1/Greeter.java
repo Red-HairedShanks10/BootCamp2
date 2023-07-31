3b80a3dba5a6529657d0ab46a47dc234709009ce
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-07-31 14:00:57 +0200

    java commit

---
+++ JAVA/JavagroupA/src/book2/ch1/Greeter.java
@@ -0,0 +1,7 @@
+package book2.ch1;
+
+public class Greeter {
+    public void sayHello(){
+        System.out.println("Hello I am the Greeter");
+    }
+}
---
+++ JAVA/JavagroupA/src/book2/ch1/HelloApp2.java
@@ -0,0 +1,9 @@
+package book2.ch1;
+
+public class HelloApp2
+{
+public static void main(String[] args)
+{
+
+}
+}
