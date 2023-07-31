1874baa68b0929684d4366b2c50d224f80b8dd2f
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-07-31 21:10:30 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch2/HelloApp.java
+++ JAVA/JavagroupA/src/book2/ch2/HelloApp.java
@@ -5,11 +5,15 @@ public class HelloApp {
     static String helloMessage;
     public static void main(String[] args)
     {
+        //declaring multiple variables in one statement
+        // int x,y,z;
+
         /*
         helloMessage = "Hello World!";
         System.out.println(helloMessage); */
 
-        //declaring multiple variables in one statement
-       // int x,y,z;
+
+
+
     }
 }
