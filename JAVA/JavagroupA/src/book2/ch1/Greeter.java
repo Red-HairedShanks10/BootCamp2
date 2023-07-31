754112db422f07a93f148fb35a130ff8033b86a5
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-07-31 14:18:43 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch1/Greeter.java
+++ JAVA/JavagroupA/src/book2/ch1/Greeter.java
@@ -3,6 +3,10 @@ import javax.swing.JOptionPane;
 
 public class Greeter {
     public void sayHello(){
-        System.out.println("Hello I am the Greeter");
+        //System.out.println("Hello I am the Greeter");
+
+        JOptionPane.showMessageDialog(null,
+                "Hello, World!", "Greeter",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }
