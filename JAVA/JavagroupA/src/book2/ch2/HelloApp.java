4af6ef507bdfc29966731b38a24a4931bbd58d6b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-07-31 15:40:34 +0200

    java commit

---
+++ JAVA/JavagroupA/src/book2/ch2/HelloApp.java
@@ -0,0 +1,10 @@
+package book2.ch2;
+
+public class HelloApp {
+    static String helloMessage;
+    public static void main(String[] args)
+    {
+        helloMessage = "Hello, World!";
+        System.out.println(helloMessage);
+    }
+}
