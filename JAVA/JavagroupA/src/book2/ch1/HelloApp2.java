19b8c65724a1b2a53483d15fb2ee3461a85c93c7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 09:11:47 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch1/HelloApp2.java
+++ JAVA/JavagroupA/src/book2/ch1/HelloApp2.java
@@ -1,10 +1,16 @@
 package book2.ch1;
+
+import book2.ch2.HelloApp;
+
 //creating instance of the Greeter class in HelloAPP
 public class HelloApp2 {
     public static void main(String[] args) {
 
-        Greeter gObject = new Greeter();
-        gObject.sayHello();
+       /* Greeter gObject = new Greeter();
+        gObject.sayHello(); */
+       System.out.println(HelloApp.helloMessage);
+       // HelloApp.main(args);
+
 
     }
 
--- JAVA/JavagroupA/src/book2/ch2/HelloApp.java
+++ JAVA/JavagroupA/src/book2/ch2/HelloApp.java
@@ -2,17 +2,17 @@ package book2.ch2;
 
 public class HelloApp {
 
-    static String helloMessage;//declaring a class variable of string type
+    public static String helloMessage;//declaring a class variable of string type
     int num1; //can you declare class variables without 'static' and get away with it?
     public static void main(String[] args)
     {
         //declaring multiple variables in one statement
         // int x,y,z;
 
-       /*  using the class variable   within the main method
-        *assigning it a value
-        helloMessage = "Hello World!";
-        System.out.println(helloMessage); */
+      /*   using the class variable   within the main method
+        *assigning it a value */
+        helloMessage = "Hello World! From book2.ch2 ";
+        System.out.println(helloMessage);
 
 
     }
