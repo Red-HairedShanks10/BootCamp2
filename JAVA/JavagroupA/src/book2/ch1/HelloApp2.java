d153d5f06ff190800a67c73d37af481726ff4d1a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 10:13:20 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch1/HelloApp2.java
+++ JAVA/JavagroupA/src/book2/ch1/HelloApp2.java
@@ -1,6 +1,6 @@
 package book2.ch1;
 
-import book2.ch2.HelloApp;//import this HelloApp
+import book2.ch2.HelloApp; //import this HelloApp
 
 //creating instance of the Greeter class in HelloAPP
 public class HelloApp2 {
@@ -11,6 +11,14 @@ public class HelloApp2 {
 
        /*System.out.println(HelloApp.helloMessage);//will be null becoz of the initial declaration in HelloApp
        HelloApp.main(args);//the helloMessage string will display a value bcoz main is where assign value to the string */
+
+        //**Using an instance from a different package
+        HelloApp instanceVarBTwo = new HelloApp();
+        System.out.println(instanceVarBTwo.iHelloMessage);//will return null becoz of initial declaration....
+        //*can we assign a value to this instance var?
+        instanceVarBTwo.iHelloMessage = "Hello message from HelloApp2";
+        System.out.println(instanceVarBTwo.iHelloMessage);
+
 
 
 
