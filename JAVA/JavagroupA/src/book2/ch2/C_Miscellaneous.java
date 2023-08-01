d99d212bd91616f7d7bd4abca9289be952f7b03b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 19:55:27 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch2/C_Miscellaneous.java
+++ JAVA/JavagroupA/src/book2/ch2/C_Miscellaneous.java
@@ -3,6 +3,7 @@ package book2.ch2;
 public class C_Miscellaneous {
     public static void main(String[] args) {
 
+        //testing difference between print() and println();
         int i = 64;
         int j = 23;
         System.out.print(i);
