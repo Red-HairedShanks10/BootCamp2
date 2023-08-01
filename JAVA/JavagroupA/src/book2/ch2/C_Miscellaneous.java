7ed59af5cd8b4df3dd5dc63ac2b48bd1c0eb66b0
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 19:47:27 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch2/C_Miscellaneous.java
+++ JAVA/JavagroupA/src/book2/ch2/C_Miscellaneous.java
@@ -3,6 +3,11 @@ package book2.ch2;
 public class C_Miscellaneous {
     public static void main(String[] args) {
 
+        int i = 64;
+        int j = 23;
+        System.out.print(i);
+        System.out.print(" and ");
+        System.out.println(j);
 
     }
 
