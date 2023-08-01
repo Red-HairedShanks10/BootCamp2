7339b0b66efaa7a48a13254ee3b72f0b43c9b386
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 20:03:55 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch2/ScannerApp.java
+++ JAVA/JavagroupA/src/book2/ch2/ScannerApp.java
@@ -4,7 +4,7 @@ package book2.ch2;
 import java.util.Scanner;
 
 public class ScannerApp {
-    Scanner scannerIn = new Scanner(System.in);
+   static Scanner scannerIn = new Scanner(System.in);
     public static void main(String[] args) {
 
 
