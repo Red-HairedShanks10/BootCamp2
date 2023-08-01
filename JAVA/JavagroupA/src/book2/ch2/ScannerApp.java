3922c2af84eecdef0feb74daec8292682c813713
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 20:57:03 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch2/ScannerApp.java
+++ JAVA/JavagroupA/src/book2/ch2/ScannerApp.java
@@ -9,13 +9,16 @@ public class ScannerApp {
     public static void main(String[] args) {
 
        System.out.println("enter a number: ");//prompt a user to enter a number
-       int x = scannerIn.nextInt();//using Scanner's nextInt() to read the input value?
-       System.out.println("Your number is: " + x );//printing the input value...
+      // int x = scannerIn.nextInt();//using Scanner's nextInt() to read the input value?
+       //System.out.println("Your number is: " + x );//printing the input value...
         //if user inputs a different data type the program crashes
         /*to avoid this we can add an if statement that uses
-        * Scanner methods that check for the right input value
+        * Scanner methods that check for the right input value:
         */
-
+        if (scannerIn.hasNextInt()){
+            int x = scannerIn.nextInt();//using Scanner's nextInt() to read the input value?
+            System.out.println("Your number is: " + x );//printing the input value...
+        }
 
 
 
