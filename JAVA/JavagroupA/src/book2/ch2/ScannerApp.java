1da463a390686dce666931f015c470a4cd5a23c8
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 20:02:42 +0200

    java commit

---
+++ JAVA/JavagroupA/src/book2/ch2/ScannerApp.java
@@ -0,0 +1,12 @@
+package book2.ch2;
+
+
+import java.util.Scanner;
+
+public class ScannerApp {
+    Scanner scannerIn = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+    }
+}
