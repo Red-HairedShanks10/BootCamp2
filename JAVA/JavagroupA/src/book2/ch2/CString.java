25390b3ac4094d5eb7a77d1d63929730ac92677a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 12:44:28 +0200

    java commit

---
+++ JAVA/JavagroupA/src/book2/ch2/CString.java
@@ -0,0 +1,10 @@
+package book2.ch2;
+
+public class CString {
+    public static void main(String[] args){
+
+        int x = 34;
+        String s = Integer.toString(x);
+        System.out.println(s);
+    }
+}
