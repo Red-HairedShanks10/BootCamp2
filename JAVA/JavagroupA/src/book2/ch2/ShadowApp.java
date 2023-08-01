3ec7992e95f2a7f01f96d341e1550104b20aa8ca
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 13:28:09 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch2/ShadowApp.java
+++ JAVA/JavagroupA/src/book2/ch2/ShadowApp.java
@@ -2,6 +2,14 @@ package book2.ch2;
 
 public class ShadowApp {
     public static void main(String[] args) {
-        
+        {
+            x = 5; →8
+            System.out.println("x = " + x); →9
+            int x; →10
+            x = 10; →11
+            System.out.println("x = " + x); →12
+            System.out.println("ShadowApp.x = " +
+                    ShadowApp.x); →14
+        }
     }
 }
