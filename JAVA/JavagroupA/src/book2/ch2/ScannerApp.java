b8a5f8c9daf453b03e3c2c47f79f5b29f4ad8d9b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-01 20:05:42 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch2/ScannerApp.java
+++ JAVA/JavagroupA/src/book2/ch2/ScannerApp.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 
 public class ScannerApp {
    static Scanner scannerIn = new Scanner(System.in);
+
     public static void main(String[] args) {
+       System.out.println("enter a number: ");
+
 
 
     }
