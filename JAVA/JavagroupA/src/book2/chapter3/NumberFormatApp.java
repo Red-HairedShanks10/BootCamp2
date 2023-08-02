a9a92645b28b17b550455cad9e72909bbd5c2b4e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 11:25:56 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/NumberFormatApp.java
+++ JAVA/JavagroupA/src/book2/chapter3/NumberFormatApp.java
@@ -5,27 +5,23 @@ import java.text.NumberFormat;
 public class NumberFormatApp {
     static NumberFormat cf = NumberFormat.getCurrencyInstance();
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         printMyAllowance();
         printCostOfPaintBallGun();
     }
 
-    public static void printMyAllowance()
-    {
+    public static void printMyAllowance() {
         double myAllowance = 3340;
         cf = NumberFormat.getCurrencyInstance();
         System.out.println("My allowance is: " + cf.format(myAllowance));
     }
 
-    public static void printCostOfPaintBallGun()
-    {
+
+    public static void printCostOfPaintBallGun() {
         double costOfPaintBallGun = 74.555;
         cf = NumberFormat.getCurrencyInstance();
         System.out.println("Cost of Arial-SV Paint Ball Gun: " + cf.format(costOfPaintBallGun));
     }
-
-
 
 
 }
---
+++ JAVA/JavagroupA/src/book2/chapter3/WeirdJava.java
@@ -0,0 +1,7 @@
+package book2.chapter3;
+
+public class WeirdJava {
+    public static void main(String[] args) {
+
+    }
+}
