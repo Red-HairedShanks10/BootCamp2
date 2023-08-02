349e2d64a4b77aa807afdc0e40065027316053fd
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 10:46:00 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/RoundFunctApp.java
+++ JAVA/JavagroupA/src/book2/chapter3/RoundFunctApp.java
@@ -2,4 +2,25 @@ package book2.chapter3;
 
 public class RoundFunctApp {
 
+    public static void main(String[] args) {
+        double x = 29.4;
+        double y =93.5;
+        double z = 19.3;
+
+
+        System.out.println("round(x): " +  Math.round(x));
+        System.out.println("round(x): " +  Math.round(x));
+        System.out.println("round(x): " +  Math.round(x));
+
+        System.out.println("round(x): " +  Math.round(x));
+        System.out.println("round(x): " +  Math.round(x));
+        System.out.println("round(x): " +  Math.round(x));
+
+        System.out.println("round(x): " +  Math.round(x));
+        System.out.println("round(x): " +  Math.round(x));
+        System.out.println("round(x): " +  Math.round(x));
+
+
+
+    }
 }
