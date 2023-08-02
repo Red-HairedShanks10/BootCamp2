d12bc06e8d6fa8b54c81b041b0b1f68835643035
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 10:06:01 +0200

    java commit

---
+++ JAVA/JavagroupA/src/book2/chapter3/CircleAreaApp.java
@@ -0,0 +1,7 @@
+package book2.chapter3;
+
+public class CircleAreaApp {
+    public static void main(String[] args) {
+
+    }
+}
--- JAVA/JavagroupA/src/book2/chapter3/MainClass.java
+++ JAVA/JavagroupA/src/book2/chapter3/MainClass.java
@@ -9,15 +9,15 @@ public class MainClass {
         int b = -a; // b is -5
         int c = -b; // c is +5
 
-        //unary plus doesnt do anything here...
+        //unary plus doesn't do anything here...
         int a2 = -5; // a is -5
         int b2 = +a2; // b is -5
         a2 = 5; // a is now 5
         int c2 = +a2; // c is 5
 
         //unary operators with complex expressions
-        int a = 3, b = 4, c = 5;
-        int d = a * -(b + c); // d is -27
+        int a3 = 3, b3 = 4, c3 = 5;
+        int d = a3 * -(b3 + c3); // d is -27
       /***************/
     }
 }
