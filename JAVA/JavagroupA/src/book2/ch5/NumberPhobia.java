6fb57df6a03e84dfccbfe98bc79cde3f164ced65
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 12:41:10 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/NumberPhobia.java
+++ JAVA/JavagroupA/src/book2/ch5/NumberPhobia.java
@@ -5,4 +5,13 @@ import java.util.Scanner;
 public class NumberPhobia {
     Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        int num = 2;
+        string input; //string input
+
+        while (true) {
+
+        }
+    }
+
 }
