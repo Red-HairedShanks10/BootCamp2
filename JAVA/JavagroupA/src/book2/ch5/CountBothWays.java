b9cbbfd8ca9e26922c2e1731613c0508b486171c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 21:40:30 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/CountBothWays.java
+++ JAVA/JavagroupA/src/book2/ch5/CountBothWays.java
@@ -4,7 +4,8 @@ package book2.ch5;
 public class CountBothWays {
     public static void main(String[] args) {
         int a, b;
-        for (a = 1, b = 10; a <= 10; a++, b--) {
+        // we can have multple count expressions and initialisations
+        for (a = 1, b = 10; a <= 10; a++, b--) {//what about setting up conditions when having multiple counter?
             System.out.println(a + " " + b);
         }
     }
