2d6f23177ceaa03f4814e1c843e1e998a1fb7267
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 07:13:24 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch2/EnumTest.java
+++ JAVA/JavagroupA/src/book2/ch2/EnumTest.java
@@ -4,8 +4,8 @@ public class EnumTest {
     public enum Blades {Naginata, Saber, Ngombe, Katana}
     public static void main(String[] args)
     {
-        Blades nagi;
-        nagi = Blades.Naginata;
+        Blades nagi;//declare the variable with the enum as the data type
+        nagi = Blades.Naginata;//can the enum variable bet set to only store values of the Blade data type?
         System.out.println("The blade is a " + nagi);
     }
 }
