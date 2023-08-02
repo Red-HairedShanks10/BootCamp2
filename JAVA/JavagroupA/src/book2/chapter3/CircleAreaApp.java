07c3ebc83386e7c79072dbe44a0818221dcc96df
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 10:07:30 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/CircleAreaApp.java
+++ JAVA/JavagroupA/src/book2/chapter3/CircleAreaApp.java
@@ -1,6 +1,9 @@
 package book2.chapter3;
 
+import java.util.Scanner;
+
 public class CircleAreaApp {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
     }
