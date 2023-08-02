a3bc9fade7d83161717d9db5601bd09148f65b04
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 12:29:31 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch4/MainClass.java
+++ JAVA/JavagroupA/src/book2/ch4/MainClass.java
@@ -4,6 +4,10 @@ public class MainClass {
     public static void main(String[] args) {
 
         String answer = "Yes";
-        if (answer = "Yes") System.out.println("The answer is Yes.");
+       // if (answer = "Yes") System.out.println("The answer is Yes.");
+
+        if (answer.equals("Yes")) {
+            System.out.println("The answer is Yes.");
+        }
     }
 }
