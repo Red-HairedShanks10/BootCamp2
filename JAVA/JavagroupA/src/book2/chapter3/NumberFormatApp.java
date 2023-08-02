68441c5e9d76828c77c475591b5e596690e632c6
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 11:12:51 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/NumberFormatApp.java
+++ JAVA/JavagroupA/src/book2/chapter3/NumberFormatApp.java
@@ -5,5 +5,13 @@ import java.text.NumberFormat;
 public class NumberFormatApp {
     static NumberFormat cf = NumberFormat.getCurrencyInstance();
 
+    public static void main(String[] args)
+    {
+        printMyAllowance();
+        printCostOfPaintBallGun();
+    }
+
+
+
 
 }
