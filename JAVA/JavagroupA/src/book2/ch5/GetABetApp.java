8d804f9013efbc48e9c6de23fbc20d2cfeaba124
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 13:14:10 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/GetABetApp.java
+++ JAVA/JavagroupA/src/book2/ch5/GetABetApp.java
@@ -9,6 +9,14 @@ public class GetABetApp {
         int bank = 1000; // assume the user has $1,000
         int bet; // the bet entered by the user
         System.out.println("You can bet between 1 and " + bank);
+
+        do
+        {
+           // System.out.print("Enter your bet: ");
+           // bet = sc.nextInt();
+        } while ( (bet <= 0) || (bet > bank) );
+        //System.out.println("Your money's good here.");
+    }
     }
 
 }
