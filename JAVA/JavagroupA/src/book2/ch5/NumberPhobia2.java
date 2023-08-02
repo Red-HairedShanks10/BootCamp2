7f627fb4a553d267ade4a34ce8bbb3dbcb601452
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 12:56:53 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/NumberPhobia2.java
+++ JAVA/JavagroupA/src/book2/ch5/NumberPhobia2.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 public class NumberPhobia2
 {
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args)
     {
         int number = 2;
         String input = "Y";
+
         while (input.equalsIgnoreCase("Y"))
         {
             System.out.println(number + " ");
-            System.out.print
-                    ("Do you want to keep counting?"
-                            + " (Y or N)");
+            System.out.print("Do you want to keep counting?" + " (Y or N)");
             input = sc.next();
             number += 2;
         }
-        System.out.println("\nWhew! That was close.");
+        System.out.println("YOU QUIT.");
     }
 }
