5a73d1fe090199644ea9ce435aab2f0c8a566470
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 21:33:43 +0200

    java commit

---
+++ JAVA/JavagroupA/src/book2/ch5/CountBothWays.java
@@ -0,0 +1,8 @@
+package book2.ch5;
+
+/*Using this file to try out multiple expressions within the for loop setup*/
+public class CountBothWays {
+    public static void main(String[] args) {
+
+    }
+}
