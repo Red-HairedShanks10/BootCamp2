854ebb6674292e11d32cb121aa4425c036907e79
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 21:31:29 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/LaunchControl.java
+++ JAVA/JavagroupA/src/book2/ch5/LaunchControl.java
@@ -1,8 +1,10 @@
 package book2.ch5;
-
+/*Counting down using a for loop*/
 public class LaunchControl {
     public static void main(String[] args) {
+
         System.out.print("We are go for launch in T minus ");
+
         for (int count = 10; count >= 0; count--) {
             if (count == 8) {
                 System.out.println("Ignition sequence start!");
