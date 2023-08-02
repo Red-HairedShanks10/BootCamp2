98c37bb3a25ffe3cb999440bbf7905a230992685
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 10:50:13 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/MainClass.java
+++ JAVA/JavagroupA/src/book2/chapter3/MainClass.java
@@ -1,4 +1,7 @@
 package book2.chapter3;
+
+import java.text.NumberFormat;
+
 /*Using this class to work on miscellaneous code*/
 public class MainClass {
     public static void main(String[] args) {
@@ -19,5 +22,10 @@ public class MainClass {
         int a3 = 3, b3 = 4, c3 = 5;
         int d = a3 * -(b3 + c3); // d is -27
       /***************/
+
+       double salesTax = 2.425;
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+
+
     }
 }
