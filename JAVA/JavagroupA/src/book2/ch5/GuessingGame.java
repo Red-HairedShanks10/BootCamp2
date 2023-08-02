2ec7503fe71ced7b5882395474bc5e45592294b5
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 22:41:08 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/GuessingGame.java
+++ JAVA/JavagroupA/src/book2/ch5/GuessingGame.java
@@ -18,6 +18,40 @@ public class GuessingGame {
             System.out.println("\nI'm thinking of a number " + "between 1 and 10.");
             System.out.print("What do you think it is? ");
 
-
+            do {
+                guess = sc.nextInt();
+                validInput = true;
+                if ((guess < 1) || (guess > 10)) {
+                    System.out.print
+                            ("I said, between 1 and 10. "
+                                    + "Try again: ");
+                    validInput = false;
+                }
+            }
+            while (!validInput);
+            // Check the guess
+            if (guess == number)
+                System.out.println("You're right!");
+            else
+                System.out.println("You're wrong! " +
+                        "The number was " + number);
+            // Play again?
+            do {
+                System.out.print("\nPlay again? (Y or N)");
+                answer = sc.next();
+                validInput = true;
+                if (answer.equalsIgnoreCase("Y")) ;
+                else if (answer.equalsIgnoreCase("N"))
+                    keepPlaying = false;
+                else
+                    validInput = false;
+            }
+            while (!validInput);
         }
+        System.out.println("\nThank you for playing!");
     }
+}
+
+
+
+
