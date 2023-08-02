7bb4d23b951097d31ebce10698211a74c20f693b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 22:38:01 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/GuessingGame.java
+++ JAVA/JavagroupA/src/book2/ch5/GuessingGame.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class GuessingGame {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        boolean keepPlaying = true;
+        System.out.println("Let's play a guessing game!");
+        while (keepPlaying)
+        {
+            boolean validInput;
+            int number, guess;
+            String answer;
+// Pick a random number
+
 
     }
 }
