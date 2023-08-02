a7daa84f8f43cb0a01d749f49d56932162907edb
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 09:57:29 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/MainClass.java
+++ JAVA/JavagroupA/src/book2/chapter3/MainClass.java
@@ -4,16 +4,20 @@ public class MainClass {
     public static void main(String[] args) {
 
 
-        //Unary plus and minus operators
+        /*Unary plus and minus operators*/
         int a = 5; // a is 5
         int b = -a; // b is -5
         int c = -b; // c is +5
 
-        //unary plus
+        //unary plus doesnt do anything here...
         int a2 = -5; // a is -5
         int b2 = +a2; // b is -5
         a2 = 5; // a is now 5
         int c2 = +a2; // c is 5
+
+        //unary operators with complex expressions
+        int a = 3, b = 4, c = 5;
+        int d = a * -(b + c); // d is -27
 
     }
 }
