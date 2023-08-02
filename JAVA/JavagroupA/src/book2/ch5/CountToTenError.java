4a575af7d830711eb55a98ae382f10ed1f492d86
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 20:31:50 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/CountToTenError.java
+++ JAVA/JavagroupA/src/book2/ch5/CountToTenError.java
@@ -1,13 +1,13 @@
 package book2.ch5;
 
+/*Testing the scope of the for loop iterator variable */
 public class CountToTenError {
     public static void main(String[] args) {
 
-        for (int i = 1; i <=10; i++){
-            System.out.println(i);
-
+        for (int i = 1; i <=10; i++){//when declared within the for initialisation stage of the for loop
+            System.out.println(i);//the iterator variable will be accessible within the loop body
         }
-        System.out.println("The final value of i is " + i);
+        System.out.println("The final value of i is " + i);//if used out of the loop then there will be errors
 
     }
 
