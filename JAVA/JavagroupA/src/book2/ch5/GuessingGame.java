d241b43213c7df5cf42eeb45917c49a17c8e3eae
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 22:37:07 +0200

    java commit

---
+++ JAVA/JavagroupA/src/book2/ch5/GuessingGame.java
@@ -0,0 +1,10 @@
+package book2.ch5;
+
+import java.util.Scanner;
+
+public class GuessingGame {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
--- JAVA/JavagroupA/src/book2/ch5/Matrix.java
+++ JAVA/JavagroupA/src/book2/ch5/Matrix.java
@@ -1,10 +1,16 @@
 package book2.ch5;
+/*Exploring nested for loops*/
 
 public class Matrix {
+
     public static void main(String[] args) {
-        for(int x = 1; x < 10; x++){
-            for (int y = 1; y < 10; y++) {
-                System.out.print(x + "-" + y + " ");
+
+        for(int x = 1; x < 10; x++){//in main loop we set up x iterations
+
+            //this for loop will be executed for each x iteration
+            for (int y = 1; y < 10; y++) {//set up y iteration as well
+                //at current y iteration
+                System.out.print(x + "-" + y + " ");//print out the x iteration num  and y iteration num
             }
             System.out.println();
         }
