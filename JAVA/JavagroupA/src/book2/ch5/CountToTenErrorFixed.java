813bd29af3753837bd77ddfd3837414a8bc099c7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 20:37:10 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/CountToTenErrorFixed.java
+++ JAVA/JavagroupA/src/book2/ch5/CountToTenErrorFixed.java
@@ -4,7 +4,7 @@ public class CountToTenErrorFixed {
     public static void main(String[] args) {
         int i;
         for (i = 1; i <=10; i++){
-
+            System.out.println(i);
         }
     }
 
