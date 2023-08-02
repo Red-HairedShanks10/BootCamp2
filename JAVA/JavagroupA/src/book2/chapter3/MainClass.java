8e718623bb4b642358e9adfad5533e0b904f9020
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 09:54:16 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/MainClass.java
+++ JAVA/JavagroupA/src/book2/chapter3/MainClass.java
@@ -3,5 +3,11 @@ package book2.chapter3;
 public class MainClass {
     public static void main(String[] args) {
 
+
+        //Unary plus and minus operators
+        int a = 5; // a is 5
+        int b = -a; // b is -5
+        int c = -b; // c is +5
+
     }
 }
