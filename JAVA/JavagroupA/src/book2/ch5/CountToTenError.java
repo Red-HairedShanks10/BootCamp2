2755bd535e104a47440cf5f32143f9bde7f32812
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 20:32:29 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/CountToTenError.java
+++ JAVA/JavagroupA/src/book2/ch5/CountToTenError.java
@@ -4,7 +4,7 @@ package book2.ch5;
 public class CountToTenError {
     public static void main(String[] args) {
 
-        for (int i = 1; i <=10; i++){//when declared within the for initialisation stage of the for loop
+        for (int i = 1; i <=10; i++){//when declared within the initialisation stage of the for loop
             System.out.println(i);//the iterator variable will be accessible within the loop body
         }
         System.out.println("The final value of i is " + i);//if used out of the loop then there will be errors
