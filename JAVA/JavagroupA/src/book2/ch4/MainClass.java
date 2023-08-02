399fab2708e196918edeb3ef22f4912d46dee0af
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 12:28:59 +0200

    java commit

---
+++ JAVA/JavagroupA/src/book2/ch4/MainClass.java
@@ -0,0 +1,9 @@
+package book2.ch4;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        String answer = "Yes";
+        if (answer = "Yes") System.out.println("The answer is Yes.");
+    }
+}
