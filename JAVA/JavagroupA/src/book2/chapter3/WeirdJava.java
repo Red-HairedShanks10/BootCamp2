269367ee81db32af6514ffd472f95506cdeee994
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 11:28:35 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/WeirdJava.java
+++ JAVA/JavagroupA/src/book2/chapter3/WeirdJava.java
@@ -1,11 +1,12 @@
 package book2.chapter3;
-
-
+/*Testing the weird and bad parts of Java numbers*/
 
 import java.text.NumberFormat;
 
 public class WeirdJava {
     public static void main(String[] args) {
+
+        //weird float things......
         float x = 0.1f;
         NumberFormat nf = NumberFormat.getNumberInstance();
         nf.setMinimumFractionDigits(10);
