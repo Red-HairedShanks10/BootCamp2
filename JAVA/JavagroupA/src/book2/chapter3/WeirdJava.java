0e74ce3d82ff77b268b60c51a4e7d5f3dbdd5b6d
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 11:27:02 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/WeirdJava.java
+++ JAVA/JavagroupA/src/book2/chapter3/WeirdJava.java
@@ -1,7 +1,14 @@
 package book2.chapter3;
 
+
+
+import java.text.NumberFormat;
+
 public class WeirdJava {
     public static void main(String[] args) {
-
+        float x = 0.1f;
+        NumberFormat nf = NumberFormat.getNumberInstance();
+        nf.setMinimumFractionDigits(10);
+        System.out.println(nf.format(x));
     }
 }
