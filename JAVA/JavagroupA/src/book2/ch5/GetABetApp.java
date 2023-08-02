ae6fc6d528a4fa59c37a75971ba23027ca8c9736
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 13:23:16 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/GetABetApp.java
+++ JAVA/JavagroupA/src/book2/ch5/GetABetApp.java
@@ -7,7 +7,7 @@ public class GetABetApp {
 
     public static void main(String[] args) {
 
-        double bank = 1000; // assume the user has $1,000
+        double bank = 1000; // assume the user has this amount
         double bet = 0; // the bet entered by the user
         System.out.println("You can bet between 1 and " + bank);
 
