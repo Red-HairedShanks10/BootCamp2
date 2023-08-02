558cfb12783e4844b42482dd24209647dc020be7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-02 10:11:01 +0200

    java commit

--- JAVA/JavagroupA/src/book2/chapter3/CircleAreaApp.java
+++ JAVA/JavagroupA/src/book2/chapter3/CircleAreaApp.java
@@ -9,6 +9,7 @@ public class CircleAreaApp {
         System.out.println("Welcome to the circle area calculator.");
         System.out.print("Enter the radius of your circle: ");
         double radius = sc.nextDouble();
-
+         double area = Math.PI * (radius * radius);
+         System.out.println("your area is: " + area);
     }
 }
