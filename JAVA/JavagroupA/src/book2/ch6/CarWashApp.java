369897dd603d15267913664319805e90d8e2a9a5
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 09:56:00 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch6/CarWashApp.java
+++ JAVA/JavagroupA/src/book2/ch6/CarWashApp.java
@@ -1,4 +1,39 @@
 package book2.ch6;
 
+import java.util.Scanner;
+
 public class CarWashApp {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("The spar application!\n\n");
+        System.out.print("Enter the package code: ");
+
+        String s = sc.next();
+        s.toLowerCase();
+        char p = s.charAt(0);
+        String details = "";
+
+        switch (p) {
+
+            case 'a':
+                details += "Massage";
+
+            case 'b':
+                details += "\tSauna relaxation ... \n";
+                break;
+
+            case 'c':
+                details += "\tPedicure ... \n";
+                break;
+
+            default:
+                details = "That's not one of the codes.";
+                break;
+        }
+        System.out.println("\nThat package includes: \n");
+        System.out.println(details);
+    }
+
 }
