cc4555871f05eb414fc4a90c8fb2999a2cff9f6c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 20:47:30 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch8/CrazyWithZeros.java
+++ JAVA/JavagroupA/src/book2/ch8/CrazyWithZeros.java
@@ -16,14 +16,14 @@ public class CrazyWithZeros {
     }
 
 
-    public static int divideTheseNumbers(int a, int b) →16
+    public static int divideTheseNumbers(int a, int b)
             throws Exception
     {
         int c;
         try
         {
-            c = a / b; →22
-            System.out.println("It worked!"); →23
+            c = a / b;
+            System.out.println("It worked!");
         }
 
 
