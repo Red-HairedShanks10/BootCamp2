0bfbfbfac6429d33858b6ce53da36b3815533e57
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 21:53:41 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch8/MyException.java
+++ JAVA/JavagroupA/src/book2/ch8/MyException.java
@@ -3,4 +3,21 @@ package book2.ch8;
 public class MyException {
 
 
+    public static void main(String[] args)
+    {
+        try
+        {
+            doSomething(true);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Exception!");
+        }
+    }
+    public static void doSomething(boolean t)
+            throws Exception
+    {
+        if (t)
+            throw new Exception();
+    }
 }
