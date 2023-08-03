024291ecd82d60b8547d0e0383f1c9d31171310c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 20:13:35 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch8/GetInteger2.java
+++ JAVA/JavagroupA/src/book2/ch8/GetInteger2.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class GetInteger2 {
     static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.print("Enter an integer: ");
+    }
 
 
 }
