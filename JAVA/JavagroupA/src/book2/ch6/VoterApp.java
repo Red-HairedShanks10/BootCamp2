b9ee9c18d009b1901796f0c79bd80820132210d5
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 08:52:00 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch6/VoterApp.java
+++ JAVA/JavagroupA/src/book2/ch6/VoterApp.java
@@ -1,5 +1,12 @@
 package book2.ch6;
 
+import java.util.Scanner;
+
 public class VoterApp {
 
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+    }
 }
