fcef777d434a6b2536253d141129cef25e0aacbd
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 08:53:46 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch6/VoterApp.java
+++ JAVA/JavagroupA/src/book2/ch6/VoterApp.java
@@ -7,6 +7,20 @@ public class VoterApp {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
+        System.out.println
+                ("Welcome to the voting machine "
+                        + "error code decoder.\n\n"
+                        + "If your voting machine generates "
+                        + "an error code,\n"
+                        + "you can use this program to determine "
+                        + "the exact\ncause of the error.\n");
+        System.out.print("Enter the error code: ");
+
+        int err = sc.nextInt();
+
+
+
+
 
     }
 }
