b8c32fa0bb3e4a27cfba6ccd9873ea1df91d1089
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 09:24:36 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch6/SalesApp.java
+++ JAVA/JavagroupA/src/book2/ch6/SalesApp.java
@@ -1,7 +1,34 @@
 package book2.ch6;
 
+import java.util.Scanner;
+
 public class SalesApp {
 
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double commissionRate;
+
+        String userClass =sc.nextLine();
+        char salesClass = userClass.charAt(0);
+
+        switch (salesClass)
+        {
+            case 1:
+                commissionRate = 0.02;
+                break;
+            case 2:
+                commissionRate = 0.035;
+                break;
+            case 3:
+                commissionRate = 0.05;
+                break;
+            default:
+                commissionRate = 0.0;
+                break;
+        }
+
+    }
 
 
 }
