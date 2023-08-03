ed7ef2af702bf1de4faaa9ceefc954f74b512d63
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 13:42:48 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch8/DividebyZero.java
+++ JAVA/JavagroupA/src/book2/ch8/DividebyZero.java
@@ -1,4 +1,18 @@
 package book2.ch8;
 
 public class DividebyZero {
+    public static void main(String[] args) {
+
+        try{
+            int a = 0, b = 10;
+            System.out.println(a/b);
+        }catch(ArithmeticException ex){
+            Sy
+
+        }
+
+
+    }
+
+
 }
