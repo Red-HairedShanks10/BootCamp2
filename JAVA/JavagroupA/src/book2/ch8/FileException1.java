718d7f049f635427dad1e5a3802ec54b7319e450
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 21:11:38 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch8/FileException1.java
+++ JAVA/JavagroupA/src/book2/ch8/FileException1.java
@@ -18,4 +18,16 @@ public class FileException1 {
 
 class FileException2 {
 
+    public static void main(String[] args) {
+        openFile("C:\test.txt");
+    }
+
+    public static void openFile(String fileName) {
+        try {
+            FileInputStream f = new FileInputStream(fileName);
+        }catch (Exception err){
+            System.out.println(err);
+        }
+    }
+
 }
