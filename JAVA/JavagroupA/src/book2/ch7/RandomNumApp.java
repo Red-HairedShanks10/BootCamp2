481e312f92192bf66480b6f6cfd4e15f48fe501d
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 10:30:28 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch7/RandomNumApp.java
+++ JAVA/JavagroupA/src/book2/ch7/RandomNumApp.java
@@ -4,8 +4,13 @@ public class RandomNumApp {
 
     public static void main(String[] args) {
 
-        int number = getNumber();
+        int number = getRandomNumber();
+        System.out.println(number);
 
     }
 
+    public static int getRandomNumber(){
+        int randomNumber = (int) Math.floor(Math.random() * 10 + 1 );
+        return randomNumber;
+    }
 }
