c69c2c3f7ee18f2791bca0f1534375da81fa9410
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 20:24:49 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch8/GetInterger.java
+++ JAVA/JavagroupA/src/book2/ch8/GetInterger.java
@@ -38,6 +38,22 @@ public class GetInterger {
 
 
 class GetInteger2 {
-
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        System.out.print("Enter an integer: ");
+        int i = GetAnInteger();
+        System.out.println("You entered " + i);
+    }
+    public static int GetAnInteger()
+    {
+        while (!sc.hasNextInt())
+        {
+            sc.nextLine();
+            System.out.print("That's not "
+                    + "an integer. Try again: ");
+        }
+        return sc.nextInt();
+    }
 
 }
