e8e0f4441b3bbca18fc7dafe33b510e704c46540
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 13:44:49 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch8/DividebyZero.java
+++ JAVA/JavagroupA/src/book2/ch8/DividebyZero.java
@@ -4,7 +4,7 @@ public class DividebyZero {
     public static void main(String[] args) {
 
         try{
-            int a = 0, b = 10;
+            int a = 10, b = 0;
             System.out.println(a/b);
         }catch(ArithmeticException ex){
             System.out.println("error: " + ex.getMessage());
