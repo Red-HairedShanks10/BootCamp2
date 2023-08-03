6ed691a46c24336bf4708d2ccd21208e0c993eb7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 07:34:28 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch5/GuessingGame.java
+++ JAVA/JavagroupA/src/book2/ch5/GuessingGame.java
@@ -25,9 +25,10 @@ public class GuessingGame {
                 if ((guess < 1) || (guess > 10)) {//make sure user guesses a number between 1-10
                     System.out.print
                             ("I said, between 1 and 10. " + "Try again: ");
-                    validInput = false;//set valid input
+                    validInput = false;//set valid input false
                 }
-            }
+            }//end of do while loop
+
             while (!validInput);
             // Check the guess
             if (guess == number)
