d07992950f8e5de3947ca0cb879ce4e739849c74
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-03 20:45:14 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch8/CrazyWithZeros.java
+++ JAVA/JavagroupA/src/book2/ch8/CrazyWithZeros.java
@@ -2,4 +2,16 @@ package book2.ch8;
 
 public class CrazyWithZeros {
 
+    public static void main(String[] args)
+    {
+        try
+        {
+            int answer = divideTheseNumbers(5, 0);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Tried twice, "
+                    + "still didn't work!");
+        }
+    }
 }
