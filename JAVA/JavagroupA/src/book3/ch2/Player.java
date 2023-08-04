faa24c9a087f259ef34012dd939c6d91fc38562e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 10:15:16 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/Player.java
+++ JAVA/JavagroupA/src/book3/ch2/Player.java
@@ -1,4 +1,15 @@
 package book3.ch2;
 
 public class Player {
+    private int health;
+    public int getHealth()
+    {
+        return health;
+    }
+    public void setHealth(int h)
+    {
+        health = h;
+    }
+}
+
 }
