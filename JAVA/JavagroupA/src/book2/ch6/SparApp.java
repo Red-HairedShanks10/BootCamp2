ccb921f47628cadd4c5d0d885ea319ba48a43e59
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 07:45:38 +0200

    java commit

--- JAVA/JavagroupA/src/book2/ch6/SparApp.java
+++ JAVA/JavagroupA/src/book2/ch6/SparApp.java
@@ -10,26 +10,26 @@ public class SparApp {
         System.out.println("The spar application!\n\n");
         System.out.print("Enter the package code: ");
 
-        String s = sc.next();
-        s.toLowerCase();
-        char p = s.charAt(0);
-        String details = "";
+        String s = sc.next();//get the user input
+        s.toLowerCase();//convert to lower case
+        char p = s.charAt(0);//get the one character of the String
+        String details = "";//leave details as an empty string...
 
-        switch (p) {
+        switch (p) {//use the p char value for each case
 
-            case 'a':
-                details += "Massage";
+            case 'a'://if char is (a)
+                details += "Massage";//display the package for option A
 
-            case 'b':
-                details += "\tSauna relaxation ... \n";
+            case 'b'://if b
+                details += "\tSauna relaxation ... \n";//display this package
                 break;
 
-            case 'c':
-                details += "\tPedicure ... \n";
+            case 'c'://if option c
+                details += "\tPedicure ... \n";//this package
                 break;
 
-            default:
-                details = "That's not one of the codes.";
+            default://if value is none of the cases above
+                details = "That's not one of the codes.";//display this message...
                 break;
         }
         System.out.println("\nThat package includes: \n");
