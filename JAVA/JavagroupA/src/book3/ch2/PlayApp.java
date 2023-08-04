864eac8d006c57c0ffca5e80275a3448c84b68db
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 10:57:33 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/PlayApp.java
+++ JAVA/JavagroupA/src/book3/ch2/PlayApp.java
@@ -5,7 +5,7 @@ public class PlayApp {
     public static void main(String[] args) {
         Player player1 = new Player();
         Player player2 = new Player();
-        player1.setHealth(10);
+        player1.setHealth(4);
         player2.setHealth(20);
         System.out.println("Player 1 has health at: " + player1.getHealth() );
         System.out.println("Player 2 has health at: " + player2.getHealth() );
