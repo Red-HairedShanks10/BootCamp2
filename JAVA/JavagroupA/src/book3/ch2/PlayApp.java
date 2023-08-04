4ed7ff7c646d7a904d201235541279595126e8e6
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 10:44:28 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/PlayApp.java
+++ JAVA/JavagroupA/src/book3/ch2/PlayApp.java
@@ -2,7 +2,13 @@ package book3.ch2;
 
 public class PlayApp {
 
-    Player player1 = new Player();
+    public static void main(String[] args) {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        player1.setHealth(10);
+
+
+    }
 
 
 }
