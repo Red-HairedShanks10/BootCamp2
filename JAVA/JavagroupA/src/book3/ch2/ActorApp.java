e076e12e078231afad646e9d602c384747a032fc
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 12:20:32 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/ActorApp.java
+++ JAVA/JavagroupA/src/book3/ch2/ActorApp.java
@@ -3,6 +3,7 @@ package book3.ch2;
 public class ActorApp {
 
     public static void main(String[] args) {
-
+        Actor actor1 = new Actor("Johnny", "Depp");
+        Actor actor2 = new Actor("Arnold", "From Terminator", false);
     }
 }
