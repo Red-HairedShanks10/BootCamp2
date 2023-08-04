6fcbf03d61223bca3268928c6c1fbc644c8b6f56
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 12:23:25 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/ActorApp.java
+++ JAVA/JavagroupA/src/book3/ch2/ActorApp.java
@@ -3,7 +3,9 @@ package book3.ch2;
 public class ActorApp {
 
     public static void main(String[] args) {
-        Actor actor1 = new Actor("Johnny", "Depp");
+        Actor actor1 = new Actor("Johnny", "Depp", true);
         Actor actor2 = new Actor("Arnold", "From Terminator", false);
+        System.out.println(actor1.firstName + "is a good: " + actor1.good);
+        System.out.println(actor2.firstName + actor2.lastName + "is a good: " + actor2.good);
     }
 }
