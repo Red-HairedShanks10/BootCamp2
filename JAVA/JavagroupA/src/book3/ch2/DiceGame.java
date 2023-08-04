c774bb7920e41f0b74e02b4b7635e4e9eace31ab
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 09:58:41 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/DiceGame.java
+++ JAVA/JavagroupA/src/book3/ch2/DiceGame.java
@@ -2,3 +2,18 @@ package book3.ch2;
 
 public class DiceGame {
 }
+public class DiceGame
+{
+    public static void main(String[] args)
+    {
+        Dice d = new Dice();
+        d.roll();
+    }
+}
+public class Dice
+{
+    public void roll()
+    {
+// code that rolls the dice goes here
+    }
+}
