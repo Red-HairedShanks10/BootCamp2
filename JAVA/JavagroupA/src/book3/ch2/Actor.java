e4f817bba7aa4562e10432b294d5d8a2a00313e4
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 12:17:45 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/Actor.java
+++ JAVA/JavagroupA/src/book3/ch2/Actor.java
@@ -1,5 +1,20 @@
 package book3.ch2;
 
 public class Actor {
+String firstName, lastName;
+boolean good, great;
+
+public Actor(String cfirstName, String clastName){
+
+    this.firstName = cfirstName;
+    this.lastName = clastName;
+}
+
+public Actor(String cfirstName, String clastName, boolean cGood){
+    this.firstName = cfirstName;
+    this.lastName = clastName;
+    this.good = cGood;
+}
+
 
 }
