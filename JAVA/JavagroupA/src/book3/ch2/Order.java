3578518d006a6a180736c5b68488b49c7645c9b3
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 11:06:56 +0200

    java commit

---
+++ JAVA/JavagroupA/src/book3/ch2/Order.java
@@ -0,0 +1,18 @@
+package book3.ch2;
+
+public class Order {
+
+    double unitPrice, quantityOrdered;
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getQuantityOrdered() {
+        return quantityOrdered;
+    }
+}
--- JAVA/JavagroupA/src/book3/ch2/OrderApp.java
+++ JAVA/JavagroupA/src/book3/ch2/OrderApp.java
@@ -1,9 +1,5 @@
 package book3.ch2;
 
 public class OrderApp {
-    double unitPrice, quantityOrdered;
 
-    public double getQuantityOrdered() {
-        return quantityOrdered;
-    }
 }
