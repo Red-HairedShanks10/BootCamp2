def39ec1397b9043e7e72c8d7ae9f1e287a3c976
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 11:27:01 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/OrderApp.java
+++ JAVA/JavagroupA/src/book3/ch2/OrderApp.java
@@ -1,10 +1,13 @@
 package book3.ch2;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
+
 
 public class OrderApp {
 
    static Scanner sc = new Scanner(System.in);
+   static NumberFormat cf = NumberFormat.getCurrencyInstance();
     public static void main(String[] args) {
 
         System.out.println("Enter quantity of product");
@@ -12,7 +15,8 @@ public class OrderApp {
         Order order1 = new Order();
         order1.setQuantityOrdered(qtyOrder);
         order1.setUnitPrice(20);
-        System.out.println(order1.getOrderTotal());
+        System.out.println("Total cost of your order: " + cf.format(order1.getOrderTotal()));
+
     }
 
 }
