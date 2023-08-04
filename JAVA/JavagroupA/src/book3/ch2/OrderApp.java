8cc0abb8902db33960a556af8d3cd6a25be18f52
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-04 11:18:05 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/OrderApp.java
+++ JAVA/JavagroupA/src/book3/ch2/OrderApp.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class OrderApp {
 
-    Scanner sc = new Scanner(System.in);
+   static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
+        System.out.println("Enter quantity of product");
+        double qtyOrder = sc.nextDouble();
         Order order1 = new Order();
-        order1.setQuantityOrdered(2);
+        order1.setQuantityOrdered(qtyOrder);
         order1.setUnitPrice(20);
         System.out.println(order1.getOrderTotal());
     }
