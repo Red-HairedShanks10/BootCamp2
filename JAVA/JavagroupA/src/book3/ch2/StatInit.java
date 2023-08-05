3e1b38a43b72238f8962568a44ad1237a136e531
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-05 21:10:39 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/StatInit.java
+++ JAVA/JavagroupA/src/book3/ch2/StatInit.java
@@ -3,7 +3,7 @@ package book3.ch2;
 import java.util.Scanner;
 
 public class StatInit {
-    Scanner sc = new Scanner(System.in);
+//    Scanner sc = new Scanner(System.in);
     public int x;
     {
         System.out.print(
