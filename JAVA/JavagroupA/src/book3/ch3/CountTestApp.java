ddc7fb44c97aa4055ca06a158d3d93d9376c182c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-06 16:43:40 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch3/CountTestApp.java
+++ JAVA/JavagroupA/src/book3/ch3/CountTestApp.java
@@ -4,6 +4,7 @@ public class CountTestApp {
     public static void main(String[] args){
         printCount();
         for (int i = 0; i < 10; i++){
+            CountTest c1 = new CountTest();
 
         }
     }
