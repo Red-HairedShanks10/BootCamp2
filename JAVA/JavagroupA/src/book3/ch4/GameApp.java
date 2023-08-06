04271f024aa245cd9c2585cc47d15ccb26a8a49b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-06 19:55:59 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch4/GameApp.java
+++ JAVA/JavagroupA/src/book3/ch4/GameApp.java
@@ -4,6 +4,7 @@ public class GameApp {
     public static void main(String[] args) {
 
         Chess chess = new Chess();
-        chess.play();
+        //calling the overridden method of the Game class from child class-->Chess
+        chess.play();//will call chess.play() if it exist//otherwise it will grab from super class
     }
 }
