e3cd911646caf888c8abb4752be45efaae2d29bd
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-07 10:29:21 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch4/Ball.java
+++ JAVA/JavagroupA/src/book3/ch4/Ball.java
@@ -68,6 +68,6 @@ class BaseBall extends Ball{
 class SoftBall extends Ball{
     public SoftBall(double softBallW){
         super(softBallW);//this calls the base class's constructor//whether implicit ot explicit
-        System.out.println("BaseBall constructor weight: " + softBallW);
+        System.out.println("Softball constructor weight: " + softBallW);
     }
 }
