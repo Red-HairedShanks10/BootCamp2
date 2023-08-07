0189091d11978538e95356561c97b83a06586ae6
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-07 21:15:36 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch4/TicTacToeApp.java
+++ JAVA/JavagroupA/src/book3/ch4/TicTacToeApp.java
@@ -5,5 +5,6 @@ public class TicTacToeApp {
     public static void main(String[] args){
         Player p1 = new Player();
         Player p2 = new BetterPlayer();
+        playTheGame(p1, p2);
     }
 }
