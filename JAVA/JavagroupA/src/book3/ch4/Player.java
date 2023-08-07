7aa035969ba8931530650c3601ca1668b11ec9a5
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-07 21:07:43 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch4/Player.java
+++ JAVA/JavagroupA/src/book3/ch4/Player.java
@@ -4,8 +4,7 @@ public class Player {
 
     public int move(){
         for (int i = 0; i < 9; i++){
-            System.out.println(
-                    "\nThe basic player says:");
+            System.out.println("\nThe basic player says:");
             System.out.println(
                     "I'll take the first open square!");
             return firstOpenSquare();
