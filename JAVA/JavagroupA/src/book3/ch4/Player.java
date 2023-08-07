abe140579808102cac44249a46ec26da43bf6f10
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-07 21:05:39 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch4/Player.java
+++ JAVA/JavagroupA/src/book3/ch4/Player.java
@@ -4,7 +4,8 @@ public class Player {
 
     public int move(){
         for (int i = 0; i < 9; i++){
-
+            System.out.println(
+                    "\nThe basic player says:");
         }
 
     }
