20a77df70784cfb90bfa6ad96c6a577f3a6186bc
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-08 10:45:05 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch5/TicTacToe.java
+++ JAVA/JavagroupA/src/book3/ch5/TicTacToe.java
@@ -1,6 +1,6 @@
 package book3.ch5;
 
-public class TicTacToe implements Playable, CardGame {
+public class TicTacToe extends Game implements Playable, CardGame {
     @Override
     public void play() {
         System.out.println("playing a Tic-Tac-Toe game");
