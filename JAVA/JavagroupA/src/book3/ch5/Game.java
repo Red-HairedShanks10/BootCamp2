06653bf9572ee694db2128202889a3efaae1478c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-08 13:30:43 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch5/Game.java
+++ JAVA/JavagroupA/src/book3/ch5/Game.java
@@ -1,16 +1,14 @@
 package book3.ch5;
 
-public abstract class Game implements Playable {
+public class Game implements Playable {
 
     @Override
-    public abstract void play();
-
-
-
-    @Override
-    public abstract void quit();
+    public void play(){
+        System.out.println("Playing....");
+    };
 
 
 }
+
 
 
--- JAVA/JavagroupA/src/book3/ch5/TestLambdaCollection.java
+++ JAVA/JavagroupA/src/book3/ch5/TestLambdaCollection.java
@@ -3,7 +3,17 @@ package book3.ch5;
 public class TestLambdaCollection {
     public static void main(String[] args)
     {
-        Game g = new Game();
+        Game g = new Game() {
+            @Override
+            public void play() {
+
+            }
+
+            @Override
+            public void quit() {
+                System.out.println();
+            }
+        };
         g.play();
         g.quit();
     }
