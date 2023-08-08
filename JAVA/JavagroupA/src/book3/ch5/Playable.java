ffc2bb5dd31e79174cb4911e09f2228a2f36b5ec
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-08 13:30:57 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch5/Playable.java
+++ JAVA/JavagroupA/src/book3/ch5/Playable.java
@@ -3,6 +3,6 @@ package book3.ch5;
 public interface Playable {
     void play();
     default void quit(){
-        System.out.println("Sorry, quitting is not allowed.");
+        System.out.println("Quitting is not allowed.");
     };
 }
