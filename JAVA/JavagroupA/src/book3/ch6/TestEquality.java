685646211aa77c344f9889a7038541e7c727aa76
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-08 14:04:35 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch6/TestEquality.java
+++ JAVA/JavagroupA/src/book3/ch6/TestEquality.java
@@ -2,6 +2,7 @@ package book3.ch6;
 
 public class TestEquality {
     public static void main(String[] args) {
-        Employee emp1 =
+        Employee emp1 = new Employee("Marco", "Red");
+        Employee emp2 = new Employee("Leo", "Goat");
     }
 }
