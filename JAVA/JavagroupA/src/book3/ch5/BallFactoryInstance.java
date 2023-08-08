b10e4c23c1deb43edc5cd7837ac634307e57ccc8
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-08 09:41:18 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch5/BallFactoryInstance.java
+++ JAVA/JavagroupA/src/book3/ch5/BallFactoryInstance.java
@@ -7,6 +7,6 @@ public class BallFactoryInstance {
                    return  new Baseball();
         }else if (b.equalsIgnoreCase("SoftBall")){
             return new SoftBall();
-        }
+        }return null;
     }
 }
