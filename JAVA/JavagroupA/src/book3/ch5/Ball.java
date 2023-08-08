5cf945040b54813a242ace4c050fe97ecec49786
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-08 09:23:48 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch5/Ball.java
+++ JAVA/JavagroupA/src/book3/ch5/Ball.java
@@ -1,14 +1,15 @@
 package book3.ch5;
 
-public  abstract class Ball {
+public abstract class Ball {
     public abstract int hit(int batSpeed);
 }
 
-abstract class Baseball extends Ball{
+abstract class Baseball extends Ball {
 
-    public int hit(int batSpeed){
-int x = 100;
-batSpeed = batSpeed * x;
+    public int hit(int batSpeed) {
+        int x = 100;
+        batSpeed = batSpeed * x;
+        return batSpeed;
     }
 }
 
