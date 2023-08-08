950aa4508c17656a0865707190c6234cf1d71603
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-08 20:26:53 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch6/CloneTest.java
+++ JAVA/JavagroupA/src/book3/ch6/CloneTest.java
@@ -1,5 +1,8 @@
 package book3.ch6;
 
 public class CloneTest {
-
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(
+                "Martinez", "Anthony");
+    }
 }
