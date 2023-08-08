6b4d181d8ce12722035a4735ad9871286bf8b45d
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-08 20:27:23 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch6/CloneTest.java
+++ JAVA/JavagroupA/src/book3/ch6/CloneTest.java
@@ -2,7 +2,8 @@ package book3.ch6;
 
 public class CloneTest {
     public static void main(String[] args) {
-        Employee emp1 = new Employee(
-                "Martinez", "Anthony");
+        Employee emp1 = new Employee("Martinez", "Anthony");
+        emp1.setSalary(40000.0);
+
     }
 }
