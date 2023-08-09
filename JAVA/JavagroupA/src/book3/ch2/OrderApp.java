50dd91b0b38e1de5350b05a5eb6db23c50fdb2b7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-09 22:12:57 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch2/OrderApp.java
+++ JAVA/JavagroupA/src/book3/ch2/OrderApp.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class OrderApp {
 
-   static Scanner sc = new Scanner(System.in);
+   static Scanner sc = new Scanner(System.in);//
    static NumberFormat cf = NumberFormat.getCurrencyInstance();
     public static void main(String[] args) {
 
