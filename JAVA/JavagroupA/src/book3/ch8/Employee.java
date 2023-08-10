163e7fdbe00fadcd46667aa2e27c3729c7906882
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 11:53:21 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch8/Employee.java
+++ JAVA/JavagroupA/src/book3/ch8/Employee.java
@@ -1,2 +1,16 @@
 package book3.ch8;
 
+/** Represents an employee.
+ * @author Doug Lowe
+ * @author www.LoweWriter.com
+ * @version 1.5
+ * @since 1.0
+ */
+public class Employee
+{
+    private String lastName;
+    private String firstName;
+    private Double salary;
+    /** Represents the employee's address.
+     */
+    public Address address;
