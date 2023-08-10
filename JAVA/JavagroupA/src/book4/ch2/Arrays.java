fd77731c4cf8a880ce716319bbc2d42a77341f51
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 13:48:24 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch2/Arrays.java
+++ JAVA/JavagroupA/src/book4/ch2/Arrays.java
@@ -1,12 +1,19 @@
 package book4.ch2;
 
-public class Arrays {
+import java.util.Scanner;
 
+public class Arrays {
+ static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         String[] firstArray = new String[5];///declaring an array
-        firstArray[0] = "Black Mamba";
-        System.out.println(firstArray[0]);
+       //irstArray[0] = "Black Mamba";
+      //System.out.println(firstArray[0]);
+
+        System.out.print("How many players? ");
+        int count = sc.nextInt(); // sc is a Scanner
+        String[] players = new String[count];
+        System.out.println(players.length);
     }
 
 }
