e5d45f57a1698873a0c747efc2d6e9b8831390dd
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 13:51:50 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch2/Arrays.java
+++ JAVA/JavagroupA/src/book4/ch2/Arrays.java
@@ -9,7 +9,7 @@ public class Arrays {
         String[] firstArray = new String[5];///declaring an array
        firstArray[0] = "Black Mamba";
        firstArray[1] = "King Cobra";
-
+        firstArray[2] = "Taipan";
       //System.out.println(firstArray[0]);
 
         /*
