057007aec06182e7ae18cc9ac47aac938518cd91
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 13:50:06 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch2/Arrays.java
+++ JAVA/JavagroupA/src/book4/ch2/Arrays.java
@@ -10,10 +10,11 @@ public class Arrays {
        //irstArray[0] = "Black Mamba";
       //System.out.println(firstArray[0]);
 
+        /*
         System.out.print("How many players? ");
         int count = sc.nextInt(); // sc is a Scanner
         String[] players = new String[count];
-        System.out.println(players.length);
+        System.out.println(players.length); */
     }
 
 }
