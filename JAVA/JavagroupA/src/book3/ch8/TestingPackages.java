d08232de12198be48f5b37bf7464b199fe8765aa
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 10:12:43 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch8/TestingPackages.java
+++ JAVA/JavagroupA/src/book3/ch8/TestingPackages.java
@@ -5,7 +5,7 @@ import com.gmail.sheryldreyer.util.Console;
 import java.util.ArrayList;
 
 public class TestingPackages {
-    ArrayList array1 = new ArrayList();//if we have imported the package we can do this.....
+    //ArrayList array1 = new ArrayList();//if we have imported the package we can do this.....
    while (Console.askYorN("Keep going?"))
     {
         System.out.println("D'oh!");
