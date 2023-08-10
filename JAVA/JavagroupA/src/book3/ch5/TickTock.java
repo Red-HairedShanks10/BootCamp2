d759411368b2d55dda894b6b411161e6fed0b3d4
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 09:04:33 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch5/TickTock.java
+++ JAVA/JavagroupA/src/book3/ch5/TickTock.java
@@ -2,7 +2,7 @@ package book3.ch5;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class TickTock {//
+public class TickTock {
     public static void main(String[] args) {
 
         Timer t = new Timer(1000, new Ticker());
