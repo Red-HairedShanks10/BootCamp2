5bff053e2310b3f334f371f5514a2a8ee41fc354
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 13:44:15 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch2/Arrays.java
+++ JAVA/JavagroupA/src/book4/ch2/Arrays.java
@@ -6,6 +6,7 @@ public class Arrays {
     public static void main(String[] args) {
         String[] firstArray = new String[5];///declaring an array
         firstArray[0] = "Black Mamba";
+        System.out.println(firstArray[0]);
     }
 
 }
