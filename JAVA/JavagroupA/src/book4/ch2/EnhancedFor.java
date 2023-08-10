ec3713248285c71707756db98b62dd5e609c2f92
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 15:00:55 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch2/EnhancedFor.java
+++ JAVA/JavagroupA/src/book4/ch2/EnhancedFor.java
@@ -2,6 +2,6 @@ package book4.ch2;
 
 public class EnhancedFor {
     public static void main(String[] args) {
-        String[] days = {"Sunday", "Monday","Tuesday","Wednesday","Thursday",}
+        String[] days = {"Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday",}
     }
 }
