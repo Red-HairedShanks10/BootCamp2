9115518245bf5f229f30f4511ca398a88e101f66
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 15:02:01 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch2/EnhancedFor.java
+++ JAVA/JavagroupA/src/book4/ch2/EnhancedFor.java
@@ -4,7 +4,7 @@ public class EnhancedFor {
     public static void main(String[] args) {
         String[] days = {"Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday",}
                 for (String d: days){
-                    
+                    System.out.println(d);
                 }
     }
 }
