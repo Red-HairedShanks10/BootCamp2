52426ee5b1df6ffbaa3f22da15f275cc4d44e3f8
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 15:06:47 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch2/EnhancedFor.java
+++ JAVA/JavagroupA/src/book4/ch2/EnhancedFor.java
@@ -3,6 +3,7 @@ package book4.ch2;
 public class EnhancedFor {
     public static void main(String[] args) {
 
+        String[] daysOfWeek = getDaysOfWeek();
         for (String d: days){
             System.out.println(d);
         }
