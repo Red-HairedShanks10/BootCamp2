9b0f573f27e174eec1bf6b0fdb6bdd2cc9b5b360
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 13:00:12 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch1/StringBuilderApp.java
+++ JAVA/JavagroupA/src/book4/ch1/StringBuilderApp.java
@@ -6,6 +6,7 @@ public class StringBuilderApp {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.print("Enter a string: ");
 
     }
 }
