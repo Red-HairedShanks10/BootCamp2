031a1129a0882817b64bc7f22f98e46bfe446fee
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 13:42:32 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch2/Arrays.java
+++ JAVA/JavagroupA/src/book4/ch2/Arrays.java
@@ -1,10 +1,11 @@
 package book4.ch2;
 
 public class Arrays {
-    String[] firstArray;///declaring an array
+
 
     public static void main(String[] args) {
-        
+        String[] firstArray;///declaring an array
+        firstArray
     }
 
 }
