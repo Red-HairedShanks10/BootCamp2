cddc1bb8bded154a3fc152bd1396e42c0c2804c9
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 12:03:30 +0200

    java commit

--- JAVA/JavagroupA/src/com/gmail/marco/util/Payroll/Address.java
+++ JAVA/JavagroupA/src/com/gmail/marco/util/Payroll/Address.java
@@ -1,5 +1,10 @@
 package com.gmail.marco.util.Payroll;
-
+/** Represents an adress of employee.
+ * @author Doug Lowe
+ * @author www.LoweWriter.com
+ * @version 1.5
+ * @since 1.0
+ */
 public class Address implements Cloneable {
     public String street;
     public String city;
