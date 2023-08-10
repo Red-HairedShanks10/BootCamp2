70a8a4fe8ba530e6db0940b6f72c2713ea6cbdcf
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 10:01:35 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch8/TestingPackages.java
+++ JAVA/JavagroupA/src/book3/ch8/TestingPackages.java
@@ -3,5 +3,5 @@ package book3.ch8;
 import java.util.ArrayList;
 
 public class TestingPackages {
-    ArrayList array1 = new ArrayList();//if we have imported the package
+    ArrayList array1 = new ArrayList();//if we have imported the package we can do this.....
 }
