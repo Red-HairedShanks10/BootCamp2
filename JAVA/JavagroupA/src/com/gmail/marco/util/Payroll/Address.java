6a4e4e253c707a409ef4a4443936af1ad24333d7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 12:02:45 +0200

    java commit

---
+++ JAVA/JavagroupA/src/com/gmail/marco/util/Payroll/Address.java
@@ -0,0 +1,8 @@
+package com.gmail.marco.util.Payroll;
+
+public class Address implements Cloneable {
+    public String street;
+    public String city;
+    public String state;
+    public String zipCode;
+}
--- JAVA/JavagroupA/src/com/gmail/marco/util/Payroll/Employee.java
+++ JAVA/JavagroupA/src/com/gmail/marco/util/Payroll/Employee.java
@@ -82,10 +82,3 @@ public class Employee {
         this.salary = salary;
     }
 }
-class Address implements Cloneable
-{
-    public String street;
-    public String city;
-    public String state;
-    public String zipCode;
-}
