b5446776c981126c3f718cd1d108d454a0e15258
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-10 15:23:38 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch2/TwoDimensionalArray.java
+++ JAVA/JavagroupA/src/book4/ch2/TwoDimensionalArray.java
@@ -2,6 +2,18 @@ package book4.ch2;
 
 public class TwoDimensionalArray {
     public static void main(String[] args) {
-
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        System.out.println("\tNorth\t\tSouth\t\tEast\t\tWest");
+        int year = 2004;
+        for (int y = 0; y < 5; y++)
+        {
+            System.out.print(year + "\t");
+            for (int region = 0; region < 4; region++){
+                System.out.print(cf.format(sales[y][region]));
+                System.out.print("\t");
+            }
+            year++;
+            System.out.println();
+        }
     }
 }
