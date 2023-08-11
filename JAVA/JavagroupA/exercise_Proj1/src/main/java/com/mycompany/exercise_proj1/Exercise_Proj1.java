8e2d17ea04472b89e69a319aa1397f39f5223e97
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-11 11:16:03 +0200

    java commit

---
+++ JAVA/JavagroupA/exercise_Proj1/src/main/java/com/mycompany/exercise_proj1/Exercise_Proj1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercise_proj1;
+
+/**
+ *
+ * @author Marco
+ */
+public class Exercise_Proj1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
