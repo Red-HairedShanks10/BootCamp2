38402ac1fbd6272b61096ea87a216cbf08fe1b23
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-19 19:51:41 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
+++ JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
@@ -12,9 +12,9 @@ public class ArrayListClass {
         names.add(2, "Kamo");//specifying the position of this element value
         System.out.println(names);
     ///using regular loop//to process names arrayList
-        for (int i = 0; i < names.size(); i++){
+     /*   for (int i = 0; i < names.size(); i++){
             System.out.println("this person is: " + names.get(i));
-        }
+        }*/
 
         ///using enhanced for loop
         for(Object n : names ){
