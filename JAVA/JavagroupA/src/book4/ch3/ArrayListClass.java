abbb9aed262c909f60694599f348e88c43f97a18
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-19 19:33:03 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
+++ JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class ArrayListClass {
     public static void main(String[] args) {
         ArrayList names = new ArrayList();
-        names.add("Drink Pepsi");
+        names.add("Marco");
+        names.add("Marco");
+
     }
 }
