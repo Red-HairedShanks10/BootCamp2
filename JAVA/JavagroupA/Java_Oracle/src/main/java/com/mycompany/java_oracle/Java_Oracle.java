aa801bc3e8c7794d5fb3b69a069631745b1ca8ed
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-21 15:00:25 +0200

    java commit

---
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/com/mycompany/java_oracle/Java_Oracle.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_oracle;
+
+/**
+ *
+ * @author Marco
+ */
+public class Java_Oracle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
