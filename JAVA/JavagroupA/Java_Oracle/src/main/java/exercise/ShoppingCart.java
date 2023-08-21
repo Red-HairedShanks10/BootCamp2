c5c7ed2827c1c1beb1dba7e43ce547619b5ba0f2
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-21 15:15:10 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ShoppingCart.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ShoppingCart.java
@@ -2,6 +2,8 @@ package exercise;
 
 public class ShoppingCart {
     public static void main (String[] args){
-        System.out.println("Welcome to the Shopping Cart!");    
+
+        System.out.println("Welcome to the Shopping Cart!");
+
     }    
 }
