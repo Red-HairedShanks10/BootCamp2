af836e1d50acec36d78bfacea991417e30f24e59
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-22 09:27:41 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch5/ShoppingCart5_3.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch5/ShoppingCart5_3.java
@@ -10,7 +10,7 @@ public class ShoppingCart5_3 {
 
         double price = 139.99;
         int quantity = 3;
-        double tax = 1.04;
+        double tax = 1.34;
        
         String items[];
         items = new String[4];
