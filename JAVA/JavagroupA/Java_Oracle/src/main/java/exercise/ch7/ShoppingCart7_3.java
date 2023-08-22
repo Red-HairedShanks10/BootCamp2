5f138b4a62b3d1861255a58f72fa5a8770521200
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-22 09:59:46 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch7/ShoppingCart7_3.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch7/ShoppingCart7_3.java
@@ -8,7 +8,7 @@ public class ShoppingCart7_3 {
         
 	//  Declare and initialize variables of type long, float, and char.
         
-        long long1 = 7000000000L;
+        long long1 = 7_000_000_000L;
         float flt1 = 18.43F;
         char ch1 = 'G';
         
