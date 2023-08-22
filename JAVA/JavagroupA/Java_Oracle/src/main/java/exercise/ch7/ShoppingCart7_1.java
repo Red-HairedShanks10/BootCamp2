4e18fb7db8f105be5cfc34a2a9952a34167c1439
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-22 09:48:05 +0200

    java commit

---
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch7/ShoppingCart7_1.java
@@ -0,0 +1,17 @@
+
+package exercise.ch7;//7-1
+
+public class ShoppingCart7_1 {
+    public static void main (String[] args){
+        String custName = "Steve Smith";
+        String firstName;
+        int spaceIdx;
+        
+        // Get the index of the space character (" ") in custName.
+        spaceIdx = custName.indexOf(" ");
+
+	// Use the substring method to parse out the first name and print it.
+        firstName = custName.substring(0, spaceIdx);
+        System.out.println(firstName);
+    }
+}
