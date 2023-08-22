10c3a24e9b7fa61c830617f262a5afbf75451665
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-22 09:33:52 +0200

    java commit

---
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch6/Item.java
@@ -0,0 +1,13 @@
+
+package exercise.ch6;
+
+public class Item {
+
+        public int itemID;
+        public String desc;
+        public double price;
+        public int quantity;
+        
+}
+
+
