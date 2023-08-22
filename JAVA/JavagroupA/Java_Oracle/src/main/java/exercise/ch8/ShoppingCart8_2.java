17e2a175f0da19b1e9392eb0781805bc1e94c1e7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-22 10:20:46 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch8/ShoppingCart8_2.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch8/ShoppingCart8_2.java
@@ -6,7 +6,7 @@ public class ShoppingCart8_2 {
     public static void main(String[] args) {
         Item8_2 item1 = new Item8_2();
 
-        // Call the 3-arg setItemFields method and then call displayItem.
+        // Call the 3-arg setItemFields method and displayItem.
         item1.setItemFields("Gaming PC", 1, 134.95);
         item1.displayItem();
 
