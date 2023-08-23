898e069bb1874e6c815a962899dd610b73bd3dae
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-23 09:45:24 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch9/ShoppingCart.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch9/ShoppingCart.java
@@ -6,7 +6,7 @@ public class ShoppingCart {
     public static void main(String args[]){
 
         // Declare, instantiate, and initialize a Customer object
-        Customer9_1 cust1 = new Customer9_1("Bob Miller", "555-44-3212");
+        Customer9_1 cust1 = new Customer9_1("Marco", "555-44-3212");
 
 	// Print the customer object name
         System.out.println("Customer name: "+cust1.getName());
