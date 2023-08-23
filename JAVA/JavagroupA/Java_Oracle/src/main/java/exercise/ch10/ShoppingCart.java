525a51a81eaa9632372632dd54e63b952fb50c37
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-23 10:09:40 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch10/ShoppingCart.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch10/ShoppingCart.java
@@ -3,7 +3,7 @@ package exercise.ch10;
 
 public class ShoppingCart {
     public static void main(String args[]){
-        Order order = new Order("Marco", 950.00, "JH", Order.NONPROFIT);
-        System.out.println("Discount is: "+ order.getDiscount());
+        Order order1 = new Order("Marco", 950.00, "JH", Order.NONPROFIT);
+        System.out.println("For " + order1.name + " Discount is: "+ order1.getDiscount());
     }
 }
