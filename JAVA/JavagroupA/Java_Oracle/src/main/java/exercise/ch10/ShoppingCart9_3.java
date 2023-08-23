4c53c65b93bdef7df9010ef0e0dd66338b408f79
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-23 10:35:26 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch10/ShoppingCart9_3.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch10/ShoppingCart9_3.java
@@ -11,7 +11,7 @@ public class ShoppingCart9_3 {
          Order9_3 order2 = new Order9_3("Eze", 840.00, "WC", Order9_3.PRIVATE);//order instance
         System.out.println("For " + order2.name + " Discount is: "+ order2.getDiscount());//display customer's discount
         
-        Order9_3 order3 = new Order9_3("Zeke", 460.00, "EC", Order9_3.CORP);
+        Order9_3 order3 = new Order9_3("Zeke", 460.00, "EC", Order9_3.CORP);//order instance
         System.out.println("For " + order3.name + " Discount is: "+ order3.getDiscount());//display customer's discount
     }
 }
