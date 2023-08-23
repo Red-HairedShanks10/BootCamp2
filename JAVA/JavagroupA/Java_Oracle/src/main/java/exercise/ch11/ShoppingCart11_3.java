8949ca0978ce77727b27adc45144fc00ed496ddd
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-23 10:54:18 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch11/ShoppingCart11_3.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch11/ShoppingCart11_3.java
@@ -2,7 +2,7 @@
 package exercise.ch11;
 
 public class ShoppingCart11_3 {
-    Item11_3[] items = {new Item11_3("Shirt",25.60), 
+    Item11_3[] items = {new Item11_3("Phone",55.60), 
                     new Item11_3("WristBand",1.00), 
                     new Item11_3("Pants",35.99)};
     
