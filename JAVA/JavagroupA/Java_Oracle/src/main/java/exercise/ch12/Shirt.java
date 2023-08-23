04c1c1bd31ba4e32f0f2247ea6ae434003903af7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-23 11:14:40 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch12/Shirt.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch12/Shirt.java
@@ -5,7 +5,7 @@ public class Shirt extends Item12_1{
     private char colorCode;
     
     public Shirt(double price, char size, char colorCode){
-        super ("Shirt", price);
+        super ("Shirt", price);//calling Item constructor
         this.size = size;
         this.colorCode = colorCode;
     } 
