84b3dcda110e0aa5c3c67823cf90b9d871e8628b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-23 11:30:03 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch12/ShoppingCart12_2.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch12/ShoppingCart12_2.java
@@ -4,7 +4,7 @@ public class ShoppingCart12_2{
 
     public static void main(String[] args) {
         // instantiate a Shirt object and call its display method
-        Shirt12_2 shirt2 = new Shirt12_2(25.99, 'M', 'P');
+        Shirt12_2 shirt2 = new Shirt12_2(29.95, 'L', 'R');
         shirt2.display();  
     }
 }
