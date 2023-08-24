f428cf38a8fbab7366428793c478013368e1d0c8
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-24 08:45:58 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch12/ShoppingCart12_3.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch12/ShoppingCart12_3.java
@@ -4,7 +4,7 @@ public class ShoppingCart12_3 {
     public static void main(String[] args) {
          // declare and instantiate a Shirt object using an Item reference type
         Item12_3 item = new Shirt12_3(35.95, 'L', 'R');
-        
+        Item12_3 item2 = new Shirt12_3(25.95, 'M', 'Y');
         
         // call the display method on the object, then the getColor method
         item.display();
