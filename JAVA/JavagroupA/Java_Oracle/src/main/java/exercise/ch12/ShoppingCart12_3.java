282ebfda2b6d2470b166b72265a6fd9b98231cf9
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-24 08:27:22 +0200

    java commit

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch12/ShoppingCart12_3.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch12/ShoppingCart12_3.java
@@ -8,6 +8,7 @@ public class ShoppingCart12_3 {
         
         // call the display method on the object, then the getColor method
         item.display();
+         if (item instanceof Shirt) {
 
     }
 }
