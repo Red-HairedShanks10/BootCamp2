3227c3baf9a634e7c17e3626ed6a6f2e8eb510c5
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-28 21:54:17 +0200

    java exercises

--- JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch13/ShoppingCart.java
+++ JAVA/JavagroupA/Java_Oracle/src/main/java/exercise/ch13/ShoppingCart.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class ShoppingCart {
-    ArrayList<Item> items = new ArrayList();
+    ArrayList<Item13_2> items = new ArrayList();
     
     public static void main(String[] args){
         ShoppingCart cart = new ShoppingCart();
