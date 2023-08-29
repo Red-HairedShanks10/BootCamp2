ddebccbcf5876e1edd06a82eb110064bedbb013f
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-29 12:03:13 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
+++ JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
@@ -11,6 +11,7 @@ public class ArrayListClass {
 
         //adding workers:
         workers.add(emp1);
+        workers.add(emp2);
         System.out.println("current employees: " + workers);
        // names.add("Marco");
         names.add("Muzi");
