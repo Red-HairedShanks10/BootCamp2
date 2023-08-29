53493272dc473f23f625243096744fc2a7951f93
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-29 11:56:11 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
+++ JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
@@ -34,9 +34,9 @@ public class ArrayListClass {
         }*/
 
         //updating values/elements
-        names.set(0, "ludo");
+       /* names.set(0, "ludo");
         names.set(2, "Drako");
-        System.out.println("new names: " + names);
+        System.out.println("new names: " + names);*/
 
 
     }
