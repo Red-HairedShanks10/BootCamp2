08d1bcb3b71270813e81b7ff2ef4aa2738e1dbfc
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-30 10:39:51 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
+++ JAVA/JavagroupA/src/book4/ch3/ArrayListClass.java
@@ -28,8 +28,8 @@ public class ArrayListClass {
         //emps2
         emps2.add(emp2);
         emps2.add(emp3);
-        emps1.removeAll(emps2);//removeAll
-        System.out.println(emps1);
+        emps1.removeAll(emps2);//removeAll, removes the two elements in emps2 that are also in emps1
+        System.out.println(emps1);//
         emps1.retainAll(emps2);
         System.out.println(emps1);
 
