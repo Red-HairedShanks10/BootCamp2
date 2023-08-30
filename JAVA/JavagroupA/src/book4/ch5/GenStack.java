2293712642b8e259911c20d2c6dffa4c99f1d279
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-30 12:43:28 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch5/GenStack.java
+++ JAVA/JavagroupA/src/book4/ch5/GenStack.java
@@ -3,10 +3,15 @@ package book4.ch5;
 public class GenStack<E> {
     private LinkedList<E> list = new LinkedList<E>();
 
-    public void push(E item){
+    /*public void push(E item){
 
         list.addFirst(item);
+    }*/
+    public void push(E item){
+
+        list.push(item);
     }
+
 
     public E pop(){
 
