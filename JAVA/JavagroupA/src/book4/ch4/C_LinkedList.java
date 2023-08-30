45cb9263678711ace00881690650c9193df1a8dc
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-30 11:04:54 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch4/C_LinkedList.java
+++ JAVA/JavagroupA/src/book4/ch4/C_LinkedList.java
@@ -5,5 +5,7 @@ import java.util.LinkedList;
 public class C_LinkedList {
     public static void main(String[] args) {
         LinkedList<String> officers = new LinkedList<>();
+
+        officers.add("Blake");
     }
 }
