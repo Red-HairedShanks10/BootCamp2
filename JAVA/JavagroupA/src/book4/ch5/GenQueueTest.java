68923534440f051d59e4e875739002924b684bb8
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-30 13:13:18 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch5/GenQueueTest.java
+++ JAVA/JavagroupA/src/book4/ch5/GenQueueTest.java
@@ -1,5 +1,8 @@
 package book4.ch5;
 
+import book3.ch4.HourlyEmployee;
+import com.gmail.marco.Payroll.Employee;
+
 public class GenQueueTest {
     public static void main(String[] args) {
         GenQueue<Employee> empList;
