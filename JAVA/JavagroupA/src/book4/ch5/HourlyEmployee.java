9b23b34a076d5292279a5f10b813a64056de23d1
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-30 13:19:51 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch5/HourlyEmployee.java
+++ JAVA/JavagroupA/src/book4/ch5/HourlyEmployee.java
@@ -1,20 +1,10 @@
 package book4.ch5;
 
-import book3.ch4.EmployeeApp;
 
-import java.text.NumberFormat;
+public class HourlyEmployee extends Employee{
 
-public class HourlyEmployee extends EmployeeApp {
-double rate;
 
-public HourlyEmployee(double rate){
-this.rate = rate;
-}
-
-    public void getFormattedRate(){
-        NumberFormat hf = NumberFormat.getPercentInstance();
-        msg = hf.format(this.rate);
-        System.out.println("the rate is: " + msg);
-}
-
+    public HourlyEmployee(String lastName, String firstName) {
+        super(lastName, firstName);
+    }
 }
