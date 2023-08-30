04bf0e6c76bc2e126f9dda7b2102bea4a744fa92
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-30 11:04:11 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch4/C_LinkedList.java
+++ JAVA/JavagroupA/src/book4/ch4/C_LinkedList.java
@@ -1,7 +1,9 @@
 package book4.ch4;
 
+import java.util.LinkedList;
+
 public class C_LinkedList {
     public static void main(String[] args) {
-
+        LinkedList<String> officers = new LinkedList<>();
     }
 }
