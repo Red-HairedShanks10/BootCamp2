14414862cb388654a484d8ba6df4f24a6b6dfb20
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-30 11:31:26 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch4/C_LinkedList.java
+++ JAVA/JavagroupA/src/book4/ch4/C_LinkedList.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public class C_LinkedList {
     public static void main(String[] args) {
         LinkedList<String> officers = new LinkedList<>();
-        String Rico = "Rico";
+        String Rico = " Brigadier General Rico";
         officers.add("Lieutenant General Blake");
         officers.add("General Marco");
         officers.add("Captain Tumijin");
@@ -14,6 +14,7 @@ public class C_LinkedList {
         officers.add(2,"Sergeant Major Tuttle");
         System.out.println("commissioned officers: " + officers );
        String removeOfficer = officers.set(2,"Murdock");
+       officers.add(5,Rico);//removing with a reference of a string value
         System.out.println("commissioned officers(replacement): " + officers );
         //addFirst
         /*officers.addFirst("Lieutenant General Blake");
