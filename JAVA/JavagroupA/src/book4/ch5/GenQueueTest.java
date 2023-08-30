7414bc334203a5ebdd43994ac3a037eb9e8ef2ff
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-30 13:12:14 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch5/GenQueueTest.java
+++ JAVA/JavagroupA/src/book4/ch5/GenQueueTest.java
@@ -3,6 +3,6 @@ package book4.ch5;
 public class GenQueueTest {
     public static void main(String[] args) {
         GenQueue<Employee> empList;
-
+        empList = new GenQueue<Employee>();
     }
 }
