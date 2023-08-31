aed149a1571dfe0696f167e790a2aa2fe16ccc03
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 13:06:14 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch6/Spell.java
+++ JAVA/JavagroupA/src/book4/ch6/Spell.java
@@ -54,12 +54,12 @@ public class Spell {
                 .filter(s -> s.name.toLowerCase().startsWith("a"))
                 .forEach(s -> System.out.println(s) );
 */
-       double Total = spells.stream()
+       /*double Total = spells.stream()
                //.filter(s -> s.type == SpellType.SPELL)
                 //.filter(s -> s.name.toLowerCase() //.startsWith("a"))
                 .mapToDouble(s -> s.price )
                 .sum();
-       System.out.println("total price for all Spells is: " + Total);
+       System.out.println("total price for all Spells is: " + Total);*/
 
 /*        double Spellcount = spells.stream()
                 //.filter(s -> s.type == SpellType.SPELL)
@@ -73,8 +73,9 @@ public class Spell {
         OptionalDouble avg = spells.stream()
                 .mapToDouble(s -> s.price)
                 .average();
+                int avgC = (int) avg.getAsDouble();
                 if (avg.isPresent()){
-                    System.out.println("average price for all Spells is: " + avg);
+                    System.out.println("average price for all Spells is: " + avgC);
                 }
 
     }
