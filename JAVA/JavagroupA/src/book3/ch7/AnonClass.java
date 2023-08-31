f5fe7238c0e3b53214d51dbdcece83e68f33bf3b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 10:35:36 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch7/AnonClass.java
+++ JAVA/JavagroupA/src/book3/ch7/AnonClass.java
@@ -3,7 +3,7 @@ package book3.ch7;
 public class AnonClass {
     public static void main(String[] args){
 
-        Ball b = () ->  System.out.println("you hit it");
+        Ball b = () ->  System.out.println("you hit it");//lambda expression
 /*        Ball b = new Ball(){
             public void hit(){
                 System.out.println("you hit it");
