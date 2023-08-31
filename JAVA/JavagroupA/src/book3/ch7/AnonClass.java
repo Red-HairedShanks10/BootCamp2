e82c172d4e8fa834223f2a588956c50d785e5911
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 10:30:57 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch7/AnonClass.java
+++ JAVA/JavagroupA/src/book3/ch7/AnonClass.java
@@ -2,6 +2,9 @@ package book3.ch7;
 
 public class AnonClass {
     public static void main(String[] args){
-        Ball b = new Ball()
+        Ball b = new Ball(){
+            public void hit()
+        }
+
     }
 }
