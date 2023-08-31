c4eb584a73b6b040aa629a333900f4be6595b245
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 12:19:48 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch6/Spell.java
+++ JAVA/JavagroupA/src/book4/ch6/Spell.java
@@ -48,6 +48,7 @@ public class Spell {
         //spells.stream().forEach(s -> System.out.println(s) );
 
         spells.stream().filter(s -> s.type == SpellType.SPELL)
+                .filter(s -> s.name.toLowerCase().startsWith("a"))
                 .forEach(s -> System.out.println(s) );
     }
 
