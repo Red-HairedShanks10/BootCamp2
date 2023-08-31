f915b4cc14c0fa3f1d95028457e82666b4a2f1de
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 12:08:29 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch6/Spell.java
+++ JAVA/JavagroupA/src/book4/ch6/Spell.java
@@ -25,14 +25,17 @@ public class Spell {
 
     public static void main(String[] args) {
         ArrayList<Spell> spells = new ArrayList<>();
+
+        spells.add(new Spell("Amateratsu", Spell.SpellType.SPELL, "Produces black flames."));
+        spells.add(new Spell("Spirit-Gun", Spell.SpellType.SPELL,"fires heavy spirit energy into a single beam"));
+        spells.add(new Spell("Koto-amatsukami", Spell.SpellType.SPELL,"Projects Illusions that can inflict physical damage"));
         spells.add(new Spell("Aparecium", Spell.SpellType.SPELL, "Makes invisible ink appear."));
         spells.add(new Spell("Avis", Spell.SpellType.SPELL,"Launches birds from your wand."));
         spells.add(new Spell("Engorgio", Spell.SpellType.CHARM, "Enlarges something."));
         spells.add(new Spell("Fidelius", Spell.SpellType.CHARM, "Hides a secret within someone."));
         spells.add(new Spell("Finite Incatatum", Spell.SpellType.SPELL, "Stops all current spells."));
         spells.add(new Spell("Locomotor Mortis", Spell.SpellType.CURSE, "Locks an opponent's legs."));
-        spells.add(new Spell("Amateratsu", Spell.SpellType.SPELL, "Produces black flames."));
-        spells.add(new Spell("Spirit-Gun", Spell.SpellType.SPELL,"fires heavy spirit energy into a single beam"));
+
 
         /*for (Spell spell : spells){
             System.out.println(spell.name);
