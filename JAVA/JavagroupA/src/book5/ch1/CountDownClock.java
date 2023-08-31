e8e67560259c53f8f69a0137a93b34a8c35f8314
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 13:57:29 +0200

    java commit

--- JAVA/JavagroupA/src/book5/ch1/CountDownClock.java
+++ JAVA/JavagroupA/src/book5/ch1/CountDownClock.java
@@ -5,7 +5,7 @@ public class CountDownClock extends Thread{
     public void run(){
         for (int t = 20; t > 0; t--){
             System.out.println("T minus " + t);
-            if (t == 0){
+            if (t == 1){
                 System.out.println("Operation rescue Mark from another planet,under way");
             }
             try
