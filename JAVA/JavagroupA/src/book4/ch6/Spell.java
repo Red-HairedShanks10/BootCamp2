2c6d3699784b91e999a99e83c4d5fb6c29e5d086
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 12:14:25 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch6/Spell.java
+++ JAVA/JavagroupA/src/book4/ch6/Spell.java
@@ -22,8 +22,8 @@ public class Spell {
         return "Spell:" +
                 "name: " + name + '\'' +
                 ", type: " + type +
-                ", description: {'" + description + '}'+'\'' +
-                ' ';
+                ", description: {'" + description + '\'' +
+                '}';
     }
 
 
