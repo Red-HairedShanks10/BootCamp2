b85c1f4f737cd774848871ae2d0aa15a95c722ba
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 11:57:58 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch6/Spell.java
+++ JAVA/JavagroupA/src/book4/ch6/Spell.java
@@ -21,9 +21,13 @@ public class Spell {
         return this.name;
     }
 
+    public static void main(String[] args) {
+        ArrayList<Spell> spells = new ArrayList<>();
+        spells.add(new Spell("Aparecium", Spell.SpellType.SPELL, "Makes invisible ink appear."));
+        spells.add(new Spell("Avis", Spell.SpellType.SPELL,"Launches birds from your wand."));
+        spells.add(new Spell("Engorgio", Spell.SpellType.CHARM, "Enlarges something."));
+        spells.add(new Spell("Fidelius", Spell.SpellType.CHARM, Hides a secret within someone."));
+    }
 
-    ArrayList<Spell> spells = new ArrayList<>();
-    spells.add(new Spell("Aparecium", Spell.SpellType.SPELL, "Makes invisible ink appear."));
-    spells.add(new Spell("Avis", Spell.SpellType.SPELL,"Launches birds from your wand."));
-    spells.add(new Spell("Engorgio", Spell.SpellType.CHARM, "Enlarges something."));
+
 }
