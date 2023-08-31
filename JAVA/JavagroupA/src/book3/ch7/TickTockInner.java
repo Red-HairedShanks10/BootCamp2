c02e7870c61c78e3ba4bc71fc0a3574bc98e450a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 11:08:41 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch7/TickTockInner.java
+++ JAVA/JavagroupA/src/book3/ch7/TickTockInner.java
@@ -2,6 +2,6 @@ package book3.ch7;
 import java.awt.event.*;
 import javax.swing.*;
 public class TickTockInner {
-    private String tickMessage = "Tick..."; →6
+    private String tickMessage = "Tick...";
     private String tockMessage = "Tock...";
 }
