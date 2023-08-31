6c967d6cb1509bd2003700864a1af33b039f87f7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 12:43:25 +0200

    java commit

--- JAVA/JavagroupA/src/book4/ch6/Spell.java
+++ JAVA/JavagroupA/src/book4/ch6/Spell.java
@@ -49,10 +49,10 @@ public class Spell {
 
         //spells.stream().forEach(s -> System.out.println(s) );
 
-        spells.stream().filter(s -> s.type == SpellType.SPELL)
+       /* spells.stream().filter(s -> s.type == SpellType.SPELL)
                 .filter(s -> s.name.toLowerCase().startsWith("a"))
                 .forEach(s -> System.out.println(s) );
-
+*/
        double Total = spells.stream().filter(s -> s.type == SpellType.SPELL)
                 //.filter(s -> s.name.toLowerCase() //.startsWith("a"))
                 .mapToDouble(s -> s.price )
