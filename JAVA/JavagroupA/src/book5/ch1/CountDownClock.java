c78ff56d2c33caa66f20a0e5ca818d49086dc22b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 13:50:08 +0200

    java commit

--- JAVA/JavagroupA/src/book5/ch1/CountDownClock.java
+++ JAVA/JavagroupA/src/book5/ch1/CountDownClock.java
@@ -1,4 +1,16 @@
 package book5.ch1;
 
-public class CountDownClock {
+public class CountDownClock extends Thread{
+
+    public void run(){
+        for (int t = 20; t > 0; t--){
+            System.out.println("T minus " + t);
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {}
+        }
+    }
 }
