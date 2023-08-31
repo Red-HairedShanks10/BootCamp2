aa816eb7d6e71a83ad21f5170b1743088b94629c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 10:39:56 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch7/AnonClass.java
+++ JAVA/JavagroupA/src/book3/ch7/AnonClass.java
@@ -3,7 +3,11 @@ package book3.ch7;
 public class AnonClass {
     public static void main(String[] args){
         //lambda expression//can have more than one statement in it
-        Ball b = () ->  {System.out.println("you hit it"); System.out.println("congrats..."); };
+        Ball b = () ->  {
+                        //statement one...
+                        System.out.println("you hit it");
+                        //statement two...
+                         System.out.println("congrats..."); };
 /*        Ball b = new Ball(){
             public void hit(){
                 System.out.println("you hit it");
