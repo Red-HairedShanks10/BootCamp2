750a32d42b9ef317da47606e8db47b0579485203
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-08-31 10:34:59 +0200

    java commit

--- JAVA/JavagroupA/src/book3/ch7/AnonClass.java
+++ JAVA/JavagroupA/src/book3/ch7/AnonClass.java
@@ -2,6 +2,8 @@ package book3.ch7;
 
 public class AnonClass {
     public static void main(String[] args){
+
+        Ball b = () ->  System.out.println("you hit it");
         Ball b = new Ball(){
             public void hit(){
                 System.out.println("you hit it");
