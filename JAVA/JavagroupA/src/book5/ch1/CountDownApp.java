de3710a152d42a620b6425fb74acd599249cf8a9
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-03 16:01:04 +0200

    java commit

--- JAVA/JavagroupA/src/book5/ch1/CountDownApp.java
+++ JAVA/JavagroupA/src/book5/ch1/CountDownApp.java
@@ -6,6 +6,7 @@ public class CountDownApp {
         Thread clock = new CountDownClock();
         Runnable flood, ignition, liftoff;
         flood = new LaunchEvent(16, "Flood the pad!");
+        ignition = new LaunchEvent(6, "Start engines!");
         clock.start();
     }
 }
