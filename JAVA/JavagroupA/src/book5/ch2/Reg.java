768606cd9ef9a71cde1a7c69422d44c2b3394711
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-03 14:47:11 +0200

    java commit

--- JAVA/JavagroupA/src/book5/ch2/Reg.java
+++ JAVA/JavagroupA/src/book5/ch2/Reg.java
@@ -34,7 +34,9 @@ public class Reg {
                     doneMatching = true;
                 }else {
                     matcher = pattern.matcher(s);
-                    if (matcher.matches())
+                    if (matcher.matches()){
+                        System.out.println("Match");
+                    }
                 }
             }
         }
