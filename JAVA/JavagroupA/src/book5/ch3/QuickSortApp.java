2f9fe3b7d833af5ef958f3962811ae025e362d0a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-05 16:01:44 +0200

    java commit

--- JAVA/JavagroupA/src/book5/ch3/QuickSortApp.java
+++ JAVA/JavagroupA/src/book5/ch3/QuickSortApp.java
@@ -16,7 +16,9 @@ public class QuickSortApp {
 
         private static void printArray(int[] array){
             System.out.println();
+            for (int i = 0; i < array.length; i++){
 
+            }
         }
 
 
