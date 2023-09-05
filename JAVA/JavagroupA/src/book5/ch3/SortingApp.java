5132cf35a07995357ef51cdf34c6825646e982d6
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-05 15:33:48 +0200

    java commit

--- JAVA/JavagroupA/src/book5/ch3/SortingApp.java
+++ JAVA/JavagroupA/src/book5/ch3/SortingApp.java
@@ -3,4 +3,12 @@ package book5.ch3;
 public class SortingApp {
 
 
+    public static void sort(int low, int high)
+    {
+        if (low >= high)
+            return;
+        int p = partition(low, high);
+        sort (low, p);
+        sort (p+1, high);
+    }
 }
