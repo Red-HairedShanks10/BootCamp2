ff741bb56d6b31c2b444f902e4097c59b1871e17
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-05 17:59:24 +0200

    java commit

--- JAVA/JavagroupA/src/book5/ch4/TimeTester.java
+++ JAVA/JavagroupA/src/book5/ch4/TimeTester.java
@@ -1,7 +1,10 @@
 package book5.ch4;
 
+import java.time.*;
+
 public class TimeTester {
     public static void main(String[] args){
-
+        System.out.println("\nLocalTime: "
+                + LocalTime.now().toString());
     }
 }
