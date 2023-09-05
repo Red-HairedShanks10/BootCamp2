9d70740f547b0df7b050e79142a9db23bf129d4d
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-05 16:02:52 +0200

    java commit

--- JAVA/JavagroupA/src/book5/ch3/QuickSortApp.java
+++ JAVA/JavagroupA/src/book5/ch3/QuickSortApp.java
@@ -2,18 +2,19 @@ package book5.ch3;
 
 public class QuickSortApp {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int LEN = 100;
         int[] unsorted = new int[LEN];
-        for (int i = 0; i<LEN; i++){
-            unsorted[i] = (int)(Math.random() * 100) + 1;
+        for (int i = 0; i < LEN; i++) {
+            unsorted[i] = (int) (Math.random() * 100) + 1;
             System.out.println("Unsorted array:");
             printArray(unsorted);
             int[] sorted = sort(unsorted);
             System.out.println("\n\nSorted array:");
             printArray(sorted);
         }
-
+        ;
+    }
         private static void printArray(int[] array){
             System.out.println();
             for (int i = 0; i < array.length; i++){
@@ -28,5 +29,5 @@ public class QuickSortApp {
 
 
 
-    }
+
 }
