f0fced3717cbb4a17eed6f275bdde43fc31edac9
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-05 19:01:58 +0200

    java commit

--- JAVA/JavagroupA/src/book5/ch4/FormatDateTime.java
+++ JAVA/JavagroupA/src/book5/ch4/FormatDateTime.java
@@ -3,5 +3,14 @@ import java.util.*;
 import java.time.*;
 import java.time.format.*;
 public class FormatDateTime {
-
+    public static void main(String[] args)
+    {
+        LocalDateTime now = LocalDateTime.now();
+        printDate(now, "YYYY-MM-dd");
+        printDate(now, "MM-dd-YYYY");
+        printDate(now, "dd MMM YYYY");
+        printDate(now, "MMMM d, YYYY");
+        printDate(now, "HH:mm");
+        printDate(now, "h:mm a");
+    }
 }
