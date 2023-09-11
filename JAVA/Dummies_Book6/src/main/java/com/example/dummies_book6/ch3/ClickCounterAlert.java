311e0c0afbffdd957cc137b6cea94b208e6427f5
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 18:41:42 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch3/ClickCounterAlert.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch3/ClickCounterAlert.java
@@ -1,2 +1,23 @@
 package com.example.dummies_book6.ch3;
 
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+public class ClickCounterAlert extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    Button btn;
+    Label lbl;
+    int iClickCount = 0;
+    @Override public void start(Stage primaryStage)
+    {
+// Create the button
+        btn = new Button();
+        btn.setText("Click me please!");
+        btn.setOnAction(e -> buttonClick());
