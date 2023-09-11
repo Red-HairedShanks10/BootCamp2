5d2ea94d0efa404adc7e51b5ff6b38090cefbf42
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 18:24:38 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract4.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract4.java
@@ -1,3 +1,17 @@
 package com.example.dummies_book6.ch2;
 
 
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
+public class AddSubtract4 extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    Button btnAdd;
+    Button btnSubtract;
