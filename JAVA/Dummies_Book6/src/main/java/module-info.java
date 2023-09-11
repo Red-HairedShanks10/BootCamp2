b1542dd75f742e2c724a5a4d34c83ebe16c1e7c8
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 08:51:59 +0200

    JFX commit

---
+++ JAVA/Dummies_Book6/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module com.example.dummies_book6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.dummies_book6 to javafx.fxml;
+    exports com.example.dummies_book6;
+    exports com.example.dummies_book6.ch1;
+    opens com.example.dummies_book6.ch1 to javafx.fxml;
+    exports com.example.dummies_book6.ch2;
+    opens com.example.dummies_book6.ch2 to javafx.fxml;
+    exports com.example.dummies_book6.ch3;
+    opens com.example.dummies_book6.ch3 to javafx.fxml;
+    exports com.example.dummies_book6.ch4;
+    opens com.example.dummies_book6.ch4 to javafx.fxml;
+    exports com.example.dummies_book6.ch5;
+    opens com.example.dummies_book6.ch5 to javafx.fxml;
+    exports com.example.dummies_book6.ch6;
+    opens com.example.dummies_book6.ch6 to javafx.fxml;
+}
