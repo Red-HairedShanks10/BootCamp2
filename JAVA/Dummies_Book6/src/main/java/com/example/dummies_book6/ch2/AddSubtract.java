96d14901045f859309460659d6f42cb5ee76b36a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 17:57:37 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract.java
@@ -59,6 +59,7 @@ public class AddSubtract extends Application implements EventHandler <ActionEven
         {
             iCounter--;
         }
+        lbl.setText(Integer.toString(iCounter));
     }
 
 
