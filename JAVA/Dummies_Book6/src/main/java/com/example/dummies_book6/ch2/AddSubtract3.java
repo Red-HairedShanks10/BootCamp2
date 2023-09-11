a72f9cfa0664b291124e85da055c5d44cfead491
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 18:18:07 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract3.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract3.java
@@ -1,3 +1,35 @@
 package com.example.dummies_book6.ch2;
 
-
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
+public class AddSubtract3 extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    Button btnAdd;
+    Button btnSubtract;
+    Label lbl;
+    int iCounter = 0;
+    @Override public void start(Stage primaryStage)
+    {
+// Create the Add button
+        btnAdd = new Button();
+        btnAdd.setText("Add");
+        btnAdd.setOnAction(
+                new EventHandler<ActionEvent>() →26
+        {
+            public void handle(ActionEvent e) →28
+            {
+                iCounter++; →30
+                lbl.setText(Integer.toString(iCounter));
+            }
+        } );
+// Create the Subtract button
+        btnSubtract = new Button();
+        btnSubtract.setText("Subtract");
