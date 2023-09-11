a1eac788b2c619a146f9f12b6c2c05ae7ff7a6dd
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 18:27:31 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract4.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract4.java
@@ -43,7 +43,7 @@ public class AddSubtract4 extends Application
         // Add the layout pane to a scene
         Scene scene = new Scene(pane, 200, 75);
         // Add the scene to the stage, set the title
-// and show the stage
+        // and show the stage
         primaryStage.setScene(scene);
         primaryStage.setTitle("Add/Sub");
         primaryStage.show();
