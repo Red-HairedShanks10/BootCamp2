9774d449183e34cd75fdb7d1f5a952e967677d27
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 18:43:20 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch3/ClickCounterAlert.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch3/ClickCounterAlert.java
@@ -25,9 +25,9 @@ public class ClickCounterAlert extends Application
         // Add the button to a layout pane
         BorderPane pane = new BorderPane();
         pane.setCenter(btn);
-// Add the layout pane to a scene
+        // Add the layout pane to a scene
         Scene scene = new Scene(pane, 250, 150);
-// Add the scene to the stage, set the title
+        // Add the scene to the stage, set the title
 // and show the stage
         primaryStage.setScene(scene);
         primaryStage.setTitle("Click Counter");
