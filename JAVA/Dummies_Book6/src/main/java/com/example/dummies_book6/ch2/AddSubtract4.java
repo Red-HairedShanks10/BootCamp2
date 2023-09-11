0686fa32c54450404709892f2c01cf161bc89746
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 18:25:33 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract4.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract4.java
@@ -17,3 +17,37 @@ public class AddSubtract4 extends Application
     Button btnSubtract;
     Label lbl;
     int iCounter = 0;
+
+    @Override public void start(Stage primaryStage)
+    {
+// Create the Add button
+        btnAdd = new Button();
+        btnAdd.setText("Add");
+        btnAdd.setOnAction( e -> →25
+        {
+            iCounter++;
+            lbl.setText(Integer.toString(iCounter));
+        } );
+// Create the Subtract button
+        btnSubtract = new Button();
+        btnSubtract.setText("Subtract");
+        btnSubtract.setOnAction( e -> →34
+        {
+            iCounter--;
+            lbl.setText(Integer.toString(iCounter));
+        } );
+// Create the Label
+        lbl = new Label();
+        lbl.setText(Integer.toString(iCounter));
+// Add the buttons and label to an HBox pane
+        HBox pane = new HBox(10);
+        pane.getChildren().addAll(lbl, btnAdd, btnSubtract);
+// Add the layout pane to a scene
+        Scene scene = new Scene(pane, 200, 75);
+// Add the scene to the stage, set the title
+// and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Add/Sub");
+        primaryStage.show();
+    }
+}
