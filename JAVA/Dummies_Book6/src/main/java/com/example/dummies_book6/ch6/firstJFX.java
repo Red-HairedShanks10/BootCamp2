9914853ac2da31c7ac3b179991edf720b3f7beb0
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 08:49:39 +0200

    JFX commint

---
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch6/firstJFX.java
@@ -0,0 +1,25 @@
+package com.example.dummies_book6.ch6;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class firstJFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane pane = new GridPane();
+        Scene scene = new Scene(pane, 320, 240);
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
