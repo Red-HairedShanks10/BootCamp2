da19a1ba6f1af0d12ae4f675d44c5c0fbfb9597b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 18:11:00 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract2.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract2.java
@@ -9,59 +9,23 @@ import javafx.event.*;
 
 public class AddSubtract2 extends Application →8
         {
-public static void main(String[]args)
+public static void main(String[] args)
         {
         launch(args);
         }
         Button btnAdd;
         Button btnSubtract;
         Label lbl;
-        int iCounter=0;
+        int iCounter = 0;
 @Override public void start(Stage primaryStage)
         {
 // Create a ClickHandler instance
-        ClickHandler ch=new ClickHandler(); →23
+        ClickHandler ch = new ClickHandler(); →23
 // Create the Add button
-        btnAdd=new Button();
+        btnAdd = new Button();
         btnAdd.setText("Add");
         btnAdd.setOnAction(ch); →28
 // Create the Subtract button
-        btnSubtract=new Button();
+        btnSubtract = new Button();
         btnSubtract.setText("Subtract");
-        btnSubtract.setOnAction(ch); →33
-
-// Create the Label
-        lbl=new Label();
-        lbl.setText(Integer.toString(iCounter));
-// Add the buttons and label to an HBox pane
-        HBox pane=new HBox(10);
-        pane.getChildren().addAll(lbl,btnAdd,btnSubtract);
-// Add the layout pane to a scene
-        Scene scene=new Scene(pane,200,75);
-// Add the scene to the stage, set the title
-// and show the stage
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Add/Sub");
-        primaryStage.show();
-        }
-
-private class ClickHandler →53
-        implements EventHandler<ActionEvent>
-{
-@Override public void handle(ActionEvent e) →56
-        {
-        if(e.getSource()==btnAdd)
-        {
-        iCounter++;
-        }
-        else
-        {
-        if(e.getSource()==btnSubtract)
-        {
-        iCounter--;
-        }
-        }
-        lbl.setText(Integer.toString(iCounter));
-        }
-        }
-        }
+        btnSubtract.setOnAction(ch); //→33
