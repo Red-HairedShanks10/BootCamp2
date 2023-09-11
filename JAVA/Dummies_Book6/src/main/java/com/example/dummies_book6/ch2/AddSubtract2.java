67735c35a93fc6d67835794850788734d453ce4c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 18:12:53 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract2.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract2.java
@@ -28,3 +28,37 @@ public static void main(String[] args)
         btnSubtract = new Button();
         btnSubtract.setText("Subtract");
         btnSubtract.setOnAction(ch); //→33
+
+            // Create the Label
+            lbl = new Label();
+            lbl.setText(Integer.toString(iCounter));
+// Add the buttons and label to an HBox pane
+            HBox pane = new HBox(10);
+            pane.getChildren().addAll(lbl, btnAdd, btnSubtract);
+// Add the layout pane to a scene
+            Scene scene = new Scene(pane, 200, 75);
+// Add the scene to the stage, set the title
+// and show the stage
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Add/Sub");
+            primaryStage.show();
+        }
+    private class ClickHandler →53
+            implements EventHandler <ActionEvent>
+    {
+        @Override public void handle(ActionEvent e) { //→56
+            if (e.getSource()==btnAdd)
+            {
+                iCounter++;
+            }
+            else
+            {
+                if (e.getSource()==btnSubtract)
+                {
+                    iCounter--;
+                }
+            }
+            lbl.setText(Integer.toString(iCounter));
+        }
+    }
+}
