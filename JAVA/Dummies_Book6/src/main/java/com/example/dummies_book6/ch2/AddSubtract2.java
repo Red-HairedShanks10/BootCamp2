fe533533e9c9531881024afcaf39e5ffa6e6f555
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-11 18:16:07 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract2.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch2/AddSubtract2.java
@@ -16,8 +16,7 @@ public static void main(String[] args)
         Button btnSubtract;
         Label lbl;
         int iCounter = 0;
-@Override public void start(Stage primaryStage)
-        {
+@Override public void start(Stage primaryStage) {
         // Create a ClickHandler instance
         ClickHandler ch = new ClickHandler(); //→23
         // Create the Add button
