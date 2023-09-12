1e71e27f7bb370ac7b9dfe7b036b7c09b436cc3c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 18:16:59 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/PizzaOrder.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/PizzaOrder.java
@@ -11,3 +11,37 @@ import javafx.scene.text.*;
 
 public class PizzaOrder extends Application
 {
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    Stage stage;
+    // Customer name, phone and address fields
+    TextField txtName;
+    TextField txtPhone;
+    TextField txtAddress;
+    // Size radio buttons
+    RadioButton rdoSmall;
+    RadioButton rdoMedium;
+    RadioButton rdoLarge;
+    // Crust style radio buttons
+    RadioButton rdoThin;
+    RadioButton rdoThick;
+    // Topping radio buttons
+    CheckBox chkPepperoni;
+    CheckBox chkSausage;
+    CheckBox chkLinguica;
+    CheckBox chkOlives;
+    CheckBox chkMushrooms;
+    CheckBox chkTomatoes;
+    CheckBox chkAnchovies;
+    @Override public void start(Stage primaryStage)
+    {
+        stage = primaryStage;
+// ----- Create the top pane -----
+        Text textHeading = new Text("Order Your Pizza Now!");
+        textHeading.setFont(new Font(20));
+        HBox paneTop = new HBox(textHeading);
+        paneTop.setPadding(new Insets(20, 10, 20, 10));
+// ---------- Create the customer pane ----------
+// Create the name label and text field
