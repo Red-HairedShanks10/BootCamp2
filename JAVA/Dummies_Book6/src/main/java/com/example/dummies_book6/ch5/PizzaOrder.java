183f33c814954f1742386d218f0e4036e202759f
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 18:19:40 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/PizzaOrder.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/PizzaOrder.java
@@ -172,7 +172,7 @@ public class PizzaOrder extends Application
         else
             msg += "the following toppings:\n"
                     + toppings;
-// Display the message
+        // Display the message
         Alert a = new Alert(Alert.AlertType.INFORMATION,msg);
         a.setTitle("Order Details");
         a.showAndWait();
