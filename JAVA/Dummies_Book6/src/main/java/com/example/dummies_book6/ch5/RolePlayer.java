c417f7ee00a3b487cac3cc53db3ea5c05d61bff3
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 18:11:02 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/RolePlayer.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/RolePlayer.java
@@ -46,7 +46,7 @@ public static void main(String[] args)
 // Create the Actor pane →52
                 HBox paneActor = new HBox(20, lblActor, txtActor);
                 paneActor.setPadding(new Insets(10));
-// Create the Button pane →56
+                // Create the Button pane →56
                 HBox paneButton = new HBox(20, btnOK);
                 paneButton.setPadding(new Insets(10));
                 paneButton.setAlignment(Pos.BOTTOM_RIGHT);
