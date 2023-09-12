76a0f8a50886ff3364a7bad3d2fd4cd7b997501a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 18:09:35 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/RolePlayer.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/RolePlayer.java
@@ -1,16 +1,16 @@
 package com.example.dummies_book6.ch5;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class RolePlayer extends Application {
-
-    public static void main(String[] args) {
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+public class RolePlayer extends Application →8
+        {
+public static void main(String[] args)
+        {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
-}
+        }
+        TextField txtCharacter; →15
+        TextField txtActor;
