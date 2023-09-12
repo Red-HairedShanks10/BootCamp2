49e3a1064af84e2fb362f93da4f78af22bc3bf56
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 15:46:02 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
@@ -1,16 +1,8 @@
 package com.example.dummies_book6.ch4;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class PizzaOrder extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
-}
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
