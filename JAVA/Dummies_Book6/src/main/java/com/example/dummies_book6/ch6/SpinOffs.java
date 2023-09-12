802f74ae77934e8666c25f1592b6c2805cecd4ae
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 18:26:33 +0200

    JFX commit

---
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch6/SpinOffs.java
@@ -0,0 +1,8 @@
+package com.example.dummies_book6.ch6;
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
