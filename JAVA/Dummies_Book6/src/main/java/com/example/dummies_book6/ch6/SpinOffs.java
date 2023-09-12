d38237a19fb75aedcf01bca1c17719377e1a995c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 18:27:12 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch6/SpinOffs.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch6/SpinOffs.java
@@ -6,3 +6,17 @@ import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
+
+
+public class SpinOffs extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    TreeView<String> tree; →15
+    Label lblShowName;
+    @Override public void start(Stage primaryStage)
+    {
+        TreeItem<String> root, andy, archie, →20
+        happy, george, maude;
