6f15a93358aba074dbd48737281f0325940bdc24
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 18:22:27 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/PizzaOrder.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/PizzaOrder.java
@@ -44,7 +44,7 @@ public class PizzaOrder extends Application
         HBox paneTop = new HBox(textHeading);
         paneTop.setPadding(new Insets(20, 10, 20, 10));
 // ---------- Create the customer pane ----------
-// Create the name label and text field
+        // Create the name label and text field
         Label lblName = new Label("Name:");
         lblName.setPrefWidth(100);
         txtName = new TextField();
