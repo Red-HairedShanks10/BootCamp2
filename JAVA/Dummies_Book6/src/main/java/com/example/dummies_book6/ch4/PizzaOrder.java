65a0ff2ee8b3479d332c282b282fd373df85e587
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 19:04:21 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
@@ -115,6 +115,7 @@ public class PizzaOrder extends Application
         grid.addRow(2, lblAddress, txtAddress);
         grid.addRow(3, paneSize, paneCrust, paneToppings);
         grid.add(paneButtons,2,4);
+
         // Set alignments and spanning →119
         grid.setHalignment(lblName, HPos.RIGHT);
         grid.setHalignment(lblPhone, HPos.RIGHT);
