1cbae1a692f1311a9247e1a1ace3b570467f5cc7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 15:46:41 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
@@ -14,3 +14,47 @@ public class PizzaOrder extends Application
     {
         launch(args);
     }
+
+    Stage stage;
+    TextField txtName;
+    TextField txtPhone;
+    TextField txtAddress;
+    RadioButton rdoSmall;
+    RadioButton rdoMedium;
+    RadioButton rdoLarge;
+    RadioButton rdoThin;
+    RadioButton rdoThick;
+    CheckBox chkPepperoni;
+    CheckBox chkMushrooms;
+    CheckBox chkAnchovies;
+    @Override public void start(Stage primaryStage)
+    {
+        stage = primaryStage;
+// Create the name label and text field →32
+        Label lblName = new Label("Name:");
+        txtName = new TextField();
+        txtName.setMinWidth(100);
+        txtName.setPrefWidth(200);
+        txtName.setMaxWidth(300);
+        txtName.setPromptText("Enter the name here");
+// Create the phone number label and text field →40
+        Label lblPhone = new Label("Phone Number:");
+        txtPhone = new TextField();
+        txtPhone.setMinWidth(60);
+        txtPhone.setPrefWidth(120);
+        txtPhone.setMaxWidth(180);
+        txtPhone.setPromptText("Enter the phone number here");
+// Create the address label and text field →48
+        Label lblAddress = new Label("Address:");
+        txtAddress = new TextField();
+        txtAddress.setMinWidth(100);
+        txtAddress.setPrefWidth(200);
+        txtAddress.setMaxWidth(300);
+        txtAddress.setPromptText("Enter the address here");
+// Create the size pane →56
+        Label lblSize = new Label("Size");
+        rdoSmall = new RadioButton("Small");
+        rdoMedium = new RadioButton("Medium");
+        rdoLarge = new RadioButton("Large");
+        rdoMedium.setSelected(true);
+        ToggleGroup groupSize = new ToggleGroup();
