01cf27603a9b973e8588a7d56e3a12039efa5697
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 18:42:54 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
@@ -67,6 +67,7 @@ public class PizzaOrder extends Application
         rdoLarge.setToggleGroup(groupSize);
         VBox paneSize = new VBox(lblSize, rdoSmall, rdoMedium, rdoLarge);
         paneSize.setSpacing(10);
+
         // Create the crust pane →70
         Label lblCrust = new Label("Crust");
         rdoThin = new RadioButton("Thin");
