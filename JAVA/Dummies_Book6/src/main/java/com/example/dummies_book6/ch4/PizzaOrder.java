97dcde378e76424527c3546ecda60a1217cc9c15
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-12 18:38:02 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch4/PizzaOrder.java
@@ -30,7 +30,7 @@ public class PizzaOrder extends Application
     @Override public void start(Stage primaryStage)
     {
         stage = primaryStage;
-        // Create the name label and text field for customer →32
+        // Create the name label and text field for customer
         Label lblName = new Label("Name:");
         txtName = new TextField();
         txtName.setMinWidth(100);
