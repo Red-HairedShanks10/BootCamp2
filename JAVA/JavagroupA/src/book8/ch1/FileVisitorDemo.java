37f9474a4ffa5b942966793960a30895cbadc8a6
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-13 22:12:42 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch1/FileVisitorDemo.java
+++ JAVA/JavagroupA/src/book8/ch1/FileVisitorDemo.java
@@ -1,4 +1,30 @@
 package book8.ch1;
 
-public class FileVisitorDemo {
-}
+
+public class FileVisitorDemo
+{
+    public static void main(String[] args)
+    {
+        Path start = Paths.get("c:\\Windows\\System32"); →9
+        MyFileVisitor visitor = new MyFileVisitor(); →10
+        try
+        {
+            Files.walkFileTree(start, visitor); →13
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    private static class MyFileVisitor extends SimpleFileVisitor <Path> →21
+    {
+        public FileVisitResult visitFile(Path file, →23
+        BasicFileAttributes attr)
+        {
+            System.out.println(file.toString());
+            return FileVisitResult.CONTINUE; →27
+        }
+        public FileVisitResult visitFileFailed(Path file, →30
+        BasicFileAttributes attr)
+        {
+            System.out.println(file.toString() + " COULD NOT ACCESS!");
