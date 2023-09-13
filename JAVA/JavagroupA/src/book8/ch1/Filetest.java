f2eb8f874c3b8ce488f5042b8f58d49f18f56af9
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-13 22:02:06 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch1/Filetest.java
+++ JAVA/JavagroupA/src/book8/ch1/Filetest.java
@@ -1,8 +1,15 @@
 package book8.ch1;
 
+import java.io.File;
+
 public class Filetest {
     public static void main(String[] args) {
 
+        File f = new File(path);
+        if (f.createNewFile())
+            System.out.println("File created.");
+        else
+            System.out.println("File could not be created.");
     }
 
 }
