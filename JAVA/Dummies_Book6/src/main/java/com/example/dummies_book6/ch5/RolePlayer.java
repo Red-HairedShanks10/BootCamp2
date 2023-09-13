a87c04476a11512867a58fa84ce2f733ea9c6aa6
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-13 10:35:38 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/RolePlayer.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/RolePlayer.java
@@ -48,6 +48,7 @@ public static void main(String[] args)
                 Button btnOK = new Button("OK");
                 btnOK.setMinWidth(75);
                 btnOK.setOnAction(e -> btnOK_Click() );
+
                 // Create the Character pane →48
                 HBox paneCharacter = new HBox(20, lblCharacter, txtCharacter);
                 paneCharacter.setPadding(new Insets(10));
