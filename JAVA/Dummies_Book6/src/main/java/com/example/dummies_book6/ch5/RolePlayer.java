c533690e26a0f8eab4997766bb8a93f85ce10d21
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-13 10:52:06 +0200

    JFX commit

--- JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/RolePlayer.java
+++ JAVA/Dummies_Book6/src/main/java/com/example/dummies_book6/ch5/RolePlayer.java
@@ -66,7 +66,7 @@ public static void main(String[] args)
                 //to vertically stack all text boxes to their respective labels
                 VBox pane = new VBox(10, paneCharacter, paneActor, paneButton);
 
-                // Setup the stage →64
+                // Setup the stage
                 Scene scene = new Scene(pane);
                 primaryStage.setScene(scene);
                 primaryStage.setTitle("Role Player");
