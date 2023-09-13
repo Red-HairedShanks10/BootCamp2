16c6a09c87a21fd45b460b3d7b34982bca8ff108
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-13 22:05:19 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch1/Filetest.java
+++ JAVA/JavagroupA/src/book8/ch1/Filetest.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Filetest {
     public static void main(String[] args) {
 
-        File f = new File(path);
+        File f = new File("C:\\Users\\Marco\Documents\Learn\\hits.log");
         if (f.createNewFile()) {
             System.out.println("File created.");
         } else {
