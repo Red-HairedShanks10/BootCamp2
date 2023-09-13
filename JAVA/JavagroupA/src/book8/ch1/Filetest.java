850f871ff92b6132d96b1e49b2eaff377f923b38
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-13 22:06:00 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch1/Filetest.java
+++ JAVA/JavagroupA/src/book8/ch1/Filetest.java
@@ -1,15 +1,20 @@
 package book8.ch1;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Filetest {
     public static void main(String[] args) {
 
         File f = new File("C:\\Users\\Marco\\Documents\\Learn\\hits.log");
-        if (f.createNewFile()) {
-            System.out.println("File created.");
-        } else {
-            System.out.println("File could not be created.");
+        try {
+            if (f.createNewFile()) {
+                System.out.println("File created.");
+            } else {
+                System.out.println("File could not be created.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
