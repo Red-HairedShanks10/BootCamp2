06ff3199b61030bc9e7fa92a83918ac25c7a169d
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-14 14:58:09 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch2/ReadFile.java
+++ JAVA/JavagroupA/src/book8/ch2/ReadFile.java
@@ -37,3 +37,41 @@ public class ReadFile
     }
     private static Movie readMovie(BufferedReader in) →36
     {
+        String title;
+        int year;
+        double price;
+        String line = "";
+        String[] data;
+        try
+        {
+            line = in.readLine();
+        }
+        catch (IOException e)
+        {
+            System.out.println("I/O Error");
+            System.exit(0);
+        }
+        if (line == null)
+            return null;
+        else
+        {
+            data = line.split("\t");
+            title = data[0];
+            year = Integer.parseInt(data[1]);
+            price = Double.parseDouble(data[2]);
+            return new Movie(title, year, price);
+        }
+    }
+    private static class Movie →63
+    {
+        public String title;
+        public int year;
+        public double price;
+public Movie(String title, int year, double price)
+        {
+            this.title = title;
+            this.year = year;
+            this.price = price;
+        }
+    }
+}
