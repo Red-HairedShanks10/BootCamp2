012270f55973f8c21a3e8b2a1bce88318c9b58f2
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-14 18:03:03 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch2/ReadBinaryFile.java
+++ JAVA/JavagroupA/src/book8/ch2/ReadBinaryFile.java
@@ -2,8 +2,9 @@ package book8.ch2;
 
 import java.io.*;
 import java.text.NumberFormat;
-public class ReadBinaryFile
-{
+
+
+public class ReadBinaryFile {
     public static void main(String[] args){// →5
 
         NumberFormat cf = NumberFormat.getCurrencyInstance();
