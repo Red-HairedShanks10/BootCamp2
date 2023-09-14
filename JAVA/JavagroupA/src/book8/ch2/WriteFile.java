cb7e827576cf1eacb1ed872f9f59750dce8b0b01
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-14 17:38:30 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch2/WriteFile.java
+++ JAVA/JavagroupA/src/book8/ch2/WriteFile.java
@@ -23,8 +23,7 @@ public class WriteFile//writing character streams
         movies[5] = new Movie("The Game", 1997, 14.95);
         movies[6] = new Movie("Shakespeare in Love", 1998, 19.95);
         movies[7] = new Movie("Zombieland", 1997, 18.95);
-        movies[8] = new Movie("The King's Speech",
-                1997, 19.95);
+        movies[8] = new Movie("The King's Speech", 1997, 19.95);
         movies[9] = new Movie("Star Trek Into Darkness",
                 1997, 19.95);
         return movies;
