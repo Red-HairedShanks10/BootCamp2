b0088504df169094b1a45d569fcbed10feaad6bc
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-14 21:39:43 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch2/WriteBinaryFile.java
+++ JAVA/JavagroupA/src/book8/ch2/WriteBinaryFile.java
@@ -1,4 +1,18 @@
 package book8.ch2;
 
-public class WriteBinaryFile {
-}
+import java.io.*;
+public class WriteBinaryFile
+{
+    public static void main(String[] args) →4
+    {
+        Movie[] movies = getMovies();
+        DataOutputStream out = openOutputStream("movies.dat");
+        for (Movie m : movies)
+            writeMovie(m, out);
+        closeFile(out);
+    }
+    private static Movie[] getMovies() →12
+    {
+        Movie[] movies = new Movie[10];
+        movies[0] = new Movie("It's a Wonderful Life",
+                1946, 14.95);
