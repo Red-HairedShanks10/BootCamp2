60a176c4ec11194b78f2c80cf60fc91586a08d4e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-15 15:10:02 +0200

    JFX commit

--- JAVA/JavagroupA/src/book8/ch3/movies.sql
+++ JAVA/JavagroupA/src/book8/ch3/movies.sql
@@ -1,1 +1,21 @@
 create database movies;
+use movies; 
+create table movie ( →4
+id int not null auto_increment, →5
+title varchar(50), →6
+year int, →7
+price decimal(8,2), →8
+primary key(id) →9
+);
+insert into movie (title, year, price) →11
+values ("It's a Wonderful Life", 1946, 14.95);
+insert into movie (title, year, price)
+values ("Young Frankenstein", 1974, 16.95);
+insert into movie (title, year, price)
+values ("Star Wars", 1977, 17.95);
+insert into movie (title, year, price)
+values ("The Princess Bride", 1987, 16.95);
+insert into movie (title, year, price)
+values ("Glory", 1989, 14.95);
+insert into movie (title, year, price)
+values("The Game",1997,14.95);
