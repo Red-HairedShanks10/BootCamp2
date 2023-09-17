f200d938fd36181fe7a6d013ff838e9c9f3a2413
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-17 18:32:56 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch4/JdbcTest.java
+++ JAVA/JavagroupA/src/book8/ch4/JdbcTest.java
@@ -1,9 +1,32 @@
 package book8.ch4;
 
+import java.sql.*;
+
 public class JdbcTest {
     public static void main(String[] args) {
-
     }
 
-
+    private static Connection getConnection()
+    {
+        Connection con = null;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/Movies";
+            String user = "root";
+            String pw = "PassWord";
+            con = DriverManager.getConnection(url, user, pw);
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        return con;
+    }
 }
