008f96beb069614f9f228e293b850d9cbde19276
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-17 18:52:09 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch4/ListMovies.java
+++ JAVA/JavagroupA/src/book8/ch4/ListMovies.java
@@ -31,4 +31,50 @@ public class ListMovies {
         {
             Statement s = con.createStatement();
             String select = "Select title, year, price "
+                    + "from movie order by year";
+            ResultSet rows;
+            rows = s.executeQuery(select);
+            return rows;
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    private static Connection getConnection() →46
+    {
+        Connection con = null;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/Movies";
+            String user = "root";
+            String pw = "PassWord";
+            con = DriverManager.getConnection(url, user, pw);
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        return con;
+    }
+    private static Movie getMovie(ResultSet movies) →70
+    {
+        try
+        {
+            String title = movies.getString("Title");
+            int year = movies.getInt("Year");
+            double price = movies.getDouble("Price");
+            return new Movie(title, year, price);
+        }
+        catch (SQLException e)
+
+
 }
