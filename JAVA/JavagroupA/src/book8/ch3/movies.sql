02b8dea4b3063c6638e31c048f3f0141e611e2f9
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-17 16:30:04 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch3/movies.sql
+++ JAVA/JavagroupA/src/book8/ch3/movies.sql
@@ -28,4 +28,4 @@ values("The King's Speech",2010,17.85);
 insert into movie (title, year, price)
 values("Star Trek Into Darkness",2013,19.95);
 
-Select * from movie;
+Select * from movie; -- selecting every row from movies table
