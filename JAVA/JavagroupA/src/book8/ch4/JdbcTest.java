4850def160bb2643a2290dc0c4ee954b60670edc
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-17 18:40:49 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch4/JdbcTest.java
+++ JAVA/JavagroupA/src/book8/ch4/JdbcTest.java
@@ -12,7 +12,7 @@ public class JdbcTest {
         Connection con = null;
         try
         {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost/Movies";
             String user = "root";
             String pw = "password";
