8093549cca24a9bee8f16e3fba871bee30dbe4db
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-17 18:50:29 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch4/ListMovies.java
+++ JAVA/JavagroupA/src/book8/ch4/ListMovies.java
@@ -1,5 +1,31 @@
 package book8.ch4;
 
 public class ListMovies {
-
+    public static void main(String[] args) →6
+    {
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        ResultSet movies = getMovies();
+        try
+        {
+            while (movies.next())
+            {
+                Movie m = getMovie(movies);
+                String msg = Integer.toString(m.year);
+                msg += ": " + m.title;
+                msg += " (" + cf.format(m.price) + ")";
+                System.out.println(msg);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    private static ResultSet getMovies() →28
+    {
+        Connection con = getConnection();
+        try
+        {
+            Statement s = con.createStatement();
+            String select = "Select title, year, price "
 }
