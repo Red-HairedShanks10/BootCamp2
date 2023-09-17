bedff94b838920e5fb7ac99b14b38727b2a83638
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-17 20:35:27 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch5/XmlTest.java
+++ JAVA/JavagroupA/src/book8/ch5/XmlTest.java
@@ -10,6 +10,16 @@ public class XmlTest {
     public static void main(String[] args) {
         Document document = getDocument("C:\\Users\\Marco\\Desktop\\BootCamp2\\JAVA\\JavagroupA\\src\\book8\\ch5\\movies.xml");
         System.out.println("DOM created");
+
+        int count = 0;
+        Element root = doc.getDocumentElement();
+        Node movie = root.getFirstChild();
+        while (movie != null)
+        {
+            count++;
+            movie = movie.getNextSibling();
+        }
+        System.out.println("There are " + count + " movies.");
     }
 
     private static Document getDocument(String name)
