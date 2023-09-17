6aecf01b78ab7bddd0eae672b27d3a9e15feae19
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-17 20:11:47 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch5/XmlTest.java
+++ JAVA/JavagroupA/src/book8/ch5/XmlTest.java
@@ -4,4 +4,24 @@ public class XmlTest {
     public static void main(String[] args) {
 
     }
+
+    private static Document getDocument(String name)
+    {
+        try
+        {
+            DocumentBuilderFactory factory =
+                    DocumentBuilderFactory.newInstance();
+            factory.setIgnoringComments(true);
+            factory.setIgnoringElementContentWhitespace(true);
+            factory.setValidating(true);
+            DocumentBuilder builder =
+                    factory.newDocumentBuilder();
+            return builder.parse(new InputSource(name));
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
