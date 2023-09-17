f8b29569653638422cb23ddf1081271fac6ec4da
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-17 18:37:33 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch4/JdbcTest.java
+++ JAVA/JavagroupA/src/book8/ch4/JdbcTest.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class JdbcTest {
     public static void main(String[] args) {
-        getConnection();
+       Connection connection = getConnection();
     }
 
     private static Connection getConnection()
