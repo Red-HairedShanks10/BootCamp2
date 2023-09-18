5cfa6a4ea26e413aa4abadaaa4b4b5ebad0af70c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-18 10:38:29 +0200

    java commit

---
+++ JAVA/IgBooks/src/igbooks/igBook1/lesson7/TestComparable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbooks.igBook1.lesson7;
+import java.util.*;
+
+/**
+ *
+ * @author Marco
+ */
+public class TestComparable {
+    public static void main(String[] args) {
+        
+        Set<Student> studentList = new TreeSet<>();
+    }
+}
