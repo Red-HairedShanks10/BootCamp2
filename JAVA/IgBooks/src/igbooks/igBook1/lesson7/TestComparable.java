f78ab2f2f104a2fc267642e589d8eaf80995f585
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-18 10:50:05 +0200

    java commit

--- JAVA/IgBooks/src/igbooks/igBook1/lesson7/TestComparable.java
+++ JAVA/IgBooks/src/igbooks/igBook1/lesson7/TestComparable.java
@@ -13,5 +13,14 @@ public class TestComparable {
     public static void main(String[] args) {
         
         Set<Student> studentList = new TreeSet<>();
+        
+        
+        studentList.add(new Student("Ton Djembe", 1000L, 3.9));
+        studentList.add(new Student("Ishe Bongo", 2003L, 3.7));
+        studentList.add(new Student("Amara Conga", 3001L, 3.9));
+        
+        for(Student student : studentList){
+            System.out.println("student: " + student);
+        }
     }
 }
