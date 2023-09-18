75bfa894e5f0b6af7d995789a8f4cd7dafadd800
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-18 11:40:43 +0200

    java commit

--- JAVA/IgBooks/src/igbooks/igBook1/lesson7/StudentSortGpa.java
+++ JAVA/IgBooks/src/igbooks/igBook1/lesson7/StudentSortGpa.java
@@ -3,14 +3,11 @@ package igbooks.igBook1.lesson7;
 import java.util.Comparator;
 
 public class StudentSortGpa implements Comparator<Student>{
-    public int compare(ComparableStudent s1, ComparableStudent s2){
+    public int compare(Student s1, Student s2){
         if (s1.getGpa() < s2.getGpa()) { return 1; }
         else if (s1.getGpa() > s2.getGpa()) { return -1; }
         else { return 0; } 
     }
 
-    @Override
-    public int compare(Student s1, Student s2) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    
 }
