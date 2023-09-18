77f495c6648157b75cd4d3ae801898214f279107
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-18 10:22:45 +0200

    java commit

---
+++ JAVA/IgBooks/src/igbooks/igBook1/lesson7/Student.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package igbooks.igBook1.lesson7;
+
+/**
+ *
+ * @author Marco
+ */
+public class Student {
+    
+}
