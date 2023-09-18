f4715c5766d593b70811075131b53d6bc4efe4ee
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-18 11:31:37 +0200

    java commit

--- JAVA/IgBooks/src/igbooks/igBook1/lesson7/StudentSortGpa.java
+++ JAVA/IgBooks/src/igbooks/igBook1/lesson7/StudentSortGpa.java
@@ -2,8 +2,8 @@ package igbooks.igBook1.lesson7;
 
 import java.util.Comparator;
 
-public class StudentSortGpa implements Comparator<Student>{
-    public int compare(Student s1, Student s2){
+public class StudentSortGpa implements Comparator<ComparableStudent>{
+    public int compare(ComparableStudent s1, ComparableStudent s2){
         if (s1.getGpa() < s2.getGpa()) { return 1; }
         else if (s1.getGpa() > s2.getGpa()) { return -1; }
         else { return 0; } 
