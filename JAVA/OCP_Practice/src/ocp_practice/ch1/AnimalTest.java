07d5e676c30b4ab0b6de643f8fe24f0ebf9519b7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-19 20:23:28 +0200

    java commit

---
+++ JAVA/OCP_Practice/src/ocp_practice/ch1/AnimalTest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ocp_practice.ch1;
+
+/**
+  * Testing out Fields and methods of the Animal class
+  */
+public class AnimalTest {
+
+    
+    public static void main(String[] args) {
+
+    }
+    
+}
