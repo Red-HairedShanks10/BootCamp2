5d1683b075b7df422904757e8493a19cbb9421f1
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-19 22:02:37 +0200

    java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch1/AnimalTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch1/AnimalTest.java
@@ -4,6 +4,8 @@
  */
 package ocp_practice.ch1;
 
+import static ocp_practice.ch1.Animal.*;
+
 /**
   * Testing out Fields and methods of the Animal class
   */
@@ -11,7 +13,15 @@ public class AnimalTest {
 
     
     public static void main(String[] args) {
-
+        Animal snake = new Animal();
+        snake.setName("Mamba");//set name of Animal object
+        
+        //species is static variable//we can acces it without instatiation
+        //getName returns the value from setName()
+        species = snake.getName();//species stores the returned value
+        
+        System.out.println("Species is: " + species);
+        
     }
     
 }
