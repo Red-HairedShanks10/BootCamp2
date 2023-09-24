d2e17286733bd37ca49c01a6b433b0ee1630e6ff
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-24 20:46:59 +0200

    java commit

---
+++ JAVA/OCP_Practice/src/ocp_practice/ch1/NumberPicker.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ocp_practice.ch1;
+
+/**
+ *
+ * @author Marco
+ */
+public class NumberPicker {
+
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
