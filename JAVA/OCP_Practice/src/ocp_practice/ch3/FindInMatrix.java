a4aed592242aada4a70062fc8147143376c98e3e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-26 21:53:33 +0200

    java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch3/FindInMatrix.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch3/FindInMatrix.java
@@ -11,10 +11,16 @@ package ocp_practice.ch3;
 public class FindInMatrix {
 
     /**
-     * @param args the command line arguments
+     * 
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        public static void main(String[] args) {
+        int[][] list = {{1,13},{5,2},{2,2}};
+        int searchValue = 2;
+        int positionX = -1;
+        int positionY = -1;
+        PARENT_LOOP: for(int i=0; i<list.length; i++) {
+            for(int j=0; j<list[i].length; j++) {
+                if(list[i][j]==searchValue) {
+                    positionX = i;
     
 }
