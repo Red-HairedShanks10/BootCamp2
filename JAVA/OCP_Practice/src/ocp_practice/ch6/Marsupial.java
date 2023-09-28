d92f17165088c389e8c6ea58468e620e7dca71d1
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-09-28 21:12:17 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch6/Marsupial.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch6/Marsupial.java
@@ -11,4 +11,10 @@ class Kangaroo extends Marsupial{
     public double getAverageWeight() {
         return super.getAverageWeight()+20;
     }
+
+    public static void main(String[] args) {
+        System.out.println(new Marsupial().getAverageWeight()); // 50.0
+        System.out.println(new Kangaroo().getAverageWeight()); // 70.0
+    }
+
 }
