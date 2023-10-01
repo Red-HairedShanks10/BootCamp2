c6c499563b1df1426e485f30b5d16e702c4c59c7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-01 21:26:27 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/InstanceMethods.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/InstanceMethods.java
@@ -4,6 +4,7 @@ public class InstanceMethods {
     public static void main(String[] args) {
         var str = "Zoo";
         StringStart methodRef = str::startsWith;
+        StringStart lambda = s -> str.startsWith(s);
     }
 }
 
