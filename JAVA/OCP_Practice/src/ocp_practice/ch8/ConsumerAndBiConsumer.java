3a7c596410468fce550666f7cc1b8ff18d558599
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-01 21:58:09 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/ConsumerAndBiConsumer.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/ConsumerAndBiConsumer.java
@@ -2,7 +2,8 @@ package ocp_practice.ch8;
 
 public class ConsumerAndBiConsumer {
     public static void main(String[] args) {
-
+        Consumer<String> c1 = System.out::println;
+        Consumer<String> c2 = x -> System.out.println(x);
     }
 }
 
