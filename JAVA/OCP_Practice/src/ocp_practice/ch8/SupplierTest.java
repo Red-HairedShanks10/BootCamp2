34175dfab5a4bb9366e04562409bc9f383015820
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-01 21:38:18 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/SupplierTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/SupplierTest.java
@@ -1,7 +1,11 @@
 package ocp_practice.ch8;
 
+import java.time.LocalDate;
+import java.util.function.Supplier;
+
 public class SupplierTest {
     public static void main(String[] args) {
-
+        Supplier<LocalDate> s1 = LocalDate::now;
+        
     }
 }
