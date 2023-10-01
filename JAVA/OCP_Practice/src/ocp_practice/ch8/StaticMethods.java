ab701a1b35ef8d830603503535e9fb317afe20a2
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-01 21:20:17 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/StaticMethods.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/StaticMethods.java
@@ -9,6 +9,8 @@ public class StaticMethods {
     public static void main(String[] args) {
         Converter methodRef = Math::round;
         Converter lambda = x -> Math.round(x);
+
+        System.out.println(methodRef.round(100.1));
     }
 }
 
