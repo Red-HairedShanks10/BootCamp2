7cbcdbced901862bba0ed92f50d231864630dbac
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-01 21:12:56 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/MethodReference.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/MethodReference.java
@@ -12,5 +12,9 @@ class DuckHelper{
 }
 
 class Duckling{
-
+    public static void makeSound(String sound) {
+        LearnToSpeak learner = s ->
+                System.out.println(s);
+        DuckHelper.teacher(sound, learner);
+    }
 }
