76222d89c4797e2155566ea40e1155f94a60eab0
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 21:48:25 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/RemoveIfTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/RemoveIfTest.java
@@ -7,5 +7,6 @@ public class RemoveIfTest {
     public static void main(String[] args) {
         Collection<String> list = new ArrayList<>();
         list.add("Magician");
+        list.add("Assistant");
     }
 }
