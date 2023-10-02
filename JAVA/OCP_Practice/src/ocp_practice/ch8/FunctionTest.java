7143ca67b6b541ad6974a9c3f5f5f433eb888e6e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 06:44:03 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/FunctionTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/FunctionTest.java
@@ -12,8 +12,8 @@ public class FunctionTest {
         //using BiFunction:
         BiFunction<String, String, String> b1 = String::concat;
         BiFunction<String, String, String> b2 = (string, toAdd) -> string.concat(toAdd);
-
-        System.out.println(b1.apply("baby ", "chick")); 
+        //
+        System.out.println(b1.apply("baby ", "chick"));
         System.out.println(b2.apply("baby ", "chick"));
 
     }
