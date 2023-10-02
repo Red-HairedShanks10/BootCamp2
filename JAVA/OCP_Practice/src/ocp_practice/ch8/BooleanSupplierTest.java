dd2006237d9de9483bbd22d1a98db1fd88b297c0
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 10:01:18 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/BooleanSupplierTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/BooleanSupplierTest.java
@@ -4,6 +4,8 @@ public class BooleanSupplierTest {
     public static void main(String[] args) {
         BooleanSupplier b1 = () -> true;
         BooleanSupplier b2 = () -> Math.random()> .5;
+
+        System.out.println(b1.getAsBoolean()); // true
     }
 }
 
