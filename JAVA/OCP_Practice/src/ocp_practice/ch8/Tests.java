1236b6dfcdaae685041415d4bc22462965967d59
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 10:42:22 +0200

    Java commit

---
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/Tests.java
@@ -0,0 +1,11 @@
+package ocp_practice.ch8;
+
+import java.util.List;
+
+public class Tests {
+    public static void main(String[] args) {
+        Function<List<String>,String> ex1 = x -> x.get(0);
+        UnaryOperator<Long> ex2 = (Long l) -> (long) 3.14;
+
+    }
+}
