1bf859bcb10254427b407983ab592bcb3cda7e25
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 06:14:50 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/PredicateTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/PredicateTest.java
@@ -4,6 +4,9 @@ public class PredicateTest {
     public static void main(String[] args) {
         Predicate<String> p1 = String::isEmpty;
         Predicate<String> p2 = x -> x.isEmpty();
+
+        System.out.println(p1.test("")); // true
+        System.out.println(p2.test("")); // true
     }
 }
 
