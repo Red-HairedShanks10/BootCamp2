a58fe1eabb7a7a394a564f3fda42e3cbe5d2b758
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 10:42:46 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/UnaryOperatorTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/UnaryOperatorTest.java
@@ -10,21 +10,39 @@ public class UnaryOperatorTest {
   System.out.println(u1.apply("chirp"));
   System.out.println(u2.apply("roar"));
 
+  UnaryOperator<Double> mr3 = a -> {return  Math.floor(a);};
+  System.out.println(mr3.apply(3.5));
+
+
+
   //Applying BinaryOp..
   BinaryOperator<String> b1 = String::concat;
   BinaryOperator<String> b2 = (string, toAdd) -> string.concat(toAdd);
   //
   System.out.println(b1.apply("blue ", "Orca"));
-  System.out.println(b2.apply("Nile", "croc"));
+  System.out.println(b2.apply("Nile ", "croc"));
+
+
+  BinaryOperator<Double> mr1 = Math::pow;
+  BinaryOperator<Double> lambda1 = (d1,d2) -> Math.pow(d1, d2);
+  System.out.println(lambda1.apply(2.4, 4.5));
+
+  BinaryOperator<Integer> mr2 = Math::max;
+  System.out.println(mr2.apply(2, 4));
+
+
+
+
+
  }
 
 
 }
 
- interface UnaryOperator<T> extends Function<T, T> {
+ interface UnaryOperator<T>  {
   T apply(T t);
 }
 
- interface BinaryOperator<T> extends BiFunction<T, T, T> {
+ interface BinaryOperator<T>{
   T apply(T t1, T t2);
 }
