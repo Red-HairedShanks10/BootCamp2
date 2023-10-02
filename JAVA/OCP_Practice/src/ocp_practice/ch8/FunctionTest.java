641756e7b34e15d8f268e6c3c82676b0bba15570
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 06:32:11 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/FunctionTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/FunctionTest.java
@@ -2,7 +2,7 @@ package ocp_practice.ch8;
 
 public class FunctionTest {
     public static void main(String[] args) {
-
+        //
     }
 
 }
@@ -11,7 +11,7 @@ public class FunctionTest {
 
 
 interface Function<T,R>{
-    R apply(T t);
+    R apply(T t);//value returned by t will be converted to Value of R
 }
 
 interface BiFunction<T,U, R>{
