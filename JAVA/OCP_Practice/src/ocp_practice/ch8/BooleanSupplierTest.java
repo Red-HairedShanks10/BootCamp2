24e40bcc97cb1597a2db4f9b2e016793309c891f
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 10:03:34 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/BooleanSupplierTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/BooleanSupplierTest.java
@@ -4,7 +4,7 @@ public class BooleanSupplierTest {
     public static void main(String[] args) {
 
         BooleanSupplier b1 = () -> true;
-        BooleanSupplier b2 = () -> Math.random()> .5;
+        BooleanSupplier b2 = () -> Math.random()> .5;//boolean returned will be varied
 
         System.out.println(b1.getAsBoolean());
         System.out.println(b2.getAsBoolean());
