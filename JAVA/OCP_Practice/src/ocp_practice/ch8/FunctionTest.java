8d0f91c4c7fb03a2ecc1161b626be4894eb83a6a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 06:35:38 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/FunctionTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/FunctionTest.java
@@ -5,8 +5,8 @@ public class FunctionTest {
         //using Function interface
         Function<String, Integer> f1 = String::length;
         Function<String, Integer> f2 = x -> x.length();
-
-        System.out.println(f1.apply("cluck")); 
+        //
+        System.out.println(f1.apply("cluck"));
         System.out.println(f2.apply("cluck"));
     }
 
