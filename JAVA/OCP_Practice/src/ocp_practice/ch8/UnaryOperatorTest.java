a1be38f443e74c44bd674a9e07b062f3025b3068
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 09:48:27 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/UnaryOperatorTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/UnaryOperatorTest.java
@@ -14,7 +14,7 @@ public class UnaryOperatorTest {
   BinaryOperator<String> b1 = String::concat;
   BinaryOperator<String> b2 = (string, toAdd) -> string.concat(toAdd);
   //
-  System.out.println(b1.apply("baby ", "chick"));
+  System.out.println(b1.apply("baby ", "dragon"));
   System.out.println(b2.apply("baby ", "chick"));
  }
 
