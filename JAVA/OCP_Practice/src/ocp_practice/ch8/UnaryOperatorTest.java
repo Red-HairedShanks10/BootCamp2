5323f08f9c9649ecf695599f8abf03abed8d5901
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 09:28:03 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/UnaryOperatorTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/UnaryOperatorTest.java
@@ -3,6 +3,6 @@ package ocp_practice.ch8;
 public class UnaryOperatorTest {
 }
 
-public interface UnaryOperator<T> extends Function<T, T> {
+ interface UnaryOperator<T> extends Function<T, T> {
 // omitted static method
 }
