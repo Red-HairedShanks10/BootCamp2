a49a45ec0c19ae2b3812d48a2e70305a9e8be866
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 06:19:59 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/PredicateTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/PredicateTest.java
@@ -12,7 +12,7 @@ public class PredicateTest {
         //BiPredicate:
         BiPredicate<String, String> b1 = String::startsWith;
         BiPredicate<String, String> b2 = (string, prefix) -> string.startsWith(prefix);
-
+        //
         System.out.println(b1.test("chicken", "chick"));
         System.out.println(b2.test("chicken", "chick"));
     }
