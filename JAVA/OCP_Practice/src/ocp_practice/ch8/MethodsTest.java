1b736e7054acdc0ff4cc0e9b684f9f8e43ce1147
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-02 11:11:46 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch8/MethodsTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch8/MethodsTest.java
@@ -2,8 +2,7 @@ package ocp_practice.ch8;
 
 public class MethodsTest {
     public static void main(String[] args) {
-        Predicate<String> egg = s ->
-                s.contains("egg");
+        Predicate<String> egg = s -> s.contains("egg");
         Predicate<String> brown = s -> s.contains("brown");
 
             Predicate<String> brownEggs = egg.and
