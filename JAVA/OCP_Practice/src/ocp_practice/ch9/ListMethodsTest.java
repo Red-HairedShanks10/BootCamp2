f5fafdb3c5d3bc8a38e2a3d54a982210a4a1c237
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 10:15:16 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/ListMethodsTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/ListMethodsTest.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class ListMethodsTest {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+        /*List<String> list = new ArrayList<>();
          list.add("SD");
          list.add(0, "NY");
         list.set(1, "FL");
         System.out.println(list.get(0));
          list.remove("NY");
          list.remove(0);
-        list.set(0, "?");
+        list.set(0, "?");*/
 
         var numbers = Arrays.asList(1, 2, 3);
         numbers.replaceAll(x -> x*2);
