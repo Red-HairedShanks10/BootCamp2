b13b58087c74b617c899d12d909a3484ca538316
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 09:19:03 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/EqualityTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/EqualityTest.java
@@ -5,6 +5,6 @@ public class EqualityTest {
         var list1 = List.of(1, 2);
         24: var list2 = List.of(2, 1);
         25: var set1 = Set.of(1, 2);
-        26: var set2 = Set.of(2, 1);
+         var set2 = Set.of(2, 1);
     }
 }
