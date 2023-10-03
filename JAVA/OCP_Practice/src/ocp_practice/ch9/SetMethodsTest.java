cf7bd8f79792450e21dee93f995cd452e5303409
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 10:41:31 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/SetMethodsTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/SetMethodsTest.java
@@ -8,7 +8,7 @@ public class SetMethodsTest {
         4: boolean b1 = set.add(66); // true
         5: boolean b2 = set.add(10); // true
         6: boolean b3 = set.add(66); // false
-        7: boolean b4 = set.add(8); // true
+         boolean b4 = set.add(8); // true
         set.forEach(System.out::println);
     }
 }
