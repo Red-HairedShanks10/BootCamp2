a9b148a8595fed9c715d336c1c1ee9435e9b456c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 15:20:00 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/MissingDuck.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/MissingDuck.java
@@ -1,7 +1,17 @@
 package ocp_practice.ch9;
+import java.util.*;
 
 public class MissingDuck {
     public static void main(String[] args) {
-
+        private String name;
+        public int compareTo(MissingDuck quack) {
+            if (quack == null)
+                throw new IllegalArgumentException("Poorly formed duck!");
+            if (this.name == null && quack.name == null)
+                return 0;
+            else if (this.name == null) return -1;
+            else if (quack.name == null) return 1;
+            else return name.compareTo(quack.name);
+        }
     }
 }
