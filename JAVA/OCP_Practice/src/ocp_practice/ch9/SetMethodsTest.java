7117040e90c3594bb660e681095f092f13f9fb89
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 10:44:49 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/SetMethodsTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/SetMethodsTest.java
@@ -15,8 +15,8 @@ public class SetMethodsTest {
 
         //TreeSet
         Set<Integer> set = new TreeSet<>();
-        boolean b1 = set.add(66); 
-        boolean b2 = set.add(10); // true
+        boolean b1 = set.add(66);
+        boolean b2 = set.add(10); 
         boolean b3 = set.add(66); // false
         boolean b4 = set.add(8); // true
         set.forEach(System.out::println);
