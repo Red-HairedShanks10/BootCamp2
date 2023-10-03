5fb32c6b04a613a82f008096f72774592c12abec
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 11:05:21 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/DequeTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/DequeTest.java
@@ -1,7 +1,8 @@
 package ocp_practice.ch9;
+import java.util.*;
 
 public class DequeTest {
     public static void main(String[] args) {
-
+        Deque<Integer> deque = new LinkedList<>();
     }
 }
