b3b2b143678237ce9057062347c413f1dd006377
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 10:04:08 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/ListConstructorTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/ListConstructorTest.java
@@ -6,6 +6,8 @@ public class ListConstructorTest {
     public static void main(String[] args) {
         var strings = new ArrayList<String>();
         strings.add("a");
+        strings.add("b");
+        strings.add("c");
         for (String s: strings) { }
     }
 }
