3cf7230375b6c37ef1a40c76ae59fa1e212af4ea
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 09:42:17 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/ListTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/ListTest.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class ListTest {
     public static void main(String[] args) {
         String[] array = new String[] {"a", "b", "c"};
-         List<String> asList = Arrays.asList(array); // [a, b, c]
+         List<String> asList = Arrays.asList(array); 
         List<String> of = List.of(array); // [a, b, c]
          List<String> copy = List.copyOf(asList); // [a, b, c]
     }
