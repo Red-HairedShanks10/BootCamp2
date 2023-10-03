250db3ddae825725855c9f3a0599a5e31a86e1ce
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 10:41:50 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/SetMethodsTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/SetMethodsTest.java
@@ -5,7 +5,7 @@ public class SetMethodsTest {
     public static void main(String[] args) {
 
         3: Set<Integer> set = new HashSet<>();
-        4: boolean b1 = set.add(66); // true
+        boolean b1 = set.add(66); // true
         boolean b2 = set.add(10); // true
          boolean b3 = set.add(66); // false
          boolean b4 = set.add(8); // true
