d3f013f017ba3e5acdc5f24c78c5aed7a37390fe
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 12:12:37 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/MapTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/MapTest.java
@@ -4,12 +4,31 @@ import java.util.*;
 public class MapTest {
     public static void main(String[] args) {
 
+        Map<String, String> map = Map.ofEntries(
+                Map.entry("George","555-555-5555"),
+                Map.entry("Saul","554-565-7555"),
+                Map.entry("Luffy","556-556-5656")
+        );
+        Map<String, String> map2 = Map.copyOf(map);
+
+        Set<String> keys = map.keySet();
+        for(String key : keys){
+            System.out.println("map1 keys: " + key + " " +  map.get(key));
+        }
+
+        Set<String> keys2 = map2.keySet();
+        for(String key : keys2){
+            System.out.println("map2 keys: " + key + " " +  map2.get(key));
+        }
+
+
+
         //HashMaps
         Map<String, String> hashMap = new HashMap<>();
-        hashMap.put("koala", "bamboo");
-        hashMap.put("lion", "meat");
-        hashMap.put("giraffe", "leaf");
-        String food = hashMap.get("koala");
+        hashMap.put("Panda", "bamboo");
+        hashMap.put("Tiger", "meat");
+        hashMap.put("horse", "hay");
+        String foodHash = hashMap.get("Panda");
         for (String key: hashMap.keySet()){
             System.out.print(key + ",");
         }
@@ -19,9 +38,9 @@ public class MapTest {
         treeMap.put("koala", "bamboo");
         treeMap.put("lion", "meat");
         treeMap.put("giraffe", "leaf");
-        String food = treeMap.get("koala"); // bamboo
+        String foodTree = treeMap.get("lion");
         for (String key: treeMap.keySet())
-            System.out.print(key + ","); // giraffe,koala,lion,
+            System.out.print(key + ",");
 
     }
 }
