1e1415883d0ca6e21de975f8aeca0051efd1f3c9
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 10:32:26 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/ListToArray.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/ListToArray.java
@@ -1,7 +1,15 @@
 package ocp_practice.ch9;
-
+import java.util.*;
 public class ListToArray {
     public static void main(String[] args) {
 
+        13: List<String> list = new ArrayList<>();
+        14: list.add("hawk");
+        15: list.add("robin");
+        16: Object[] objectArray = list.toArray();
+        17: String[] stringArray = list.toArray(new String[0]);
+        18: list.clear();
+        19: System.out.println(objectArray.length); // 2
+        20: System.out.println(stringArray.length); //
     }
 }
