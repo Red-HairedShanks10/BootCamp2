ba835035bcc2baf55ead31e165e161d4380fc85e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 10:52:54 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/ListToArray.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/ListToArray.java
@@ -6,6 +6,7 @@ public class ListToArray {
          List<String> list = new ArrayList<>();
          list.add("hawk");
          list.add("Crow");
+        list.add("Great Horn Owl");
          Object[] objectArray = list.toArray();
          String[] stringArray = list.toArray(new String[0]);
          list.clear();
