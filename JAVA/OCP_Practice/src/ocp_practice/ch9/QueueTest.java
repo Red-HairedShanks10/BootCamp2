430b854f6003e1c124d5363e9f188d015e271a92
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 11:01:07 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/QueueTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/QueueTest.java
@@ -4,8 +4,9 @@ public class QueueTest {
     public static void main(String[] args) {
          Queue<Integer> queue = new LinkedList<>();
          queue.add(10);
-       queue.add(4);
-        System.out.println(queue.remove()); 
+         queue.add(4);
+
+         System.out.println(queue.remove());
         System.out.println(queue.peek());
     }
 }
