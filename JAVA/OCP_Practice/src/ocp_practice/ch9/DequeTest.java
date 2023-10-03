28a2324c1f79c9f4eda21982bebf2db81eea007b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 11:15:11 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/DequeTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/DequeTest.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class DequeTest {
     public static void main(String[] args) {
         Deque<Integer> deque = new LinkedList<>();
-        13: deque.offerFirst(10); // true 10
+         deque.offerFirst(10); // true 10
          deque.offerLast(4); // true 10 4
          deque.peekFirst(); // 10 10 4
          deque.pollFirst();
