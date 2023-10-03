8e7d2bee6e87f54ae6138cf5f338a9553c66859b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 15:34:16 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/MissingDuck.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/MissingDuck.java
@@ -2,19 +2,48 @@ package ocp_practice.ch9;
 
 public class MissingDuck {
 
+
     private String name;
-    public int compareTo(MissingDuck quack) {
-        if (quack == null)
-            throw new IllegalArgumentException("Poorly formed duck!");
-        if (this.name == null && quack.name == null)
-            return 0;
-        else if (this.name == null) return -1;
-        else if (quack.name == null) return 1;
-        else return name.compareTo(quack.name);
+
+    public MissingDuck(String name){
+        this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public int compareTo(MissingDuck quack) {
+        if (quack == null){
+            throw new IllegalArgumentException("Poorly formed duck!");
+        }
+
+
+
+        if (this.name == null && quack.name == null) {
+            System.out.println("Both names are null: ");
+            return 0;
+        }
+        else if (this.name == null){
+            System.out.println("this name is null");
+            return -1;
+        }
+        else if (quack.name == null) {
+            System.out.println("quack name is null");
+            return 1;
+        }
+        else {return name.compareTo(quack.name);}
+    }
 
     public static void main(String[] args) {
+        MissingDuck mDuck1 = new MissingDuck("Kurama");
+        MissingDuck mDuck2 = new MissingDuck("Kurapika");
+
+        mDuck1.compareTo(mDuck1);
+        mDuck2.compareTo(mDuck1);
+        mDuck1.compareTo(mDuck2);
+
 
     }
 }
