cb371a386b1c839690274c1b53a29f3efc500298
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 15:18:40 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/Duck.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/Duck.java
@@ -1,7 +1,29 @@
 package ocp_practice.ch9;
+import java.util.*;
 
-public class Duck implements Comparable<Duck>{
+import static java.util.Collections.sort;
+
+
+public class Duck implements Comparable<Duck> {
+    private String name;
+
+    public Duck(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    public int compareTo(Duck d) {
+        return name.compareTo(d.name);
+    }
+
     public static void main(String[] args) {
-
+        var ducks = new ArrayList<Duck>();
+        ducks.add(new Duck("Quack"));
+        ducks.add(new Duck("Puddles"));
+        //ducks.sort();
+        System.out.println(ducks);
     }
 }
