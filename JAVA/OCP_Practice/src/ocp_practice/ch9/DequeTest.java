f4361830647fbf46b799d4650045c3fdb7b03e2a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 11:28:04 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/DequeTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/DequeTest.java
@@ -16,7 +16,7 @@ public class DequeTest {
         Deque<Integer> stack = new ArrayDeque<>();
          stack.push(10);
         stack.push(4);
-         stack.peek(); // 4 4 10
+         stack.peek(); 
         stack.poll(); // 4
     }
 }
