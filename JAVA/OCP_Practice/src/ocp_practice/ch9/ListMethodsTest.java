9c93969aed7e791ea213fc536fcbf1fb07fde642
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 10:11:33 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/ListMethodsTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/ListMethodsTest.java
@@ -1,7 +1,15 @@
 package ocp_practice.ch9;
-
+import java.util.*;
 public class ListMethodsTest {
     public static void main(String[] args) {
 
+        List<String> list = new ArrayList<>();
+        4: list.add("SD"); // [SD]
+        5: list.add(0, "NY"); // [NY,SD]
+        6: list.set(1, "FL"); // [NY,FL]
+        7: System.out.println(list.get(0)); // NY
+        8: list.remove("NY"); // [FL]
+        9: list.remove(0); // []
+        10: list.set(0, "?"); // IndexOutOfBoundsException
     }
 }
