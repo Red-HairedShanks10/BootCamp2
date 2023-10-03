49c9d58cc37a78b1265710c63d8cd51c726fa98d
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-03 11:52:11 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/MapTest.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/MapTest.java
@@ -1,7 +1,16 @@
 package ocp_practice.ch9;
+import java.util.*;
 
 public class MapTest {
     public static void main(String[] args) {
 
+        //HashMaps
+        Map<String, String> map = new HashMap<>();
+        map.put("koala", "bamboo");
+        map.put("lion", "meat");
+        map.put("giraffe", "leaf");
+        String food = map.get("koala"); // bamboo
+        for (String key: map.keySet())
+            System.out.print(key + ","); // koala,giraffe,lion,
     }
 }
