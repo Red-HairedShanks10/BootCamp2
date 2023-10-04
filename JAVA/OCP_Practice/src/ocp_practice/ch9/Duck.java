3df21824f6e96adeff2c052306f5275cee00140a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-04 06:32:55 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/Duck.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/Duck.java
@@ -31,34 +31,38 @@ public class Duck implements Comparable<Duck> {
 
 
 import java.util.ArrayList;
- import java.util.Collections;
- import java.util.Comparator;
+import java.util.Collections;
+import java.util.Comparator;
 
- public class Duck implements Comparable<Duck> {
- private String name;
- private int weight;
+public class Duck implements Comparable<Duck> {
+    private String name;
+    private int weight;
 
+    public Duck(String name, int weight){
+        this.name
+    }
 
+    public String toString() {
+        return name;
+    }
 
-         public String toString() { return name; }
-
-         public int compareTo(Duck d) {
+    public int compareTo(Duck d) {
         return name.compareTo(d.name);
-         }
+    }
 
-         public static void main(String[] args) {
-         Comparator<Duck> byWeight = new Comparator<Duck>() {
- public int compare(Duck d1, Duck d2) {
-                return d1.getWeight()-d2.
+    public static void main(String[] args) {
+        Comparator<Duck> byWeight = new Comparator<Duck>() {
+            public int compare(Duck d1, Duck d2) {
+                return d1.getWeight() - d2.
                         getWeight();
-                 }
- };
-         var ducks = new ArrayList<Duck>();
+            }
+        };
+        var ducks = new ArrayList<Duck>();
         ducks.add(new Duck("Quack", 7));
         ducks.add(new Duck("Puddles", 10));
-         Collections.sort(ducks);
+        Collections.sort(ducks);
         System.out.println(ducks);
-     Collections.sort(ducks, byWeight);
-             System.out.println(ducks); 
-             }
- }
+        Collections.sort(ducks, byWeight);
+        System.out.println(ducks);
+    }
+}
