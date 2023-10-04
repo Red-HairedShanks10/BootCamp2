8a5cf8c228a1429d540a52b09bd4a028e26d64ad
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-04 09:30:24 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch9/genericInterface/Shippable.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch9/genericInterface/Shippable.java
@@ -1,4 +1,13 @@
 package ocp_exam_practice.ch9.genericInterface;
 
-public interface Shippable {
+import java.awt.*;
+
+public interface Shippable<T>{
+    void ship(T t);
+}
+
+class ShippableRobotCrate implements Shippable<Robot>{
+    public void ship(Robot robot){
+
+    }
 }
