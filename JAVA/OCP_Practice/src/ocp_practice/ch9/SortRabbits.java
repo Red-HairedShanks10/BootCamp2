ffbb4071050d9550e51ab993cfc05245f25a3c0c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-04 06:49:24 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/SortRabbits.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/SortRabbits.java
@@ -1,4 +1,11 @@
 package ocp_practice.ch9;
 
 public class SortRabbits {
+    static record Rabbit(int id) {}
+4: public static void main(String[] args) {
+        5: List<Rabbit> rabbits = new ArrayList<>();
+        6: rabbits.add(new Rabbit(3));
+        7: rabbits.add(new Rabbit(1));
+        8: Collections.sort(rabbits); // DOES NOT COMPILE
+         } }
 }
