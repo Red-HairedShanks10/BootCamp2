ac7b03a4db68f569674ea1e83833aa50bd86ed6e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-04 06:56:23 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/UseTreeSet.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/UseTreeSet.java
@@ -3,7 +3,7 @@ package ocp_practice.ch9;
 public class UseTreeSet {
     static class Rabbit{ int id; }
 4: public static void main(String[] args) {
-        5: Set<Duck> ducks = new TreeSet<>();
+         Set<Duck> ducks = new TreeSet<>();
          ducks.add(new Duck("Puddles"));
 
          Set<Rabbit> rabbits = new TreeSet<>();
