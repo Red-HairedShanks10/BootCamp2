97623edadeb1d881ca9fb88c77a6884aa122ded2
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-04 07:02:37 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/SortListMethod.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/SortListMethod.java
@@ -2,6 +2,13 @@ package ocp_practice.ch9;
 
 public class SortListMethod {
     public static void main(String[] args) {
-
+        List<String> bunnies = new ArrayList<>();
+        4: bunnies.add("long ear");
+        5: bunnies.add("floppy");
+        6: bunnies.add("hoppy");
+        7: System.out.println(bunnies); // [long ear, floppy, hoppy]
+        8: bunnies.sort((b1, b2) ->
+                b1.compareTo(b2));
+      System.out.println(bunnies); // [floppy, hoppy, long ear]
     }
 }
