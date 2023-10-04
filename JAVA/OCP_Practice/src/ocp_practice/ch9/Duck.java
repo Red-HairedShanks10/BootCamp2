23923466c031a7d3c56ba31e9413b8000c10ae4e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-04 06:26:44 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/Duck.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/Duck.java
@@ -45,7 +45,7 @@ import java.util.ArrayList;
         13: public int compareTo(Duck d) {
         14: return name.compareTo(d.name);
         15: }
-16:
+
          public static void main(String[] args) {
          Comparator<Duck> byWeight = new Comparator<Duck>() {
  public int compare(Duck d1, Duck d2) {
