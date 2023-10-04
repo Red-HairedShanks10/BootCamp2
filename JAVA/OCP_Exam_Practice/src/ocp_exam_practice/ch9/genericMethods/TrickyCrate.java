c8c71e894146da2e5fbc2acde29dababaec35978
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-04 10:38:40 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch9/genericMethods/TrickyCrate.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch9/genericMethods/TrickyCrate.java
@@ -1,5 +1,27 @@
 package ocp_exam_practice.ch9.genericMethods;
 
-public class TrickyCrate {
+import java.awt.*;
 
+public class TrickyCrate<T> {
+    public <T> T tricky(T t){
+        return t;
+    }
+
+
+    public static  String crateName(){
+        TrickyCrate<Robot> crate = new TrickyCrate<>();
+        return crate.tricky("bot");
+    }
+
+
+
+    public static  int crateNum(){
+        TrickyCrate<Robot> crate = new TrickyCrate<>();
+        return crate.tricky(2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(crateName());
+        System.out.println(crateNum());
+    }
 }
