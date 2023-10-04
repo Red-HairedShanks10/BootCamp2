ab3a31149f1dfdad18e394fc5746e55a42188b72
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-04 06:25:27 +0200

    Java commit

--- JAVA/OCP_Practice/src/ocp_practice/ch9/Duck.java
+++ JAVA/OCP_Practice/src/ocp_practice/ch9/Duck.java
@@ -1,3 +1,4 @@
+/*
 package ocp_practice.ch9;
 import java.util.*;
 
@@ -26,4 +27,35 @@ public class Duck implements Comparable<Duck> {
         //ducks.sort();
         System.out.println(ducks);
     }
-}
+}*/
+
+
+import java.util.ArrayList;
+2: import java.util.Collections;
+3: import java.util.Comparator;
+4:
+        5: public class Duck implements Comparable<Duck> {
+6: private String name;
+7: private int weight;
+8:
+        9: // Assume getters/setters/constructors provided
+        10:
+        11: public String toString() { return name; }
+12:
+        13: public int compareTo(Duck d) {
+        14: return name.compareTo(d.name);
+        15: }
+16:
+        17: public static void main(String[] args) {
+        18: Comparator<Duck> byWeight = new Comparator<Duck>() {
+19: public int compare(Duck d1, Duck d2) {
+                20: return d1.getWeight()-d2.
+                        getWeight();
+                21: }
+22: };
+        23: var ducks = new ArrayList<Duck>();
+        24: ducks.add(new Duck("Quack", 7));
+        25: ducks.add(new Duck("Puddles", 10));
+        26: Collections.sort(ducks);
+        27: System.out.println(ducks); // [Puddles, Quack]
+        28: Collections.sort(ducks, byWeight);
