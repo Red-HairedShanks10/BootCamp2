7da8b13fcac82c9eb1b27e62f7f6ffcaf5125a73
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 10:36:06 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalTest.java
@@ -3,11 +3,13 @@ package ocp_exam_practice.ch10;
 import java.util.Optional;
 
 public class OptionalTest {
+
     public static Optional<Double> average(int... scores) {
-        if (scores.length == 0) return Optional.empty();
+        if (scores.length == 0){ return Optional.empty();}
        int sum = 0;
-        for (int score: scores) sum += score;
-        Optional o = (scores.length == 0) ? Optional.empty() : Optional.of((double)sum/scores.length);
+        for (int score: scores){ sum += score;}
+
+        Optional o = (scores.length == 0 ) ? Optional.empty() : Optional.of((double)sum/scores.length);
         return o;
          }
 
@@ -15,20 +17,28 @@ public class OptionalTest {
 
     public static void main(String[] args) {
         Optional<Double> opt = average(90, 100,56,80);
-        if (opt.isPresent()) {
+        opt.ifPresent(System.out::println);
+
+       /* if (opt.isPresent()) {
             System.out.println(opt.get());
         }else {
             System.out.println("Not opt");
-        }
+        }*/
 
         Optional<Double> opt2 = average();
-        Optional o = Optional.ofNullable(opt2);
-        System.out.println("opt 2 issue: " + opt2);
+       // Optional o = Optional.ofNullable(opt2);
+        Optional o = (opt2 == null) ? Optional.empty() : Optional.of(opt2);
+        opt2.ifPresent(System.out::println);
+        //System.out.println("opt 2 issue: " + opt2);
 
       /*  if (opt2.isPresent()) {
             System.out.println(opt2.get());
         }else {
             System.out.println("opt 2 issue: " + opt2);
         }*/
+
+        Optional<Double> opt = average();
+         System.out.println(opt.orElse(Double.NaN));
+        32: System.out.println(opt.orElseGet(() -> Math.random()));
     }
 }
