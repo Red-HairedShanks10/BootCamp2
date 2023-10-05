b12774be4abd8eeddbe74da0590ff35c833b77a1
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 13:41:51 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/ReducingStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/ReducingStreams.java
@@ -12,5 +12,17 @@ public class ReducingStreams {
         Stream<Integer> numStream = Stream.of(3, 5, 6);
         int nStream = numStream.reduce(1, (a,b) -> a*b);
         System.out.println(nStream);
+
+        ///////////////////////////
+
+        BinaryOperator<Integer> op = (a, b) ->
+                a * b;
+        Stream<Integer> empty = Stream.empty();
+        Stream<Integer> oneElement = Stream.of(3);
+        Stream<Integer> threeElements = Stream.of(3, 5, 6);
+        empty.reduce(op).ifPresent(System.out::println); // no output
+        oneElement.reduce(op).ifPresent(System.out::println); // 3
+        threeElements.reduce(op).ifPresent(System.out::println); // 90
+
     }
 }
