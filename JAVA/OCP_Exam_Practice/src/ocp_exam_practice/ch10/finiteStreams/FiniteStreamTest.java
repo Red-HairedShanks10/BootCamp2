b453b44c2db663772ca9ef2f4189095cbf0fa3ba
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 09:08:04 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/finiteStreams/FiniteStreamTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/finiteStreams/FiniteStreamTest.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class FiniteStreamTest {
     public static void main(String[] args) {
         Stream<String> empty = Stream.empty(); // count = 0
-        Stream<Integer> singleElement = Stream.of(1); // count = 1
+        Stream<Integer> singleElement = Stream.of(1); 
          Stream<Integer> fromArray = Stream.of(1, 2, 3);
     }
 }
