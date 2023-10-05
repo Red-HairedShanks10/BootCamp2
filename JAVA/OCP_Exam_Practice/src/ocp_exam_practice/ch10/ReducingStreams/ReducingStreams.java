17924476d98269ea575bbcce65580764abe089f0
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 13:46:55 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/ReducingStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/ReducingStreams.java
@@ -5,14 +5,14 @@ import java.util.stream.Stream;
 
 public class ReducingStreams {
     public static void main(String[] args) {
-        Stream<String> stream = Stream.of("w","o", "l", "f");
+      /*  Stream<String> stream = Stream.of("w","o", "l", "f");
        // String word = stream.reduce("", (s,c) -> s + c );
        String word = stream.reduce("", String::concat);
-        System.out.println(word);
+        System.out.println(word);*/
 
-        Stream<Integer> numStream = Stream.of(3, 5, 6);
+        /*Stream<Integer> numStream = Stream.of(3, 5, 6);
         int nStream = numStream.reduce(1, (a,b) -> a*b);
-        System.out.println(nStream);
+        System.out.println(nStream);*/
 
         ///////////////////////////
 
@@ -21,7 +21,7 @@ public class ReducingStreams {
         Stream<Integer> empty = Stream.empty();
         Stream<Integer> oneElement = Stream.of(3);
         Stream<Integer> threeElements = Stream.of(3, 5, 6);
-        
+
         empty.reduce(op).ifPresent(System.out::println);
         oneElement.reduce(op).ifPresent(System.out::println);
         threeElements.reduce(op).ifPresent(System.out::println);
