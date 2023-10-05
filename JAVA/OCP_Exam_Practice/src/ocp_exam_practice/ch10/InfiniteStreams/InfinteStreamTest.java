ce81320606d02055be9e70a05bd073c6dc969ec7
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 11:27:08 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/InfiniteStreams/InfinteStreamTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/InfiniteStreams/InfinteStreamTest.java
@@ -2,6 +2,7 @@ package ocp_exam_practice.ch10.InfiniteStreams;
 
 public class InfinteStreamTest {
     public static void main(String[] args) {
-
+        Stream<Double> randoms = Stream.generate(Math::random);
+         Stream<Integer> oddNumbers = Stream.iterate(1, n -> n + 2);
     }
 }
