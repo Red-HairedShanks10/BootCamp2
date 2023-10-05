67d5b799c29f2431f4b5b3d145ba193551b0051a
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 13:54:55 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/ReducingStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/ReducingStreams.java
@@ -26,5 +26,11 @@ public class ReducingStreams {
         oneElement.reduce(op).ifPresent(System.out::println);
         threeElements.reduce(op).ifPresent(System.out::println);
 
+        Stream<String> stream = Stream.of("w", "o", "l", "f!");
+        int length = stream.reduce(0, (i, s) ->
+                i+s.length(), (a, b) ->
+                a+b);
+        System.out.println(length); // 5
+
     }
 }
