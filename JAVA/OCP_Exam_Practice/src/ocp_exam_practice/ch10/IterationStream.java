928fec347689583fa5939229724521f0d05879aa
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 13:17:57 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/IterationStream.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/IterationStream.java
@@ -1,7 +1,11 @@
 package ocp_exam_practice.ch10;
 
+import java.util.stream.Stream;
+
 public class IterationStream {
     public static void main(String[] args) {
 
+        Stream<String> s = Stream.of("Monkey", "Gorilla", "Bonobo");
+        s.forEach(System.out::print); // MonkeyGorillaBonobo
     }
 }
