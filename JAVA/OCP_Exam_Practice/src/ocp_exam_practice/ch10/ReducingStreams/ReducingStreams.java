1a909da98fd710f90f0bd12ff5dd4602f6a1608b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 13:30:10 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/ReducingStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/ReducingStreams.java
@@ -5,8 +5,8 @@ import java.util.stream.Stream;
 public class ReducingStreams {
     public static void main(String[] args) {
         Stream<String> stream = Stream.of("w","o", "l", "f");
-        String word = stream.reduce("", (s,c) -> s + c );
-       // String word = stream.reduce("", String::concat);
+       // String word = stream.reduce("", (s,c) -> s + c );
+       String word = stream.reduce("", String::concat);
         System.out.println(word);
     }
 }
