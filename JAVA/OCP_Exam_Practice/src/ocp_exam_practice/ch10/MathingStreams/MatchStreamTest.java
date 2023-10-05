f07cdf69a585ac3690c3622eac485577d7e2c26f
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 12:30:11 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/MathingStreams/MatchStreamTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/MathingStreams/MatchStreamTest.java
@@ -2,6 +2,13 @@ package ocp_exam_practice.ch10.MathingStreams;
 
 public class MatchStreamTest {
     public static void main(String[] args) {
-
+        var list = List.of("monkey", "2", "chimp");
+        Stream<String> infinite = Stream.generate(() ->
+                "chimp");
+        Predicate<String> pred = x ->
+                Character.isLetter(x.charAt(0));
+        System.out.println(list.stream().anyMatch(pred)); // true
+        System.out.println(list.stream().allMatch(pred)); // false
+        System.out.println(list.stream().noneMatch(pred)); // false
     }
 }
