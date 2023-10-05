eb94da518a8e82518555b21484b221d2f138b5b0
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 10:48:35 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalTest.java
@@ -44,9 +44,7 @@ public class OptionalTest {
         Optional<Double> opt4 = average();
          System.out.println(opt4.orElseThrow());
 
-        Optional<Double> opt = average();
-         System.out.println(opt.orElseThrow(
-                 () ->
-                new IllegalStateException()));
+        Optional<Double> opt5 = average();
+         System.out.println(opt.orElseThrow(() -> new IllegalStateException()));
     }
 }
