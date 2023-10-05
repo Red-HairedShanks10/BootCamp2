1f81bb8c6effe153ccc3bbd8eaf1f4fd1805fd80
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-05 07:18:00 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalTest.java
@@ -1,4 +1,12 @@
 package ocp_exam_practice.ch10;
 
+import java.util.Optional;
+
 public class OptionalTest {
+    public static Optional<Double> average(int... scores) {
+        11: if (scores.length == 0) return Optional.empty();
+        12: int sum = 0;
+        13: for (int score: scores) sum += score;
+        14: return Optional.of((double) sum / scores.length);
+        15: }
 }
