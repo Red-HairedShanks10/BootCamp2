b2f60ddf60e2e95dfff6b6337edb90604bcff98b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 11:11:19 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/PeekingStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ReducingStreams/PeekingStreams.java
@@ -1,7 +1,34 @@
 package ocp_exam_practice.ch10.ReducingStreams;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class PeekingStreams {
     public static void main(String[] args) {
-        
+       /* var peekStream = Stream.of("black bear", "brown bear", "grizzly", "grey wolf");
+        long countGbears = peekStream.filter(s -> s.startsWith("g"))
+                .peek(System.out::println).count();
+        System.out.println(countGbears);*/
+
+        var numbers = new ArrayList<>();
+        var letters = new ArrayList<>();
+
+        numbers.add(1);
+         numbers.add(2);
+        numbers.add(5);
+
+        letters.add('a');
+       // letters.add('b');
+
+        Stream<List<?>> pStream = Stream.of(numbers, letters);
+
+        pStream.map(x -> x.size())
+                .peek(System.out::println)
+                .forEach(System.out::println);
+
+       /* Stream<List<?>> bad = Stream.of(numbers, letters);
+        bad.peek(x -> x.remove(0)).map(List::size)
+                .forEach(System.out::println);*/
     }
 }
