4f6bb636d4d9717a8a5df69f1e983557fb5a162f
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 10:22:24 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/SortingStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/SortingStreams.java
@@ -1,10 +1,17 @@
 package ocp_exam_practice.ch10;
 
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class SortingStreams {
     public static void main(String[] args) {
+
         Stream<String> s = Stream.of("brown-", "bear-");
         s.sorted().forEach(System.out::print);
+
+        Stream<String> s = Stream.of("brown bear-",
+                "grizzly-");
+        s.sorted(Comparator.reverseOrder())
+                .forEach(System.out::print); // grizzly-brownbear-
     }
 }
