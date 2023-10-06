34a94ceb0ad16fbdd9440c12eb9a93daccf89e3b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 14:36:07 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalPrimitiveTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalPrimitiveTest.java
@@ -1,5 +1,6 @@
 package ocp_exam_practice.ch10;
 
+import java.util.IntSummaryStatistics;
 import java.util.OptionalDouble;
 import java.util.OptionalInt;
 import java.util.stream.DoubleStream;
@@ -8,19 +9,33 @@ import java.util.stream.LongStream;
 
 public class OptionalPrimitiveTest {
     public static void main(String[] args) {
-        LongStream longs = LongStream.of(5,10);
+       /* LongStream longs = LongStream.of(5,10);
         long sum = longs.sum();
          System.out.println(sum);
          DoubleStream doubles = DoubleStream.generate(() -> Math.PI);
          OptionalDouble min = doubles.min();
 
          DoubleStream stream = DoubleStream.generate(() -> Math.PI);
-                 stream.min();
+                 stream.min();*/
+
+        IntStream intStream = IntStream.of(1,2,3,5);
+        IntStream intStream2 = IntStream.of();
+        System.out.println(max(intStream));
+        System.out.println(max(intStream2));
+
 
     }
 
     private static int max(IntStream ints) {
         OptionalInt optional = ints.max();
         return optional.orElseThrow(RuntimeException::new);
+    }
+
+
+
+    private static int range(IntStream ints) {
+        IntSummaryStatistics stats = ints.summaryStatistics();
+        if (stats.getCount() == 0) throw new RuntimeException();
+        return stats.getMax()-stats.getMin();
     }
 }
