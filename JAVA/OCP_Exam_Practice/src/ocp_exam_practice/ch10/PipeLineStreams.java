4ea94cafcfe507de0de3db78a1b9091567031468
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 11:14:26 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PipeLineStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PipeLineStreams.java
@@ -6,5 +6,9 @@ public class PipeLineStreams {
     public static void main(String[] args) {
         var list = List.of("Toby", "Anna", "Leroy", "Alex");
 
+        list.stream().filter(n -> n.length() == 4).sorted()
+                .limit(2).forEach(System.out::println);
+        
+
     }
 }
