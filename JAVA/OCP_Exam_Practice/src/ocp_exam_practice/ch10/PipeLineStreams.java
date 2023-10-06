1f943f65b69e3b986f0bd4837af49cd7bfcc2120
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 11:58:13 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PipeLineStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PipeLineStreams.java
@@ -1,14 +1,41 @@
 package ocp_exam_practice.ch10;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PipeLineStreams {
     public static void main(String[] args) {
         var list = List.of("Toby", "Anna", "Leroy", "Alex");
 
-        list.stream().filter(n -> n.length() == 4).sorted()
-                .limit(2).forEach(System.out::println);
-        
+       /* list.stream().filter(n -> n.length() == 4)
+                .sorted()
+               .limit(2)
+                .forEach(System.out::println);*/
+
+        /*Stream.generate(() -> "Elsa")
+                .filter(n -> n.length() == 4)
+                .limit(2)
+              //  .sorted()
+                .forEach(System.out::println);*/
+
+       /* long count = Stream.of("goldfish", "finch")
+                .filter(s -> s.length() > 5)
+                .peek(System.out::println)
+                .collect(Collectors.toList())
+                .stream()
+                .count();
+        System.out.println("count of fish?: " + count);*/
+
+        List<String> stringList = Stream.of("finch", "goldfish").toList()
+                .stream().filter(n -> n.length() > 5)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+        Long count = stringList.stream().count();
+
+        System.out.println(count);
+
 
     }
 }
