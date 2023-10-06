bc1946a16d032dc69b4ef355c5deceb8acad0020
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 10:18:01 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/FlatMapping.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/FlatMapping.java
@@ -11,11 +11,14 @@ public class FlatMapping {
         var one = List.of("Bonobo");
         var two = List.of("Mama Gorilla", "Baby Gorilla");
         Stream<List<String>> animals = Stream.of(zero, one, two);
+        System.out.println("FlatMap concat: ");
         animals.flatMap(m -> m.stream()).forEach(System.out::println);
+        System.out.println("//////////////////////");
 
         ///other way to concatenate strings:
         var oneString = Stream.of("Bonobo");
         var twoStrings = Stream.of("Mama Gorilla", "Baby Gorilla");
+        System.out.println("Stream.concat(): ");
         Stream.concat(oneString,twoStrings).forEach(System.out::println);
     }
 }
