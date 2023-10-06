b2a078373c9bc106147e291027884be682d25150
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 10:21:07 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/SortingStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/SortingStreams.java
@@ -4,9 +4,8 @@ import java.util.stream.Stream;
 
 public class SortingStreams {
     public static void main(String[] args) {
-        Stream<String> s = Stream.of("brown-",
-                "bear-");
+        Stream<String> s = Stream.of("brown-", "bear-");
         s.sorted()
-                .forEach(System.out::print); // bear-brow
+                .forEach(System.out::print);
     }
 }
