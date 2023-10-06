8b5cbf6d5bbbd75529db91322786f40c63297741
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 10:08:50 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/FlatMapping.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/FlatMapping.java
@@ -1,7 +1,15 @@
 package ocp_exam_practice.ch10;
 
+import java.util.List;
+
 public class FlatMapping {
     public static void main(String[] args) {
 
+        var one = List.of("Bonobo");
+        var two = List.of("Mama Gorilla", "Baby Gorilla");
+        Stream<List<String>> animals = Stream.of(zero, one, two);
+        animals.flatMap(m ->
+                        m.stream())
+                .forEach(System.out::println);
     }
 }
