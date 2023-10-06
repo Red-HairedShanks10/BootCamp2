55e1a29e5b54c74ed86219749e92f01173fd585d
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 11:12:57 +0200

    Java Commit

---
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PipeLineStreams.java
@@ -0,0 +1,8 @@
+package ocp_exam_practice.ch10;
+
+public class PipeLineStreams {
+    public static void main(String[] args) {
+        var list = List.of("Toby", "Anna", "Leroy", "Alex");
+        
+    }
+}
