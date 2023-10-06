bb2f569959b1d059bc5f592f9f3bdd5d1f08937f
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 15:54:30 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ExceptionCaseStudy.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/ExceptionCaseStudy.java
@@ -1,4 +1,23 @@
 package ocp_exam_practice.ch10;
+import java.io.*;
+import java.util.*;
+
 
 public class ExceptionCaseStudy {
+
+    private static List<String> create() throws IOException {
+        throw new IOException();
+    }
+
+    public void good() throws IOException {
+        ExceptionCaseStudy.create().stream().count();
+    }
+
+    public void bad() throws IOException {
+        Supplier<List<String>> s = ExceptionCaseStudy::create; // DOES NOT COMPILE
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
