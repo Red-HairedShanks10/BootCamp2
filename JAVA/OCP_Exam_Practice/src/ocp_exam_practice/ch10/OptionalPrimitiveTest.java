0c23d59fbb5dcb0254302d4f85b63417c4eed0ea
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 14:19:45 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalPrimitiveTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/OptionalPrimitiveTest.java
@@ -1,4 +1,11 @@
 package ocp_exam_practice.ch10;
 
 public class OptionalPrimitiveTest {
+    public static void main(String[] args) {
+        long sum = longs.sum();
+        7: System.out.println(sum); // 15
+        8: DoubleStream doubles = DoubleStream.generate(() ->
+                Math.PI);
+         OptionalDouble min = doubles.min(); // runs infinitely
+    }
 }
