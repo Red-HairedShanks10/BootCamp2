cbc4756f4e1ca7c0f39cbd43e909cfc5e59f466c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 16:12:14 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/SpliteratorTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/SpliteratorTest.java
@@ -1,4 +1,24 @@
 package ocp_exam_practice.ch10;
 
 public class SpliteratorTest {
+    public static void main(String[] args) {
+
+        var stream = List.of("bird-",
+                "bunny-",
+                "cat-",
+                "dog-",
+                "fish-",
+                "lamb-",
+                13: "mouse-");
+        14: Spliterator<String> originalBagOfFood = stream.spliterator();
+        15: Spliterator<String> emmasBag = originalBagOfFood.trySplit();
+        16: emmasBag.forEachRemaining(System.out::print); // bird-bunny-
+        cat-
+                17:
+        18: Spliterator<String> jillsBag = originalBagOfFood.trySplit();
+        19: jillsBag.tryAdvance(System.out::print); // dog-20:
+        jillsBag.forEachRemaining(System.out::print); // fish-21:
+        originalBagOfFood.forEachRemaining(System.out::print); // lamb-mouse-
+
+    }
 }
