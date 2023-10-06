f834108a08c7ad8cf56b4849657f5e7a78b885d1
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-06 14:52:12 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/AdvancedStreams.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/AdvancedStreams.java
@@ -1,7 +1,12 @@
 package ocp_exam_practice.ch10;
 
 public class AdvancedStreams {
-    public static void main(String[] args) {
-
-    }
+        public static void main(String[] args) {
+            var cats = new ArrayList<String>();
+            26: cats.add("Annie");
+            27: cats.add("Ripley");
+            28: var stream = cats.stream();
+            29: cats.add("KC");
+             System.out.println(stream.count());
+        }
 }
