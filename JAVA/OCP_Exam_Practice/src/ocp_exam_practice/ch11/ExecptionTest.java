ab57af2565e49639b375a4ac78be729233908f21
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 10:58:24 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/ExecptionTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/ExecptionTest.java
@@ -9,6 +9,8 @@ public class ExecptionTest {
            if (distance > 10) {
                throw new IOException();
            }
+       }catch (Exception e){
+           e.printStackTrace();
        }
     }
 
