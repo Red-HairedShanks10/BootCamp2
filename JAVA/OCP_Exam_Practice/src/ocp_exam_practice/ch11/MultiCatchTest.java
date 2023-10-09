e0b29a7c31d1c051520df4398bf8b30ee645c279
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 14:00:52 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/MultiCatchTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/MultiCatchTest.java
@@ -7,6 +7,8 @@ public class MultiCatchTest {
             System.out.println(Integer.parseInt(args[1]));
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Missing or invalid input");
-        }catch (NumberFormatException e)
+        }catch (NumberFormatException e){
+            System.out.println("Missing or invalid input");
+        }
     }
 }
