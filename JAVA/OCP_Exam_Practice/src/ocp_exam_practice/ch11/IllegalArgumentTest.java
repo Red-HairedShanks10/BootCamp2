052d9241fc3d86a79eaa057938b53a98f25c2675
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 12:59:21 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/IllegalArgumentTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/IllegalArgumentTest.java
@@ -1,6 +1,7 @@
 package ocp_exam_practice.ch11;
 
 public class IllegalArgumentTest {
+    int numberOfEggs;
     public static void main(String[] args) {
 
     }
@@ -9,5 +10,6 @@ public class IllegalArgumentTest {
         if(numberEggs < 0){
             throw new IllegalArgumentException("num of eggs must not be negative");
         }
+        this.numberOfEggs = numberEggs;
     }
 }
