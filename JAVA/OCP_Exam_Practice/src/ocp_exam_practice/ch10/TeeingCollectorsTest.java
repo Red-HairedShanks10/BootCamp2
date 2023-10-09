4344918268b6a7ce32b5a4e2f961744303147e3f
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 10:55:09 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/TeeingCollectorsTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/TeeingCollectorsTest.java
@@ -1,4 +1,17 @@
 package ocp_exam_practice.ch10;
 
+import java.util.List;
+import static java.util.stream.Collectors.*;
+
 public class TeeingCollectorsTest {
+    public static void main(String[] args) {
+        var list = List.of("x", "y", "z");
+        Separations result = list.stream()
+                .collect(teeing(
+                        joining(" "),
+                        joining(" "),
+                        ((s, c) -> new Separations(s,c) )
+                ));
+        System.out.println(result);
+    }
 }
