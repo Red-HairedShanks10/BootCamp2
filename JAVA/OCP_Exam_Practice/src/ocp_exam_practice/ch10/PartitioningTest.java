2e17e0b9de0f55302842830c792a561789a3a35b
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 09:39:31 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PartitioningTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PartitioningTest.java
@@ -2,6 +2,7 @@ package ocp_exam_practice.ch10;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,8 +10,25 @@ public class PartitioningTest {
     public static void main(String[] args) {
 
         var ohMy = Stream.of("lions", "tigers", "jaguars", "bears");
-        Map<Boolean, List<String>> map = ohMy.collect(
+        /*Map<Boolean, List<String>> map = ohMy.collect(
                 Collectors.partitioningBy(s -> s.length() <= 7));
-        System.out.println(map);
+        System.out.println(map);*/
+
+
+        /*Map<Boolean, Set<String>> map2 = ohMy.collect(
+                Collectors.partitioningBy(
+                        s -> s.length() <= 7,
+                        Collectors.toSet()));
+        System.out.println(map2);*/
+
+
+        //grouping animal name lengths by the count()
+        Map<Integer, Long> map3 = ohMy.collect(
+                Collectors.groupingBy(
+                        String::length,
+                        Collectors.counting()));
+        System.out.println(map3);
+
+
     }
 }
