d31fcc2288608e4e1d7c7f2c839c6670f87da0a4
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 10:55:47 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/ExecptionTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/ExecptionTest.java
@@ -1,7 +1,11 @@
 package ocp_exam_practice.ch11;
 
+import java.io.IOException;
+
 public class ExecptionTest {
     public static void main(String[] args) {
         void fall(int distance) throws IOException {
+
+        }
     }
 }
