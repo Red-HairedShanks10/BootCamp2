14ac54978074829252f29e056dccbb056b378e47
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 13:25:46 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/TryCatchChain.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/TryCatchChain.java
@@ -1,4 +1,11 @@
 package ocp_exam_practice.ch11;
 
 public class TryCatchChain {
+    public static void main(String[] args) {
+
+    }
+
+
 }
+
+class AnimalsOutForAWalk extends RuntimeException {}
