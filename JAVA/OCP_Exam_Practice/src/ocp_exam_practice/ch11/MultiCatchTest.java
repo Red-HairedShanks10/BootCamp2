fe879b62e23e79ae245226cce70d109a379a3a87
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 13:59:36 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/MultiCatchTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/MultiCatchTest.java
@@ -3,5 +3,8 @@ package ocp_exam_practice.ch11;
 public class MultiCatchTest {
     public static void main(String[] args) {
 
+        try {
+            System.out.println(Integer.parseInt(args[1]));
+        }
     }
 }
