c3baa5c2e8322943ee68d44f6565ff76135f6d0e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 09:31:45 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PartitioningTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PartitioningTest.java
@@ -1,7 +1,11 @@
 package ocp_exam_practice.ch10;
 
+import java.util.stream.Stream;
+
 public class PartitioningTest {
     public static void main(String[] args) {
+
+        var ohMy = Stream.of("lions", "tigers", "jaguars", "bears");
 
     }
 }
