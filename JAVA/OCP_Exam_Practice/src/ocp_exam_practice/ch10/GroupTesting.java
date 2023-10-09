f23af34cfa2141f590bc80789d108710df15f57c
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 09:04:14 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/GroupTesting.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/GroupTesting.java
@@ -1,5 +1,7 @@
 package ocp_exam_practice.ch10;
 
+import java.util.stream.Stream;
+
 public class GroupTesting {
     public static void main(String[] args) {
         var ohMy = Stream.of("lions", "tigers", "bears");
