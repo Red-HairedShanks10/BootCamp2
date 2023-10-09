71b6e5fdfb431fc3cc74fbe30e5a027fedbaa316
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 11:24:26 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/Bunny.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/Bunny.java
@@ -1,4 +1,9 @@
 package ocp_exam_practice.ch11;
 
 public class Bunny {
+    public static void main(String[] args) {
+
+    }
+
+    private static void eatCarrot()throws  NoMoreCarrotsException{}
 }
