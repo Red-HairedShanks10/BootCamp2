dd4c990904f964660eb343d45e6ecaa953e1adaf
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 12:55:05 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/IllegalArgumentTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/IllegalArgumentTest.java
@@ -1,5 +1,9 @@
 package ocp_exam_practice.ch11;
 
 public class IllegalArgumentTest {
-    
+    public static void main(String[] args) {
+
+    }
+
+    public void setNumberEggs(int numberEggs) {
 }
