1564c681c42b6815733680eb6f031cff3a16873f
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 09:06:48 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/GroupTesting.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/GroupTesting.java
@@ -8,6 +8,9 @@ import java.util.stream.Stream;
 public class GroupTesting {
     public static void main(String[] args) {
         var ohMy = Stream.of("lions", "tigers", "bears");
-
+        Map<Integer, Set<String>> map = ohMy.collect(
+                Collectors.groupingBy(
+                        String::length,
+                        Collectors.toSet()));
     }
 }
