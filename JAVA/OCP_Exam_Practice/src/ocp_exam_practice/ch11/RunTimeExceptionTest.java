bca55fe6ad1c94eea37ff742ceaacc1517500d90
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 12:54:33 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/RunTimeExceptionTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/RunTimeExceptionTest.java
@@ -2,14 +2,14 @@ package ocp_exam_practice.ch11;
 
 public class RunTimeExceptionTest {
     public static void main(String[] args) {
-
+        new Frog().hop(null, 3);
 
     }
 }
 
 
 class Frog{
-    public static void hop(String name, Integer jump){
+    public  void hop(String name, Integer jump){
             System.out.println(name.toLowerCase() + " " + jump.intValue());
     }
 }
