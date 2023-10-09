bf028256df57e2d4339e6644e97b4e5f03b912ab
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-09 09:33:33 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PartitioningTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch10/PartitioningTest.java
@@ -1,11 +1,16 @@
 package ocp_exam_practice.ch10;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PartitioningTest {
     public static void main(String[] args) {
 
         var ohMy = Stream.of("lions", "tigers", "jaguars", "bears");
-
+        Map<Boolean, List<String>> map = ohMy.collect(
+                Collectors.partitioningBy(s -> s.length() <= 7));
+        System.out.println(map);
     }
 }
