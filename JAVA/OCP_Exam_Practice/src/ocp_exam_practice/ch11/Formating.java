ba73c501bd3347e5864fdf09f5926cc037cecf71
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-10 09:17:12 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/Formating.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/Formating.java
@@ -3,5 +3,8 @@ package ocp_exam_practice.ch11;
 public class Formating {
     public static void main(String[] args) {
 
+        var f1 = DateTimeFormatter.ofPattern("MMMM dd, yyyy ");
+        var f2 = DateTimeFormatter.ofPattern(" hh:mm");
+        System.out.println(dt.format(f1) + "at" + dt.format(f2));
     }
 }
