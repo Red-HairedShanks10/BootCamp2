645b87f7d9914f0bd355fe7b1d4bb72653faeadb
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-10 12:28:28 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/ZooOptions.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/ZooOptions.java
@@ -5,5 +5,8 @@ public class ZooOptions {
         var props = new Properties();
         props.setProperty("name", "Our zoo");
         props.setProperty("open", "10am");
+
+        System.out.println(props.getProperty("camel", "Bob"));
+        System.out.println(props.get("open"));
     }
 }
