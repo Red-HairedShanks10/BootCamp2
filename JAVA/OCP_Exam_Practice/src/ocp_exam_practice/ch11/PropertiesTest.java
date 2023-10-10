ec38bc34352c1f36720594ce3d3896d2236341be
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-10 11:14:45 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/PropertiesTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/PropertiesTest.java
@@ -4,4 +4,11 @@ public class PropertiesTest {
     public static void main(String[] args) {
 
     }
+
+
+    10: public static void printWelcomeMessage(Locale locale) {
+        11: var rb = ResourceBundle.getBundle("Zoo", locale);
+        12: System.out.println(rb.getString("hello")
+                13: + ", " + rb.getString("open"));
+         }
 }
