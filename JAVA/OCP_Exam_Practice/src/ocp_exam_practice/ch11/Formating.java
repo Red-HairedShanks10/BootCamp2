b6696e3188dc86094edaaaca1a1e69355ec3fd62
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-10 09:43:04 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/Formating.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch11/Formating.java
@@ -45,7 +45,7 @@ public class Formating {
         System.out.println(us.format(attendeesPerMonth)); // 266,666
         var gr = NumberFormat.getInstance(Locale.GERMANY);
         System.out.println(gr.format(attendeesPerMonth)); // 266.666
-        var za = NumberFormat.getInstance(Locale.CANADA_FRENCH);
-        System.out.println(ca.format(attendeesPerMonth));
+        var za = NumberFormat.getInstance();
+        System.out.println(za.format(attendeesPerMonth));
     }
 }
