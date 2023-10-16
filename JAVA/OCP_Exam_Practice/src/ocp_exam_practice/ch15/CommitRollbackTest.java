e00461fa0b0e76030b1202643c6ff243d980aec8
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-16 15:24:34 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch15/CommitRollbackTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch15/CommitRollbackTest.java
@@ -12,4 +12,25 @@ public class CommitRollbackTest {
              conn.setAutoCommit(false);
 
              var elephantRowsUpdated = updateRow(conn, 5, "African Elephant");
+             var zebraRowsUpdated = updateRow(conn, -5,
+                     "Zebra");
+             13:
+             14: if (! elephantRowsUpdated || ! zebraRowsUpdated)
+                 15: conn.rollback();
+             16: else {
+                 17: String selectSql = """
+18: SELECT COUNT(*)
+19: FROM exhibits
+20: WHERE num_acres <= 0""";
+                 21: try (PreparedStatement ps = conn.prepareStatement(selectSql);
+22: ResultSet rs = ps.executeQuery()) {
+                     23:
+                     24: rs.next();
+                     25: int count = rs.getInt(1);
+                     26: if (count == 0)
+                         27: conn.commit();
+                     28: else
+                     29: conn.rollback();
+                      } } } }
+
 }
