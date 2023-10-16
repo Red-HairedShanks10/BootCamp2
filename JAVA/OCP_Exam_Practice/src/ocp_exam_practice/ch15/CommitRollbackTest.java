c8f95087913c74f21f2aa21aaa1114337abedf46
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-16 15:23:52 +0200

    Java Commit

--- JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch15/CommitRollbackTest.java
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch15/CommitRollbackTest.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class CommitRollbackTest {
     public static void main(String[] args) throws SQLException {
-        6: try (Connection conn =
+         try (Connection conn =
                          DriverManager.getConnection("jdbc:hsqldb:file:zoo")) {
 
              conn.setAutoCommit(false);
