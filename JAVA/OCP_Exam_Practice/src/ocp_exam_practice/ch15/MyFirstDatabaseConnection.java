c719eab24291e12f19c49826318efbfc44351244
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-17 21:06:01 +0200

    Java Commit

---
+++ JAVA/OCP_Exam_Practice/src/ocp_exam_practice/ch15/MyFirstDatabaseConnection.java
@@ -0,0 +1,16 @@
+package ocp_exam_practice.ch15;
+import java.sql.*;
+
+public class MyFirstDatabaseConnection {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:hsqldb:file:zoo";
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement ps = conn.prepareStatement(
+                     "SELECT name FROM exhibits");
+             ResultSet rs = ps.executeQuery()) {
+            System.out.println(conn);
+            while (rs.next())
+                System.out.println(rs.getString(1));
+        } }
+
+}
