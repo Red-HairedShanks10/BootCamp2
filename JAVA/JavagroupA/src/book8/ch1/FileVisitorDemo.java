a2e55a56480c628f9511bcfa21e29d549ad6d34e
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-10-18 10:19:57 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch1/FileVisitorDemo.java
+++ JAVA/JavagroupA/src/book8/ch1/FileVisitorDemo.java
@@ -50,7 +50,8 @@ public class FileVisitorDemo
             return FileVisitResult.CONTINUE;
         }
 
-        //
+        //override preVisitDirectoryFailed()
+        // does a similar action as visitFileFail(), focuses on unavailable directories
         public FileVisitResult preVisitDirectoryFailed(Path dir, IOException e)
         {
             System.out.println(dir.toString() + " COULD NOT ACCESS!");
