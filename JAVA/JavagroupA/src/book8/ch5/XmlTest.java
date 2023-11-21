a24f9139c6fd69019a15b0c444482af224cb7ae4
Marco <123168052+Red-HairedShanks10@users.noreply.github.com>
2023-11-22 00:25:35 +0200

    java commit

--- JAVA/JavagroupA/src/book8/ch5/XmlTest.java
+++ JAVA/JavagroupA/src/book8/ch5/XmlTest.java
@@ -10,21 +10,29 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 public class XmlTest {
     public static void main(String[] args) {
+
+
+        //create a document from XML file
         Document doc = getDocument("C:\\Users\\Marco\\Desktop\\BootCamp2\\JAVA\\JavagroupA\\src\\book8\\ch5\\movies.xml");
         System.out.println("DOM created");
 
+
+        /*PROCESSING ELEMENTS*/
         int count = 0;
          Element root = doc.getDocumentElement();
-         Node movie = root.getFirstChild();
-         
+         Node movie = root.getFirstChild();//get first node which is Movie
+         //count all movie nodes available
+        //while movie node-list has elements in it:
         while (movie != null)
         {
-            count++;
-            movie = movie.getNextSibling();
+            count++;//increment count value
+            movie = movie.getNextSibling();//move to next sibling node of the current node
         }
+        //print the number of movies found:
         System.out.println("There are " + count + " movies.");
     }
 
+    //getDocument() reads and XML file into a DOM document
     private static Document getDocument(String name)
     {
         try
